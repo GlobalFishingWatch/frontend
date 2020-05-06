62d8e8767cda06e2f9e846d4ee57ed770b0e7474
j8seangel <jseangel@gmail.com>
2020-05-06 19:35:15 +0200

    stylelint prettier config

--- applications/sandbox/src/App.tsx
+++ applications/sandbox/src/App.tsx
@@ -6,9 +6,7 @@ import GFWAPI from '@globalfishingwatch/api-client/src/index'
 import { useGFWLogin } from '@globalfishingwatch/react-hooks/src/index'
 
 function App() {
-  // used any as types and local definitions are incompatible
-  // remove the /src/index from the import to use the builded version
-  const { logged, user } = useGFWLogin(GFWAPI as any)
+  const { logged, user } = useGFWLogin(GFWAPI)
 
   return (
     <div className="App">
