427c63ecc890439fe8817c562241b39666254b84
j8seangel <jseangel@gmail.com>
2020-05-06 19:18:13 +0200

    show logged example in sandbox render

--- applications/sandbox/src/App.tsx
+++ applications/sandbox/src/App.tsx
@@ -8,13 +8,18 @@ import { useGFWLogin } from '@globalfishingwatch/react-hooks/src/index'
 function App() {
   // used any as types and local definitions are incompatible
   // remove the /src/index from the import to use the builded version
-  const login = useGFWLogin(GFWAPI as any)
-  console.log('App -> login', login)
+  const { logged, user } = useGFWLogin(GFWAPI as any)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <br/>
+        Using <code>useGFWLogin</code>
+        {logged ? `Logged user: ${user?.firstName}`: 'User not logged'}
       </header>
+      <main>
+      </main>
     </div>
   );
 }
