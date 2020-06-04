036deb0025fbac5c39e0279fa3b7a079822686f9
j8seangel <jseangel@gmail.com>
2020-06-04 17:45:04 +0200

    namespace start scripts

--- applications/sandbox/src/index.tsx
+++ applications/sandbox/src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import { Provider } from 'react-redux'
-
 import * as serviceWorker from './serviceWorker'
 import { store } from './app/store'
 import App from './App'
