0e52dffa165c7b1c9a8a3e2e184b2d3e2aa34b2b
j8seangel <jseangel@gmail.com>
2020-06-05 16:03:18 +0200

    button ui-component

--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -1,3 +1,4 @@
 export { default as CountryFlag } from './countryflag'
 export { default as MiniGlobe } from './miniglobe'
 export { default as Header } from './header'
+export { default as Button } from './button'
