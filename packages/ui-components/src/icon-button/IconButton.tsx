a0061cc16bd54d4de4e980350f936b36213509a6
j8seangel <jseangel@gmail.com>
2020-06-08 13:35:34 +0200

    Icon button color fill

--- packages/ui-components/src/icon-button/IconButton.tsx
+++ packages/ui-components/src/icon-button/IconButton.tsx
@@ -3,19 +3,26 @@ import cx from 'classnames'
 import Icon, { IconType } from '../icon'
 import styles from './IconButton.module.css'
 
-type IconButtonTypes = 'default' | 'invert' | 'border'
+export type IconButtonType = 'default' | 'invert' | 'border'
+export type IconButtonSize = 'default' | 'small' | 'tiny'
 
 interface IconButtonProps {
   icon: IconType
-  type?: IconButtonTypes
+  type?: IconButtonType
+  size?: IconButtonSize
   className?: string
+  disabled?: boolean
   onClick?: (e: React.MouseEvent) => void
 }
 
 const IconButton: React.FC<IconButtonProps> = (props) => {
-  const { type = 'default', className, icon, onClick } = props
+  const { type = 'default', size = 'default', disabled = false, className, icon, onClick } = props
   return (
-    <button className={cx(styles.IconButton, styles[type], className)} onClick={onClick}>
+    <button
+      className={cx(styles.IconButton, styles[type], styles[`${size}Size`], className)}
+      onClick={onClick}
+      disabled={disabled}
+    >
       <Icon icon={icon} />
     </button>
   )
