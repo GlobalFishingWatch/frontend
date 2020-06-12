58e6ab09bda6c468e0e922b161e02c7c5f404877
j8seangel <jseangel@gmail.com>
2020-06-12 17:51:52 +0200

    accessToken url helpers in api-client

--- packages/api-client/src/index.ts
+++ packages/api-client/src/index.ts
@@ -1,3 +1,3 @@
 export { default, USER_TOKEN_STORAGE_KEY, USER_REFRESH_TOKEN_STORAGE_KEY } from './api-client'
-
+export { ACCESS_TOKEN_STRING, getAccessTokenFromUrl, removeAccessTokenFromUrl } from './utils/url'
 export type { UserData } from './types'
