b710caf2e73f30443370b719749198c09ef1f7dd
j8seangel <jseangel@gmail.com>
2020-07-22 11:46:09 +0200

    Sidebar header changes by page

--- applications/amathea/src/common/SidebarHeader.tsx
+++ applications/amathea/src/common/SidebarHeader.tsx
@@ -1,19 +1,29 @@
 import React, { Fragment } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import Link from 'redux-first-router-link'
 import IconButton from '@globalfishingwatch/ui-components/dist/icon-button'
 import Logo from '@globalfishingwatch/ui-components/dist/logo'
 import { selectUserData, logoutUserThunk } from 'features/user/user.slice'
 import { toggleMenu } from 'features/app/app.slice'
+import { isWorkspaceEditorPage } from 'routes/routes.selectors'
+import { WORKSPACES } from 'routes/routes'
 import styles from './SidebarHeader.module.css'
 
 function SidebarHeader(): React.ReactElement {
   const dispatch = useDispatch()
   const userData = useSelector(selectUserData)
+  const isWorkspaceEditor = useSelector(isWorkspaceEditorPage)
   const initials = `${userData?.firstName?.slice(0, 1)}${userData?.lastName?.slice(0, 1)}`
 
   return (
     <header className={styles.container}>
-      <IconButton icon="menu" onClick={() => dispatch(toggleMenu())} />
+      {isWorkspaceEditor ? (
+        <Link to={{ type: WORKSPACES }}>
+          <IconButton icon="home" />
+        </Link>
+      ) : (
+        <IconButton icon="menu" onClick={() => dispatch(toggleMenu())} />
+      )}
       <Logo subBrand="Marine Reserves" />
 
       <div className={styles.righSide}>
