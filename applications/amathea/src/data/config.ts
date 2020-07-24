bf7a5a2ffda814a4cb539fea6b509768a3dcd478
satellitestudiodesign <juancarlos@satellitestud.io>
2020-07-24 10:49:55 +0200

    connect graphs with time range

--- applications/amathea/src/data/config.ts
+++ applications/amathea/src/data/config.ts
@@ -2,7 +2,7 @@ export const DEFAULT_WORKSPACE = {
   latitude: 0,
   longitude: 0,
   zoom: 1,
-  start: '2017-01-01T00:00:00.000Z',
-  end: new Date().toISOString(),
+  start: '2018-01-01T00:00:00.000Z',
+  end: '2019-12-31T23:59:59.999Z',
   modal: undefined,
 }
--- applications/amathea/src/features/dataviews/DataviewGraphPanel.tsx
+++ applications/amathea/src/features/dataviews/DataviewGraphPanel.tsx
@@ -1,55 +1,33 @@
-import React from 'react'
-import {
-  ResponsiveContainer,
-  LineChart,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-  Line,
-} from 'recharts'
-import { Dataview } from '@globalfishingwatch/dataviews-client'
+import React, { Fragment } from 'react'
 import IconButton from '@globalfishingwatch/ui-components/dist/icon-button'
-import { TEST_DATAVIEW_MONTHLY_STATS } from 'data/data'
+import { DataviewGraphConfig } from 'data/data'
 import styles from './DataviewGraphPanel.module.css'
+import DataviewGraph from './DataviewGraph'
 
 interface DataviewGraphPanelProps {
-  dataview?: Dataview
+  dataview: DataviewGraphConfig
 }
 
 const DataviewGraphPanel: React.FC<DataviewGraphPanelProps> = (props) => {
   const { dataview } = props
-  const data = dataview?.id && TEST_DATAVIEW_MONTHLY_STATS[dataview?.id]
-
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <p className={styles.title}>{dataview?.name}</p>
-        <IconButton icon="info" />
-        <IconButton icon="edit" />
-        <IconButton icon="download" />
-        <IconButton icon="delete" type="warning" />
-        <IconButton icon="remove-from-map" />
-      </div>
-      <ResponsiveContainer width="100%" height={240}>
-        <LineChart
-          data={data as { month: string; value: number }[]}
-          margin={{ top: 10, right: 20, left: 0, bottom: 0 }}
-        >
-          <CartesianGrid vertical={false} />
-          <XAxis dataKey="month" />
-          <YAxis
-            type="number"
-            dataKey="value"
-            domain={[(dataMin) => Math.floor(dataMin) - 1, (dataMax) => Math.ceil(dataMax) + 1]}
-            axisLine={false}
-            tickLine={false}
-            tickCount={5}
-          />
-          <Tooltip />
-          <Line type="monotone" dataKey="value" stroke="#8884d8" strokeWidth={2} />
-        </LineChart>
-      </ResponsiveContainer>
+      {dataview && (
+        <Fragment>
+          <div className={styles.header}>
+            <p className={styles.title}>
+              {dataview.name}
+              <span className={styles.unit}>{dataview.unit && ` (${dataview.unit})`}</span>
+            </p>
+            <IconButton icon="info" tooltip={dataview.description} />
+            <IconButton icon="edit" tooltip="Edit dataview" />
+            <IconButton icon="download" tooltip="Download time series data" />
+            <IconButton icon="delete" type="warning" tooltip="Remove dataview" />
+            <IconButton icon="view-on-map" tooltip="Show on map" />
+          </div>
+          <DataviewGraph dataview={dataview} />
+        </Fragment>
+      )}
     </div>
   )
 }
