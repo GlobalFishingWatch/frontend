98a9147b181fcf66478256af1e421ce42d35309c
erik <erik@satellitestud.io>
2020-08-06 15:08:17 +0200

    separate start and end

--- packages/layer-composer/src/generators/heatmap/util/get-server-side-filters.ts
+++ packages/layer-composer/src/generators/heatmap/util/get-server-side-filters.ts
@@ -1,7 +1,9 @@
 export default (start: string, end: string, serverSideFilter = '') => {
   const serverSideFiltersList = serverSideFilter ? [serverSideFilter] : []
-  if (start && end) {
+  if (start) {
     serverSideFiltersList.push(`timestamp >= '${start.slice(0, 19).replace('T', ' ')}'`)
+  }
+  if (end) {
     serverSideFiltersList.push(`timestamp <= '${end.slice(0, 19).replace('T', ' ')}'`)
   }
 
