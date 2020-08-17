2ac22baf75ea97a1760c1f017975792c246adcca
j8seangel <jseangel@gmail.com>
2020-08-17 15:16:41 +0200

    mount worspace editor following sidebar component

--- applications/amathea/src/features/app/App.tsx
+++ applications/amathea/src/features/app/App.tsx
@@ -2,7 +2,6 @@ import React, { Suspense, lazy, memo, Fragment, useCallback, useEffect } from 'r
 import { useSelector, useDispatch } from 'react-redux'
 import SplitView from '@globalfishingwatch/ui-components/dist/split-view'
 import Menu from '@globalfishingwatch/ui-components/dist/menu'
-import { useUserConnect } from 'features/user/user.hook'
 import { useDatasetsAPI } from 'features/datasets/datasets.hook'
 import Login from 'features/user/Login'
 import Modal from 'features/modal/Modal'
@@ -16,7 +15,6 @@ import '@globalfishingwatch/ui-components/dist/base.css'
 const Map = lazy(() => import('features/map/Map'))
 const Timebar = lazy(() => import('features/timebar/Timebar'))
 const Sidebar = lazy(() => import('features/sidebar/Sidebar'))
-const WorkspaceEditor = lazy(() => import('features/workspace-editor/WorkspaceEditor'))
 
 const Main = memo(() => {
   const isWorkspaceEditor = useSelector(isWorkspaceEditorPage)
@@ -31,11 +29,10 @@ const Main = memo(() => {
 })
 
 const SidebarWrapper = memo(() => {
-  const isWorkspaceEditor = useSelector(isWorkspaceEditorPage)
   return (
     <Suspense fallback={null}>
       <SidebarHeader />
-      {isWorkspaceEditor ? <WorkspaceEditor /> : <Sidebar />}
+      <Sidebar />
     </Suspense>
   )
 })
@@ -44,7 +41,6 @@ function App(): React.ReactElement {
   const dispatch = useDispatch()
   const menuOpen = useSelector(isMenuOpen)
   const sidebarOpen = useSelector(isSidebarOpen)
-  const { logged, status } = useUserConnect()
   const { fetchDatasets } = useDatasetsAPI()
 
   useEffect(() => {
@@ -63,18 +59,14 @@ function App(): React.ReactElement {
   return (
     <Fragment>
       <Login />
-      {status === 'loading' || !logged ? (
-        <div className={styles.placeholder}>Loading</div>
-      ) : (
-        <SplitView
-          isOpen={sidebarOpen}
-          onToggle={onToggleSidebar}
-          aside={<SidebarWrapper />}
-          main={<Main />}
-          asideWidth="50%"
-          className="split-container"
-        />
-      )}
+      <SplitView
+        isOpen={sidebarOpen}
+        onToggle={onToggleSidebar}
+        aside={<SidebarWrapper />}
+        main={<Main />}
+        asideWidth="50%"
+        className="split-container"
+      />
       <Modal />
       <Menu bgImage={menuBgImage} isOpen={menuOpen} onClose={onToggleMenu} activeLinkId="map-data">
         Menu toggle
