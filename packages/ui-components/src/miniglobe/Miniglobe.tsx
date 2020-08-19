466952f2e15fbc63b277e479ce1482da746e6707
j8seangel <jseangel@gmail.com>
2020-08-19 15:58:10 +0200

    support miniglobe without bounds

--- packages/ui-components/src/miniglobe/Miniglobe.tsx
+++ packages/ui-components/src/miniglobe/Miniglobe.tsx
@@ -6,26 +6,23 @@ import jsonData from './ne_110m_land.json'
 import styles from './Miniglobe.module.css'
 import { MiniglobeCenter, MiniglobeBounds } from './index'
 
+const MIN_DEGREES_PATH = 4
+const MAX_DEGREES_PATH = 150
+const defaultCenter = { latitude: 0, longitude: 0 }
+const worldData = feature(
+  (jsonData as unknown) as Topology,
+  jsonData.objects.land as GeometryCollection
+).features
+
 interface MiniglobeProps {
   center: MiniglobeCenter
-  bounds: MiniglobeBounds
+  bounds?: MiniglobeBounds
   size?: number
   viewportThickness?: number
 }
 
-const defaultBounds = {
-  north: 10,
-  south: -10,
-  west: -10,
-  east: 10,
-}
-
-const MIN_DEGREES_PATH = 4
-const MAX_DEGREES_PATH = 150
-const defaultCenter = { latitude: 0, longitude: 0 }
-
 function MiniGlobe(props: MiniglobeProps) {
-  const { bounds = defaultBounds, center = defaultCenter, size = 40, viewportThickness = 4 } = props
+  const { bounds, center = defaultCenter, size = 40, viewportThickness = 4 } = props
 
   const projection = useMemo(() => {
     const { latitude, longitude } = center
@@ -37,82 +34,105 @@ function MiniGlobe(props: MiniglobeProps) {
     return projection
   }, [center, size])
 
-  const worldData = useMemo(
-    () =>
-      feature((jsonData as unknown) as Topology, jsonData.objects.land as GeometryCollection)
-        .features,
-    []
+  const worldComponent = useMemo(
+    () => (
+      <g className={styles.land}>
+        {worldData.map((d, i) => {
+          const path = geoPath().projection(projection)(d)
+          return path && <path key={`path-${i}`} d={path} />
+        })}
+      </g>
+    ),
+    [projection]
   )
 
-  if (!bounds) {
-    console.error('MiniGlobe: bounds not specified')
-    return null
-  }
+  const boundsComponent = useMemo(() => {
+    if (!bounds) return null
 
-  const { north, south, west, east } = bounds
-  if (north < south || west > east) {
-    console.error('MiniGlobe: bounds specified not valid')
-  }
+    const { north, south, west, east } = bounds
 
-  const longitudeExtent = east - west
-  const latitudeExtent = north - south
+    if (north < south || west > east) {
+      console.error('MiniGlobe: bounds specified not valid')
+    }
 
-  const viewportBoundsGeoJSON = {
-    type: 'Feature',
-    geometry: {
-      type: 'Polygon',
-      coordinates: [
-        [
-          [west, north],
-          [west + longitudeExtent * 0.1, north],
-          [west + longitudeExtent * 0.2, north],
-          [west + longitudeExtent * 0.3, north],
-          [west + longitudeExtent * 0.4, north],
-          [west + longitudeExtent * 0.5, north],
-          [west + longitudeExtent * 0.6, north],
-          [west + longitudeExtent * 0.7, north],
-          [west + longitudeExtent * 0.8, north],
-          [west + longitudeExtent * 0.9, north],
-          [east, north],
-          [east, north - latitudeExtent * 0.1],
-          [east, north - latitudeExtent * 0.2],
-          [east, north - latitudeExtent * 0.3],
-          [east, north - latitudeExtent * 0.4],
-          [east, north - latitudeExtent * 0.5],
-          [east, north - latitudeExtent * 0.6],
-          [east, north - latitudeExtent * 0.7],
-          [east, north - latitudeExtent * 0.8],
-          [east, north - latitudeExtent * 0.9],
-          [east, south],
-          [east - longitudeExtent * 0.1, south],
-          [east - longitudeExtent * 0.2, south],
-          [east - longitudeExtent * 0.3, south],
-          [east - longitudeExtent * 0.4, south],
-          [east - longitudeExtent * 0.5, south],
-          [east - longitudeExtent * 0.6, south],
-          [east - longitudeExtent * 0.7, south],
-          [east - longitudeExtent * 0.8, south],
-          [east - longitudeExtent * 0.9, south],
-          [west, south],
-          [west, south + latitudeExtent * 0.1],
-          [west, south + latitudeExtent * 0.2],
-          [west, south + latitudeExtent * 0.3],
-          [west, south + latitudeExtent * 0.4],
-          [west, south + latitudeExtent * 0.5],
-          [west, south + latitudeExtent * 0.6],
-          [west, south + latitudeExtent * 0.7],
-          [west, south + latitudeExtent * 0.8],
-          [west, south + latitudeExtent * 0.9],
-          [west, north],
+    const longitudeExtent = east - west
+    const latitudeExtent = north - south
+
+    const viewportBoundsGeoJSON = {
+      type: 'Feature',
+      geometry: {
+        type: 'Polygon',
+        coordinates: [
+          [
+            [west, north],
+            [west + longitudeExtent * 0.1, north],
+            [west + longitudeExtent * 0.2, north],
+            [west + longitudeExtent * 0.3, north],
+            [west + longitudeExtent * 0.4, north],
+            [west + longitudeExtent * 0.5, north],
+            [west + longitudeExtent * 0.6, north],
+            [west + longitudeExtent * 0.7, north],
+            [west + longitudeExtent * 0.8, north],
+            [west + longitudeExtent * 0.9, north],
+            [east, north],
+            [east, north - latitudeExtent * 0.1],
+            [east, north - latitudeExtent * 0.2],
+            [east, north - latitudeExtent * 0.3],
+            [east, north - latitudeExtent * 0.4],
+            [east, north - latitudeExtent * 0.5],
+            [east, north - latitudeExtent * 0.6],
+            [east, north - latitudeExtent * 0.7],
+            [east, north - latitudeExtent * 0.8],
+            [east, north - latitudeExtent * 0.9],
+            [east, south],
+            [east - longitudeExtent * 0.1, south],
+            [east - longitudeExtent * 0.2, south],
+            [east - longitudeExtent * 0.3, south],
+            [east - longitudeExtent * 0.4, south],
+            [east - longitudeExtent * 0.5, south],
+            [east - longitudeExtent * 0.6, south],
+            [east - longitudeExtent * 0.7, south],
+            [east - longitudeExtent * 0.8, south],
+            [east - longitudeExtent * 0.9, south],
+            [west, south],
+            [west, south + latitudeExtent * 0.1],
+            [west, south + latitudeExtent * 0.2],
+            [west, south + latitudeExtent * 0.3],
+            [west, south + latitudeExtent * 0.4],
+            [west, south + latitudeExtent * 0.5],
+            [west, south + latitudeExtent * 0.6],
+            [west, south + latitudeExtent * 0.7],
+            [west, south + latitudeExtent * 0.8],
+            [west, south + latitudeExtent * 0.9],
+            [west, north],
+          ],
         ],
-      ],
-    },
-  }
+      },
+    }
 
-  const showPoint = latitudeExtent <= MIN_DEGREES_PATH || longitudeExtent <= MIN_DEGREES_PATH
-  const showPath =
-    !showPoint && latitudeExtent <= MAX_DEGREES_PATH && longitudeExtent <= MAX_DEGREES_PATH
-  const path = geoPath().projection(projection)(viewportBoundsGeoJSON as any) || undefined
+    const showPoint = latitudeExtent <= MIN_DEGREES_PATH || longitudeExtent <= MIN_DEGREES_PATH
+    const showPath = latitudeExtent <= MAX_DEGREES_PATH && longitudeExtent <= MAX_DEGREES_PATH
+    if (showPoint) {
+      return (
+        <circle
+          className={styles.viewportPoint}
+          cx={size / 2}
+          cy={size / 2}
+          r={viewportThickness}
+        />
+      )
+    } else if (showPath) {
+      const path = geoPath().projection(projection)(viewportBoundsGeoJSON as any) || undefined
+      return (
+        <path
+          key="viewport"
+          d={path}
+          className={styles.viewport}
+          style={{ strokeWidth: viewportThickness }}
+        />
+      )
+    }
+  }, [bounds, projection, size, viewportThickness])
 
   return (
     <svg
@@ -123,28 +143,8 @@ function MiniGlobe(props: MiniglobeProps) {
       focusable="false"
     >
       <circle className={styles.globe} cx={size / 2} cy={size / 2} r={size / 2} />
-      <g className={styles.land}>
-        {worldData.map((d, i) => {
-          const path = geoPath().projection(projection)(d)
-          return path && <path key={`path-${i}`} d={path} />
-        })}
-      </g>
-      {showPoint && (
-        <circle
-          className={styles.viewportPoint}
-          cx={size / 2}
-          cy={size / 2}
-          r={viewportThickness}
-        />
-      )}
-      {showPath && (
-        <path
-          key="viewport"
-          d={path}
-          className={styles.viewport}
-          style={{ strokeWidth: viewportThickness }}
-        />
-      )}
+      {worldComponent}
+      {boundsComponent}
     </svg>
   )
 }
