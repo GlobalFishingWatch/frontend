d533e4d41b6f7b353f9782ca53bc30063f42407d
j8seangel <jseangel@gmail.com>
2020-08-19 18:34:17 +0200

    update modal confirmation action label

--- applications/amathea/src/features/dataviews/DataviewGraphPanel.tsx
+++ applications/amathea/src/features/dataviews/DataviewGraphPanel.tsx
@@ -78,14 +78,14 @@ const DataviewGraphPanel: React.FC<DataviewGraphPanelProps> = ({ dataview }) => 
             {!isUserContextLayer && unit && <span className={styles.unit}>({unit})</span>}
           </p>
           <IconButton icon="info" tooltip={dataview.description} />
-          <IconButton icon="edit" tooltip="Edit dataview" onClick={onEditClick} />
+          <IconButton icon="edit" tooltip="Edit dataset" onClick={onEditClick} />
           {!isUserContextLayer && (
             <IconButton icon="download" tooltip="Download time series data (Coming soon)" />
           )}
           <IconButton
             icon="delete"
             type="warning"
-            tooltip="Remove dataview"
+            tooltip="Remove dataset"
             loading={dataviewsStatus === 'loading.delete' && dataviewsStatusId === dataview.id}
             onClick={() => onDeleteClick(dataview)}
           />
