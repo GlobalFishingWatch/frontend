edd3ee82d85af77e18b5c7ec8691174411b0cf8b
j8seangel <jseangel@gmail.com>
2020-09-07 19:13:07 +0200

    legend ui-component

---
+++ packages/ui-components/src/map-legend/MapLegend.tsx
@@ -0,0 +1,36 @@
+import React, { memo, useMemo } from 'react'
+import cx from 'classnames'
+import { ExtendedStyle, ExtendedLayer } from '@globalfishingwatch/layer-composer/dist/types'
+import ColorRamp from './ColorRamp'
+import Bivariate from './Bivariate'
+import styles from './MapLegend.module.css'
+
+interface MapLegendProps {
+  className?: string
+  style?: ExtendedStyle
+  onClick?: (layer: ExtendedLayer, event: React.MouseEvent) => void
+}
+
+function MapLegend(props: MapLegendProps) {
+  const { className, style, onClick } = props
+  const layersWithLegend = useMemo(() => {
+    return style
+      ? style?.layers?.filter((layer) => layer.metadata?.legend !== undefined)
+      : ([] as ExtendedLayer[])
+  }, [style])
+  return (
+    <div className={cx(styles.legend, className)}>
+      {layersWithLegend?.map((layer) => {
+        if (layer.metadata?.legend?.type === 'colorramp') {
+          return <ColorRamp layer={layer} onClick={onClick} />
+        }
+        if (layer.metadata?.legend?.type === 'bivariate') {
+          return <Bivariate layer={layer} onClick={onClick} />
+        }
+        return null
+      })}
+    </div>
+  )
+}
+
+export default memo(MapLegend)
