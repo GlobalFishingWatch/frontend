a9c1c21e08adaa193609f5cce6a951ad896f7b53
j8seangel <jseangel@gmail.com>
2020-10-13 12:30:22 +0200

    datasets and dataviews slice

---
+++ applications/fishing-map/src/features/dataviews/dataviews.mock.ts
@@ -0,0 +1,88 @@
+import { Dataview } from '@globalfishingwatch/dataviews-client'
+import { Generators } from '@globalfishingwatch/layer-composer'
+import { Field } from '@globalfishingwatch/data-transforms'
+import {
+  FISHING_DATASET_ID,
+  TRACKS_DATASET_ID,
+  VESSELS_DATASET_ID,
+} from 'features/workspace/workspace.mock'
+
+const dataviews: Dataview[] = [
+  {
+    id: 1,
+    name: 'background',
+    description: '',
+    config: {
+      type: Generators.Type.Background,
+      color: '#00265c',
+    },
+  },
+  {
+    id: 2,
+    name: 'basemap',
+    description: '',
+    config: {
+      type: Generators.Type.Basemap,
+      basemap: Generators.BasemapType.Default,
+    },
+  },
+  {
+    id: 3,
+    name: 'Apparent fishing effort',
+    description:
+      'We use data about a vessel’s identity, type, location, speed, direction and more that is broadcast using the Automatic Identification System (AIS) and collected via satellites and terrestrial receivers. We analyze AIS data collected from vessels that our research has identified as known or possible commercial fishing vessels, and apply a fishing detection algorithm to determine “apparent fishing activity” based on changes in vessel speed and direction. The algorithm classifies each AIS broadcast data point for these vessels as either apparently fishing or not fishing and shows the former on our fishing activity heat map.',
+    config: {
+      type: Generators.Type.HeatmapAnimated,
+      color: '#00FFBC',
+      sublayers: [{ id: 'fishing', colorRamp: 'teal', datasets: [FISHING_DATASET_ID] }],
+      combinationMode: 'compare',
+      tilesAPI: `${process.env.REACT_APP_API_GATEWAY}/v1/4wings`,
+    },
+    datasetsConfig: [
+      {
+        // TODO use this in sublayers
+        datasetId: FISHING_DATASET_ID,
+        params: [{ id: 'type', value: 'heatmap' }],
+        endpoint: '4wings-tiles',
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: 'Default track',
+    description: 'Default track',
+    config: {
+      type: Generators.Type.Track,
+      color: 'red',
+    },
+    info: {
+      fields: ['name', 'flag'],
+    },
+    datasetsConfig: [
+      {
+        datasetId: TRACKS_DATASET_ID,
+        params: [{ id: 'vesselId', value: '' }],
+        query: [
+          // { id: 'binary', value: false },
+          { id: 'wrapLongitudes', value: false },
+          {
+            id: 'fields',
+            value: [Field.lonlat, Field.timestamp, Field.speed, Field.fishing].join(','),
+          },
+          {
+            id: 'format',
+            value: 'valueArray',
+          },
+        ],
+        endpoint: 'carriers-tracks',
+      },
+      {
+        datasetId: VESSELS_DATASET_ID,
+        params: [{ id: 'vesselId', value: '' }],
+        endpoint: 'carriers-vessels',
+      },
+    ],
+  },
+]
+
+export default dataviews
