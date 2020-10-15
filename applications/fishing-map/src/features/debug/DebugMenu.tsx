724c013b7fb2e0fbe5120a6c2016fa004aaf0de4
erik <erik@satellitestud.io>
2020-10-15 16:01:41 +0200

    copy

--- applications/fishing-map/src/features/debug/DebugMenu.tsx
+++ applications/fishing-map/src/features/debug/DebugMenu.tsx
@@ -11,11 +11,11 @@ function DebugMenu() {
     <div className={styles.row}>
       <div className={styles.header}>
         <Switch active={debugOptions.blob} onClick={() => dispatch(toggleOption('blob'))} />
-        <label htmlFor="option_blob">Render heatmap layer with blob style</label>
+        <label htmlFor="option_blob">Render fishing layer with blob style</label>
       </div>
       <p>
         Use a smoother rendering style. Only works when a single fishing layer is shown. Will
-        disbale interaction on this layer
+        disable interaction on this layer
       </p>
     </div>
   )
