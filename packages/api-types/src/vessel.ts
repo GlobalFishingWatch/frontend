c2df025cf9664299415c9c02c94a66e3be8b706b
j8seangel <jseangel@gmail.com>
2020-10-19 10:06:41 +0200

    Vessel and search api result types

---
+++ packages/api-types/src/vessel.ts
@@ -0,0 +1,10 @@
+export interface Vessel {
+  id: string
+  shipname: string
+  first_transmission_date: string
+  last_transmission_date: string
+  imo: string
+  mmsi: string
+  callsign: string
+  flag: string
+}
