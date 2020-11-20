7113bda6fba3ac30c49b6b903bdc648eb01dbb33
j8seangel <jseangel@gmail.com>
2020-11-20 16:50:56 +0100

    update layer-composer types definitions

--- packages/layer-composer/src/transforms/sort/sort.test.ts
+++ packages/layer-composer/src/transforms/sort/sort.test.ts
@@ -1,8 +1,10 @@
-import { Style } from 'mapbox-gl'
+import { Layer, Style } from 'mapbox-gl'
 import { ExtendedLayer, Group } from '../../types'
 import sort, { convertLegacyGroups } from './sort'
 
-const mock: Style = {
+type MockStyle = Omit<Style, 'layers'> & { layers: Partial<Layer>[] }
+
+const mock: MockStyle = {
   version: 8,
   layers: [
     {
@@ -13,42 +15,48 @@ const mock: Style = {
     },
     {
       id: 'fishing',
+      type: 'heatmap',
       metadata: {
         group: Group.Heatmap,
       },
     },
     {
       id: 'eez',
+      type: 'line',
       metadata: {
         'mapbox:group': 'static',
       },
     },
     {
       id: 'rfmo',
+      type: 'line',
       metadata: {
         group: Group.OutlinePolygons,
       },
     },
     {
       id: 'rulers',
+      type: 'line',
       metadata: {
         'mapbox:group': 'tools',
       },
     },
     {
       id: 'basemapLabels',
+      type: 'line',
       metadata: {
         group: Group.BasemapForeground,
       },
     },
     {
       id: 'custom',
+      type: 'line',
     },
   ],
 }
 
 test('sort according to order', async () => {
-  const sortedStyle = sort(convertLegacyGroups(mock))
+  const sortedStyle = sort(convertLegacyGroups(mock as Style))
   const sortedIds = sortedStyle.layers?.map((l: ExtendedLayer) => l.id)
 
   expect(sortedIds?.join('|')).toMatch('background|fishing|eez|rfmo|custom|basemapLabels|rulers')
