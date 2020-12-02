bec362f60a05945ef6bb30c62e64ae5ae50629d0
j8seangel <jseangel@gmail.com>
2020-12-02 12:29:49 +0100

    show text when no search results

--- applications/fishing-map/src/features/search/Search.tsx
+++ applications/fishing-map/src/features/search/Search.tsx
@@ -26,6 +26,7 @@ import {
   setFilters,
 } from './search.slice'
 import styles from './Search.module.css'
+import SearchNoResultsState from './SearchNoResultsState'
 import SearchEmptyState from './SearchEmptyState'
 import SearchFilters from './SearchFilters'
 import { useSearchFiltersConnect } from './search.hook'
@@ -80,133 +81,128 @@ function Search() {
   }
 
   return (
-    <div
-      className={cx(styles.container, {
-        [styles.expandedContainerOpen]: searchFiltersOpen,
-      })}
-    >
-      <Downshift onChange={onSelectionChange} itemToString={(item) => (item ? item.shipname : '')}>
-        {({ getInputProps, getItemProps, getMenuProps, highlightedIndex, selectedItem }) => (
-          <div className={styles.search}>
-            <div className={styles.inputContainer}>
-              <InputText
-                {...getInputProps()}
-                onChange={onInputChange}
-                value={searchQuery}
-                autoFocus
-                className={styles.input}
-                placeholder={t('vessel.search.placeholder', 'Type to search vessels')}
-              />
-              {searchStatus === 'loading' && <Spinner size="small" />}
-              <IconButton
-                icon={searchFiltersOpen || hasSearchFilters ? 'filter-on' : 'filter-off'}
-                tooltip={
-                  searchFiltersOpen
-                    ? t('vessel.search.filter_close', 'Close search filters')
-                    : t('vessel.search.filter_open', 'Open search filters')
-                }
-                className={cx(styles.expandable, {
-                  [styles.expanded]: searchFiltersOpen,
-                })}
-                onClick={() => setSearchFiltersOpen(!searchFiltersOpen)}
-                tooltipPlacement="bottom"
-              />
-              <IconButton
-                icon="close"
-                onClick={onCloseClick}
-                type="border"
-                tooltip={t('vessel.search.close', 'Close search')}
-                tooltipPlacement="bottom"
-              />
-            </div>
-            <SearchFilters
-              className={cx(styles.expandedContainer, {
-                [styles.expandedContainerOpen]: searchFiltersOpen,
-              })}
+    <Downshift onChange={onSelectionChange} itemToString={(item) => (item ? item.shipname : '')}>
+      {({ getInputProps, getItemProps, getMenuProps, highlightedIndex, selectedItem }) => (
+        <div className={cx(styles.search, { [styles.expandedContainerOpen]: searchFiltersOpen })}>
+          <div className={styles.inputContainer}>
+            <InputText
+              {...getInputProps()}
+              onChange={onInputChange}
+              value={searchQuery}
+              autoFocus
+              className={styles.input}
+              placeholder={t('vessel.search.placeholder', 'Type to search vessels')}
             />
-            {searchResults && (
-              <ul {...getMenuProps()} className={styles.searchResults}>
-                {searchResults?.map((entry, index: number) => {
-                  const {
-                    id,
-                    shipname,
-                    flag,
-                    fleet,
-                    mmsi,
-                    imo,
-                    callsign,
-                    dataset,
-                    firstTransmissionDate,
-                    lastTransmissionDate,
-                  } = entry
-                  const flagLabel = getFlagById(flag)?.label
-                  return (
-                    <li
-                      {...getItemProps({ item: entry, index })}
-                      className={cx(styles.searchResult, {
-                        [styles.highlighted]: highlightedIndex === index,
-                      })}
-                      key={id}
-                    >
-                      <div className={styles.name}>{shipname || '---'}</div>
-                      <div className={styles.properties}>
-                        {flagLabel && (
-                          <div className={styles.property}>
-                            <label>{t('vessel.flag', 'Flag')}</label>
-                            <span>{flagLabel}</span>
-                          </div>
-                        )}
-                        {mmsi && (
-                          <div className={styles.property}>
-                            <label>{t('vessel.mmsi', 'MMSI')}</label>
-                            <span>{mmsi}</span>
-                          </div>
-                        )}
-                        {imo && (
-                          <div className={styles.property}>
-                            <label>{t('vessel.imo', 'IMO')}</label>
-                            <span>{imo}</span>
-                          </div>
-                        )}
-                        {callsign && (
-                          <div className={styles.property}>
-                            <label>{t('vessel.callsign', 'Callsign')}</label>
-                            <span>{callsign}</span>
-                          </div>
-                        )}
-                        {fleet && (
-                          <div className={styles.property}>
-                            <label>{t('vessel.fleet', 'Fleet')}</label>
-                            <span>{formatInfoField(fleet, 'fleet')}</span>
-                          </div>
-                        )}
-                        {firstTransmissionDate && lastTransmissionDate && (
-                          <div className={styles.property}>
-                            <label>{t('vessel.transmission_plural', 'Transmissions')}</label>
-                            <span>
-                              from <I18nDate date={firstTransmissionDate} /> to{' '}
-                              <I18nDate date={lastTransmissionDate} />
-                            </span>
-                          </div>
-                        )}
-
-                        {dataset?.name && (
-                          <div className={styles.property}>
-                            <label>{t('vessel.source', 'Source')}</label>
-                            <span>{dataset.name}</span>
-                          </div>
-                        )}
-                      </div>
-                    </li>
-                  )
-                })}
-              </ul>
-            )}
-            {!searchResults && searchStatus !== AsyncReducerStatus.Loading && <SearchEmptyState />}
+            {searchStatus === 'loading' && <Spinner size="small" />}
+            <IconButton
+              icon={searchFiltersOpen || hasSearchFilters ? 'filter-on' : 'filter-off'}
+              tooltip={
+                searchFiltersOpen
+                  ? t('vessel.search.filter_close', 'Close search filters')
+                  : t('vessel.search.filter_open', 'Open search filters')
+              }
+              className={cx(styles.expandable, {
+                [styles.expanded]: searchFiltersOpen,
+              })}
+              onClick={() => setSearchFiltersOpen(!searchFiltersOpen)}
+              tooltipPlacement="bottom"
+            />
+            <IconButton
+              icon="close"
+              onClick={onCloseClick}
+              type="border"
+              tooltip={t('vessel.search.close', 'Close search')}
+              tooltipPlacement="bottom"
+            />
           </div>
-        )}
-      </Downshift>
-    </div>
+          <SearchFilters className={cx(styles.expandedContainer)} />
+          {!searchResults?.length &&
+            (searchStatus === AsyncReducerStatus.Finished ? (
+              <SearchNoResultsState />
+            ) : (
+              <SearchEmptyState />
+            ))}
+          {searchResults && searchResults.length > 0 && (
+            <ul {...getMenuProps()} className={styles.searchResults}>
+              {searchResults?.map((entry, index: number) => {
+                const {
+                  id,
+                  shipname,
+                  flag,
+                  fleet,
+                  mmsi,
+                  imo,
+                  callsign,
+                  dataset,
+                  firstTransmissionDate,
+                  lastTransmissionDate,
+                } = entry
+                const flagLabel = getFlagById(flag)?.label
+                return (
+                  <li
+                    {...getItemProps({ item: entry, index })}
+                    className={cx(styles.searchResult, {
+                      [styles.highlighted]: highlightedIndex === index,
+                    })}
+                    key={id}
+                  >
+                    <div className={styles.name}>{shipname || '---'}</div>
+                    <div className={styles.properties}>
+                      {flagLabel && (
+                        <div className={styles.property}>
+                          <label>{t('vessel.flag', 'Flag')}</label>
+                          <span>{flagLabel}</span>
+                        </div>
+                      )}
+                      {mmsi && (
+                        <div className={styles.property}>
+                          <label>{t('vessel.mmsi', 'MMSI')}</label>
+                          <span>{mmsi}</span>
+                        </div>
+                      )}
+                      {imo && (
+                        <div className={styles.property}>
+                          <label>{t('vessel.imo', 'IMO')}</label>
+                          <span>{imo}</span>
+                        </div>
+                      )}
+                      {callsign && (
+                        <div className={styles.property}>
+                          <label>{t('vessel.callsign', 'Callsign')}</label>
+                          <span>{callsign}</span>
+                        </div>
+                      )}
+                      {fleet && (
+                        <div className={styles.property}>
+                          <label>{t('vessel.fleet', 'Fleet')}</label>
+                          <span>{formatInfoField(fleet, 'fleet')}</span>
+                        </div>
+                      )}
+                      {firstTransmissionDate && lastTransmissionDate && (
+                        <div className={styles.property}>
+                          <label>{t('vessel.transmission_plural', 'Transmissions')}</label>
+                          <span>
+                            from <I18nDate date={firstTransmissionDate} /> to{' '}
+                            <I18nDate date={lastTransmissionDate} />
+                          </span>
+                        </div>
+                      )}
+
+                      {dataset?.name && (
+                        <div className={styles.property}>
+                          <label>{t('vessel.source', 'Source')}</label>
+                          <span>{dataset.name}</span>
+                        </div>
+                      )}
+                    </div>
+                  </li>
+                )
+              })}
+            </ul>
+          )}
+        </div>
+      )}
+    </Downshift>
   )
 }
 
---
+++ applications/fishing-map/src/features/search/SearchNoResultsState.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import vesselImage from 'assets/images/vessel-side@2x.png'
+import styles from './SearchNoResultsState.module.css'
+
+function SearchNoResultsState() {
+  const { t } = useTranslation()
+  return (
+    <div className={styles.emptyState}>
+      <div>
+        <img src={vesselImage} alt="vessel" className={styles.vesselImage} />
+        <p>
+          {t(
+            'vessel.search.no_results',
+            "Can't find the vessel you are looking for? Try using MMSI, IMO or IRCS"
+          )}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default SearchNoResultsState
