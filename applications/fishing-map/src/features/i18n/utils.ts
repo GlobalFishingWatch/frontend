ad6d72f8769d0821b123e9eb8af09e65ad91ed17
j8seangel <jseangel@gmail.com>
2020-12-15 11:55:20 +0100

    missing translations

--- applications/fishing-map/src/features/i18n/utils.ts
+++ applications/fishing-map/src/features/i18n/utils.ts
@@ -4,7 +4,7 @@ import i18n from './i18n'
 export const getPlaceholderBySelections = (selections?: MultiSelectOption[]): string => {
   if (!selections?.length) return i18n.t('selects.placeholder', 'Select an option')
   return selections.length > 1
-    ? `${selections.length} ${i18n.t('selects.selection', 'selected')}`
+    ? `${selections.length} ${i18n.t('selects.selected', 'selected')}`
     : selections[0].label
 }
 
