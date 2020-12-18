5b029343d6eb99f8ae1d3e4f6083936a1ad27daa
j8seangel <jseangel@gmail.com>
2020-12-18 19:06:16 +0100

    fix typescript switch error

--- packages/ui-components/src/switch/index.ts
+++ packages/ui-components/src/switch/index.ts
@@ -1,6 +1,7 @@
+import { MouseEvent } from 'react'
 export { default } from './Switch'
 
 // TODO Maybe a simple way is to have the Switch component wrap an <input type="checkbox"> so that we can use the React native event
 export interface SwitchEvent extends MouseEvent {
-    active: boolean
-  }
+  active: boolean
+}
