1bf469ec687d59a7a18271f84e3edf31658f19c9
erik <erik@satellitestud.io>
2021-01-15 13:33:39 +0100

    mirrored stream

--- packages/timebar/src/charts/stacked-activity.js
+++ packages/timebar/src/charts/stacked-activity.js
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo } from 'react'
+import React, { Fragment, useContext, useMemo } from 'react'
 import PropTypes from 'prop-types'
 import { area, stack, curveStepAfter } from 'd3-shape'
 import { scaleLinear } from 'd3-scale'
@@ -6,6 +6,8 @@ import { max } from 'd3-array'
 import ImmediateContext from '../immediateContext'
 import { DEFAULT_CSS_TRANSITION } from '../constants'
 import { TimelineContext } from '../components/timeline'
+
+const MARGIN_BOTTOM = 10
 
 const getPathContainers = (data, graphHeight, overallScale, numSublayers) => {
   if (!data) return []
@@ -16,17 +18,10 @@ const getPathContainers = (data, graphHeight, overallScale, numSublayers) => {
 
   const y = scaleLinear()
     .domain([0, max(series, (d) => max(d, (d) => d[1]))])
-    .nice()
-    .range([10, graphHeight - 10])
+    .range([0, graphHeight / 2 - MARGIN_BOTTOM / 2])
 
   const areaLayout = area()
-    .x((d) => {
-      if (!d.data) {
-        console.log(d)
-        return
-      }
-      return overallScale(d.data.date)
-    })
+    .x((d) => overallScale(d.data.date))
     .y0((d) => y(d[0]))
     .y1((d) => y(d[1]))
     .curve(curveStepAfter)
@@ -48,6 +43,8 @@ const StackedActivity = ({ data, colors, numSublayers }) => {
     return getPathContainers(data, graphHeight, overallScale, numSublayers)
   }, [data, graphHeight, overallScale, numSublayers])
 
+  const middleY = graphHeight / 2 - MARGIN_BOTTOM / 2
+
   return (
     <svg width={outerWidth} height={graphHeight}>
       <g
@@ -57,12 +54,14 @@ const StackedActivity = ({ data, colors, numSublayers }) => {
         }}
       >
         {pathContainers.map((pathContainer, sublayerIndex) => (
-          <path
-            key={sublayerIndex}
-            d={pathContainer.path}
-            fill={colors ? colors[sublayerIndex] : '#ff00ff'}
-            // fillOpacity={opacity}
-          />
+          <Fragment key={sublayerIndex}>
+            <g key="top" transform={`scale(1, -1) translate(0, ${-middleY}) `}>
+              <path d={pathContainer.path} fill={colors ? colors[sublayerIndex] : '#ff00ff'} />
+            </g>
+            <g key="bottom" transform={`translate(0, ${middleY})`}>
+              <path d={pathContainer.path} fill={colors ? colors[sublayerIndex] : '#ff00ff'} />
+            </g>
+          </Fragment>
         ))}
       </g>
     </svg>
