41908e297a6d31275a2ccb93e20c7a5acdc913fc
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2021-01-27 18:13:44 -0300

    Cleanup splash

--- applications/vessel-history/src/components/Splash.tsx
+++ applications/vessel-history/src/components/Splash.tsx
@@ -3,33 +3,15 @@ import GFWAPI from '@globalfishingwatch/api-client'
 import useGFWLogin from '@globalfishingwatch/react-hooks/dist/use-login'
 import styles from './Splash.module.css'
 
-// export default function Splash(WrappedComponent: React.ReactElement): React.ReactElement {
-//   return (props: any) => {
-//     const { loading } = useGFWLogin(GFWAPI)
-
-//     return (
-//       <React.Fragment>
-
-//         {loading && (<div className={styles.splashContainer}></div>)}
-//         {!loading && (<WrappedComponent {...props} />)}
-//       </React.Fragment>
-//     )
-//   }
-// }
-
-function Splash() {
-  return function <T>(
-    Component: React.ComponentType<T>
-  ) {
-    return function (props: T): JSX.Element {
-      const { loading } = useGFWLogin(GFWAPI)
-      return (
-        <React.Fragment>
-          {loading && <div className={styles.splashContainer}></div>}
-          {!loading && <Component {...props} />}
-        </React.Fragment>
-      );
-    };
+function Splash(Component: React.ComponentType) {
+  return function (props: React.ComponentProps<typeof Component>): JSX.Element {
+    const { loading } = useGFWLogin(GFWAPI)
+    return (
+      <React.Fragment>
+        {loading && <div className={styles.splashContainer}></div>}
+        {!loading && <Component {...props} />}
+      </React.Fragment>
+    );
   };
 }
 export default Splash
