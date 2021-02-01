5ecb9dfe6c22350b9346ee5e21495a554ed0b293
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2021-02-01 13:42:48 -0300

    Removed splash timeout

--- applications/vessel-history/src/App.tsx
+++ applications/vessel-history/src/App.tsx
@@ -3,11 +3,10 @@ import { useSelector } from 'react-redux'
 // import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom'
 import GFWAPI from '@globalfishingwatch/api-client'
 import useGFWLogin from '@globalfishingwatch/react-hooks/dist/use-login'
-import useDebounce from '@globalfishingwatch/react-hooks/dist/use-debounce'
 import Home from 'features/home/Home'
 import { getLocationType } from 'routes/routes.selectors'
 import { LOGIN, PROFILE } from 'routes/routes'
-import { BASE_URL, SPLASH_TIMEOUT } from 'data/constants'
+import { BASE_URL } from 'data/constants'
 import Profile from 'features/profile/Profile'
 import Splash from 'components/Splash'
 import './App.css'
@@ -21,12 +20,10 @@ function App() {
     window.location.href = GFWAPI.getLoginUrl(location)
   }
 
-  // Splash screen is shown at least one second
-  const debouncedLoading = useDebounce<boolean>(loading, SPLASH_TIMEOUT)
   //const search = useSelector(getLocationSearch)
   const locationType = useSelector(getLocationType)
 
-  if (debouncedLoading) {
+  if (loading) {
     return <Splash />
   }
   if (locationType === PROFILE) {
