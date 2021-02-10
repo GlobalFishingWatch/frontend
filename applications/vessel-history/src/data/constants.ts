16c21a1d87e99d211d16d9f70231f841d7bb5f3d
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2021-02-10 14:11:22 -0300

    Changed to use REACT_APP_WORKSPACE_ENV in VH to show or not the SOURCE

--- applications/vessel-history/src/data/constants.ts
+++ applications/vessel-history/src/data/constants.ts
@@ -1,5 +1,6 @@
-export const BASE_URL = process.env.NODE_ENV === 'production' ? '/port-inspector' : ''
+export const BASE_URL = process.env.NODE_ENV === 'production' ? '/vessel-history' : ''
 
 export const SPLASH_TIMEOUT = 1000
-console.log(['NODE_ENV:', process.env.NODE_ENV])
-export const SHOW_VESSEL_API_SOURCE = process.env.NODE_ENV === 'production' ? false : true
+console.log(['REACT_APP_WORKSPACE_ENV:', process.env.REACT_APP_WORKSPACE_ENV])
+export const SHOW_VESSEL_API_SOURCE =
+  process.env.REACT_APP_WORKSPACE_ENV === 'production' ? false : true
