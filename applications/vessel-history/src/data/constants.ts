9e40c3bd0eef3395e943c2b960da2e81fa02a2eb
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2021-02-10 13:55:23 -0300

    Debug node_env env var in VH

--- applications/vessel-history/src/data/constants.ts
+++ applications/vessel-history/src/data/constants.ts
@@ -1,4 +1,5 @@
 export const BASE_URL = process.env.NODE_ENV === 'production' ? '/port-inspector' : ''
 
 export const SPLASH_TIMEOUT = 1000
+console.log(['NODE_ENV:', process.env.NODE_ENV])
 export const SHOW_VESSEL_API_SOURCE = process.env.NODE_ENV === 'production' ? false : true
