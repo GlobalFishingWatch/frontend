714a2b3feaa7cd9c6366546d570b54359bd24d03
satellitestudiodesign <juancarlos@satellitestud.io>
2021-02-11 15:40:21 +0100

    remove unnecesary const

--- packages/timebar/src/charts/utils/index.js
+++ packages/timebar/src/charts/utils/index.js
@@ -1,7 +1,6 @@
 export const getTrackY = (numTracks, trackIndex, graphHeight) => {
-  const MARGIN_BOTTOM = 10
-  const MARGIN_TOP = 5
-  const finalHeight = graphHeight - MARGIN_BOTTOM - MARGIN_TOP
+  const TOTAL_MARGIN = 15
+  const finalHeight = graphHeight - TOTAL_MARGIN
   const heightPerTrack = finalHeight / numTracks
   const y = heightPerTrack * trackIndex + heightPerTrack / 2
   return y
