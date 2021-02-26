6a7b80c3d0433e02cf1614d22af3301d81da91e4
satellitestudiodesign <juancarlos@satellitestud.io>
2021-02-26 13:05:37 +0100

    aloow users to edit and delete workspaces

--- applications/fishing-map/src/features/user/UserWorkspaces.tsx
+++ applications/fishing-map/src/features/user/UserWorkspaces.tsx
@@ -1,22 +1,58 @@
 import Link from 'redux-first-router-link'
 import { useTranslation } from 'react-i18next'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { useCallback } from 'react'
 import Button from '@globalfishingwatch/ui-components/dist/button'
 import IconButton from '@globalfishingwatch/ui-components/dist/icon-button'
 import Spinner from '@globalfishingwatch/ui-components/dist/spinner'
+import { Workspace } from '@globalfishingwatch/api-types/dist'
 import { WORKSPACE } from 'routes/routes'
 import { WorkspaceCategories } from 'data/workspaces'
-import { selectWorkspaceListStatus } from 'features/workspaces-list/workspaces-list.slice'
+import {
+  fetchWorkspacesThunk,
+  selectWorkspaceListStatus,
+} from 'features/workspaces-list/workspaces-list.slice'
 import { AsyncReducerStatus } from 'utils/async-slice'
+import { deleteWorkspaceThunk, updateWorkspaceNameThunk } from 'features/workspace/workspace.slice'
 import styles from './User.module.css'
 import { selectUserWorkspaces } from './user.selectors'
+import { selectUserData } from './user.slice'
 
 function UserWorkspaces() {
   const { t } = useTranslation()
+  const dispatch = useDispatch()
+  const userData = useSelector(selectUserData)
   const workspaces = useSelector(selectUserWorkspaces)
   const workspacesStatus = useSelector(selectWorkspaceListStatus)
 
   const loading = workspacesStatus === AsyncReducerStatus.Loading
+
+  const onEditClick = useCallback(
+    async (workspace: Workspace) => {
+      const name = prompt(t('workspace.nameInput', 'Workspace name'), workspace.name)
+      if (name) {
+        await dispatch(updateWorkspaceNameThunk({ id: workspace.id, name }))
+        dispatch(fetchWorkspacesThunk({ userId: userData?.id }))
+      }
+    },
+    [dispatch, t]
+  )
+
+  const onDeleteClick = useCallback(
+    async (workspace: Workspace) => {
+      const confirmation = window.confirm(
+        `${t(
+          'workspace.confirmRemove',
+          'Are you sure you want to permanently delete this workspace?'
+        )}\n${workspace.name}`
+      )
+      if (confirmation) {
+        await dispatch(deleteWorkspaceThunk(workspace.id))
+        dispatch(fetchWorkspacesThunk({ userId: userData?.id }))
+      }
+    },
+    [dispatch, t]
+  )
 
   return (
     <div className={styles.views}>
@@ -50,6 +86,17 @@ function UserWorkspaces() {
                     <span className={styles.workspaceTitle}>{workspace.name}</span>
                     <IconButton icon="arrow-right" />
                   </Link>
+                  <IconButton
+                    icon="edit"
+                    tooltip={t('workspace.editName', 'Edit workspace name')}
+                    onClick={() => onEditClick(workspace)}
+                  />
+                  <IconButton
+                    icon="delete"
+                    type="warning"
+                    tooltip={t('workspace.remove', 'Remove workspace')}
+                    onClick={() => onDeleteClick(workspace)}
+                  />
                 </li>
               )
             })
