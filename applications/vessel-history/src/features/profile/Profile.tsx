958ff0c66dfb2bca55e0113f2273d888b543925c
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2021-03-03 15:04:17 -0300

    Implemented Back Button

--- applications/vessel-history/src/features/profile/Profile.tsx
+++ applications/vessel-history/src/features/profile/Profile.tsx
@@ -1,14 +1,18 @@
 import React, { Fragment, useState } from 'react'
 import { useSelector } from 'react-redux'
+import Link from 'redux-first-router-link'
 import { IconButton, Tabs } from '@globalfishingwatch/ui-components'
 import { Tab } from '@globalfishingwatch/ui-components/dist/tabs'
 import { getVesselInfo } from 'features/vessels/vessels.selectors'
+import { HOME } from 'routes/routes'
+import { selectQueryParam } from 'routes/routes.selectors'
 import Info from './components/Info'
 import styles from './Profile.module.css'
 
 const Profile: React.FC = (props): React.ReactElement => {
   const [lastPortVisit, setLastPortVisit] = useState({ label: '', coordinates: null })
   const [lastPosition, setLastPosition] = useState(null)
+  const q = useSelector(selectQueryParam('q'))
 
   const vessel = useSelector(getVesselInfo)
   const tabs: Tab[] = [
@@ -33,15 +37,14 @@ const Profile: React.FC = (props): React.ReactElement => {
   return (
     <Fragment>
       <header className={styles.header}>
-        <IconButton
-          type="border"
-          size="default"
-          icon="arrow-left"
-          className={styles.backButton}
-          onClick={async () => {
-            return
-          }}
-        ></IconButton>
+        <Link to={{ type: HOME, replaceQuery: true, query: { q } }}>
+          <IconButton
+            type="border"
+            size="default"
+            icon="arrow-left"
+            className={styles.backButton}
+          />
+        </Link>
         {vessel && (
           <h1>
             {vessel.getName().value?.data}
