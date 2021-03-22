36eabecae4ce0692be4f1c7dd1bc3cf52048cb86
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2021-03-22 17:55:49 -0300

    Fix field history styles

--- applications/vessel-history/src/features/profile/components/InfoFieldHistory.tsx
+++ applications/vessel-history/src/features/profile/components/InfoFieldHistory.tsx
@@ -2,6 +2,7 @@ import React, { Fragment, useCallback, useState } from 'react'
 import { DateTime } from 'luxon'
 import { Modal } from '@globalfishingwatch/ui-components'
 // eslint-disable-next-line import/order
+import { useTranslation } from 'utils/i18n'
 import { ValueItem } from 'types'
 import styles from './Info.module.css'
 
@@ -20,6 +21,7 @@ const InfoFieldHistory: React.FC<ListItemProps> = ({
   label,
   vesselName,
 }): React.ReactElement => {
+  const { t } = useTranslation()
   const [modalOpen, setModalOpen] = useState(isOpen)
   const toggleModalOpen = useCallback(() => {
     setModalOpen(!modalOpen)
@@ -39,9 +41,10 @@ const InfoFieldHistory: React.FC<ListItemProps> = ({
 
   return (
     <Fragment>
-      <button className={styles.moreValues} onClick={toggleModalOpen}>{`+${
-        history.length + 1
-      } previous ${label}s`}</button>
+      <button className={styles.moreValues} onClick={toggleModalOpen}>{`+${history.length} ${t(
+        'common.previous',
+        'previous'
+      )} ${t(`common.${label}Plural`, `${label}s`)}`}</button>
 
       {history.length && (
         <Modal
@@ -52,30 +55,37 @@ const InfoFieldHistory: React.FC<ListItemProps> = ({
           }}
         >
           <div>
-            <div className={styles.identifierField}>
-              <label>{label}</label>
-              <div>{current.value}</div>
-            </div>
-            <div className={styles.identifierField}>
-              <label>CURRENT TIME RANGE</label>
-              <div>Since {formatedDate(current.firstSeen)}</div>
+            <div className={styles.historyItem}>
+              <div className={styles.identifierField}>
+                <label>{label}</label>
+                <div>{current.value}</div>
+              </div>
+              <div className={styles.identifierField}>
+                <label>{t('common.currentTimeRange', 'CURRENT TIME RANGE')}</label>
+                <div>
+                  {t('common.since', 'Since')}{' '}
+                  {formatedDate(current.firstSeen ?? history.slice(0, 1)?.shift()?.endDate)}
+                </div>
+              </div>
             </div>
             {history.map((historyValue: ValueItem, index) => (
-              <Fragment key={index}>
+              <div className={styles.historyItem} key={index}>
                 <div className={styles.identifierField}>
                   <label>{label}</label>
                   <div>{historyValue.value}</div>
                 </div>
                 <div className={styles.identifierField}>
-                  <label>Time Range</label>
+                  <label>{t('common.timeRange', 'Time Range')}</label>
                   <div>
                     {historyValue.firstSeen && (
-                      <div>From {formatedDate(historyValue.firstSeen)}</div>
+                      <div>
+                        {t('common.from', 'From')} {formatedDate(historyValue.firstSeen)}
+                      </div>
                     )}
                     {historyValue.endDate && <div>To {formatedDate(historyValue.endDate)}</div>}
                   </div>
                 </div>
-              </Fragment>
+              </div>
             ))}
           </div>
         </Modal>
