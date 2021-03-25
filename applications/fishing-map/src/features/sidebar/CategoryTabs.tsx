822e9be2ad412bbb551d6de2028f719ad2c695eb
satellitestudiodesign <juancarlos@satellitestud.io>
2021-03-25 16:48:01 +0100

    adds feedback and help buttons

--- applications/fishing-map/src/features/sidebar/CategoryTabs.tsx
+++ applications/fishing-map/src/features/sidebar/CategoryTabs.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import Link from 'redux-first-router-link'
 import { useTranslation } from 'react-i18next'
 import cx from 'classnames'
@@ -14,6 +14,8 @@ import { selectUserData } from 'features/user/user.slice'
 import { isGuestUser } from 'features/user/user.selectors'
 import { LocaleLabels } from 'features/i18n/i18n'
 import { selectAvailableWorkspacesCategories } from 'features/workspaces-list/workspaces-list.selectors'
+import HelpModal from 'features/help/HelpModal'
+import FeedbackModal from 'features/feedback/FeedbackModal'
 import styles from './CategoryTabs.module.css'
 
 const DEFAULT_WORKSPACE_LIST_VIEWPORT = {
@@ -52,75 +54,99 @@ function CategoryTabs({ onMenuClick }: CategoryTabsProps) {
     i18n.changeLanguage(lang)
   }
 
+  const [modalHelpOpen, setModalHelpOpen] = useState(false)
+  const [modalFeedbackOpen, setModalFeedbackOpen] = useState(false)
+
+  const onHelpClick = () => {
+    setModalHelpOpen(true)
+  }
+  const onFeedbackClick = () => {
+    setModalFeedbackOpen(true)
+  }
+
   return (
-    <ul className={cx('print-hidden', styles.CategoryTabs)}>
-      <li className={styles.tab} onClick={onMenuClick}>
-        <span className={styles.tabContent}>
-          <Icon icon="menu" />
-        </span>
-      </li>
-      {availableCategories?.map((category, index) => (
+    <Fragment>
+      <ul className={cx('print-hidden', styles.CategoryTabs)}>
+        <li className={styles.tab} onClick={onMenuClick}>
+          <span className={styles.tabContent}>
+            <Icon icon="menu" />
+          </span>
+        </li>
+        {availableCategories?.map((category, index) => (
+          <li
+            key={category}
+            className={cx(styles.tab, {
+              [styles.current]:
+                locationCategory === (category as WorkspaceCategories) ||
+                (index === 0 && locationType === HOME),
+            })}
+          >
+            <Link
+              className={styles.tabContent}
+              to={getLinkToCategory(category as WorkspaceCategories)}
+            >
+              <Icon icon={`category-${category}` as IconType} />
+            </Link>
+          </li>
+        ))}
+        <div className={styles.separator}></div>
+        <li className={cx(styles.tab, styles.secondary)}>
+          <button className={styles.tabContent} onClick={onHelpClick}>
+            <Icon icon="help" />
+          </button>
+        </li>
+        <li className={cx(styles.tab, styles.secondary)}>
+          <button className={styles.tabContent} onClick={onFeedbackClick}>
+            <Icon icon="feedback" />
+          </button>
+        </li>
+        <li className={cx(styles.tab, styles.languageToggle)}>
+          <button className={styles.tabContent}>
+            <Icon icon="language" />
+          </button>
+          <ul className={styles.languages}>
+            {LocaleLabels.map(({ id, label }) => (
+              <li key={id}>
+                <button
+                  onClick={() => toggleLanguage(id)}
+                  className={cx(styles.language, {
+                    [styles.currentLanguage]: i18n.language === id,
+                  })}
+                >
+                  {label}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </li>
         <li
-          key={category}
           className={cx(styles.tab, {
-            [styles.current]:
-              locationCategory === (category as WorkspaceCategories) ||
-              (index === 0 && locationType === HOME),
+            [styles.current]: locationType === USER,
           })}
         >
-          <Link
-            className={styles.tabContent}
-            to={getLinkToCategory(category as WorkspaceCategories)}
-          >
-            <Icon icon={`category-${category}` as IconType} />
-          </Link>
+          {guestUser ? (
+            <Tooltip content={t('common.login', 'Log in')}>
+              <a href={GFWAPI.getLoginUrl(window.location.toString())}>
+                <Icon icon="user" />
+              </a>
+            </Tooltip>
+          ) : (
+            <Link
+              to={{
+                type: USER,
+                payload: {},
+                query: { ...DEFAULT_WORKSPACE_LIST_VIEWPORT },
+                replaceQuery: true,
+              }}
+            >
+              {userData ? initials : <Icon icon="user" className="print-hidden" />}
+            </Link>
+          )}
         </li>
-      ))}
-      <div className={styles.separator}></div>
-      <li className={cx(styles.tab, styles.languageToggle)}>
-        <button className={styles.tabContent}>
-          <Icon icon="language" />
-        </button>
-        <ul className={styles.languages}>
-          {LocaleLabels.map(({ id, label }) => (
-            <li key={id}>
-              <button
-                onClick={() => toggleLanguage(id)}
-                className={cx(styles.language, {
-                  [styles.currentLanguage]: i18n.language === id,
-                })}
-              >
-                {label}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </li>
-      <li
-        className={cx(styles.tab, {
-          [styles.current]: locationType === USER,
-        })}
-      >
-        {guestUser ? (
-          <Tooltip content={t('common.login', 'Log in')}>
-            <a href={GFWAPI.getLoginUrl(window.location.toString())}>
-              <Icon icon="user" />
-            </a>
-          </Tooltip>
-        ) : (
-          <Link
-            to={{
-              type: USER,
-              payload: {},
-              query: { ...DEFAULT_WORKSPACE_LIST_VIEWPORT },
-              replaceQuery: true,
-            }}
-          >
-            {userData ? initials : <Icon icon="user" className="print-hidden" />}
-          </Link>
-        )}
-      </li>
-    </ul>
+      </ul>
+      <HelpModal isOpen={modalHelpOpen} onClose={() => setModalHelpOpen(false)} />
+      <FeedbackModal isOpen={modalFeedbackOpen} onClose={() => setModalFeedbackOpen(false)} />
+    </Fragment>
   )
 }
 
