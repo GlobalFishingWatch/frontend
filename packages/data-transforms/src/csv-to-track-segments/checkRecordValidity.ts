791964c5816988bad4fb52530af6c81e801d0a75
erik <erik@satellitestud.io>
2021-04-15 10:54:26 +0200

    check validity

---
+++ packages/data-transforms/src/csv-to-track-segments/checkRecordValidity.ts
@@ -0,0 +1,27 @@
+type Args = {
+  record: Record<string, any>
+  latitude: string
+  longitude: string
+  timestamp: string
+}
+
+const checkRecordValidity = ({ record, latitude, longitude, timestamp }: Args) => {
+  const errors = []
+  if (Number.isNaN(parseInt(record[latitude])) || record[latitude] > 90 || record[latitude] < -90) {
+    errors.push('latitude')
+  }
+  if (
+    Number.isNaN(parseInt(record[longitude])) ||
+    record[longitude] > 180 ||
+    record[longitude] < -180
+  ) {
+    errors.push('longitude')
+  }
+  const d = new Date(record[timestamp])
+  if (!(d instanceof Date) || isNaN(d.getTime())) {
+    errors.push('timestamp')
+  }
+  return errors
+}
+
+export default checkRecordValidity
