919a7ad38bcc5a915c25afd34dde5a7d4c192da4
j8seangel <jseangel@gmail.com>
2021-04-20 18:49:58 +0200

    update linting dependency

--- applications/tile-inspector/.eslintrc.js
+++ applications/tile-inspector/.eslintrc.js
@@ -1,11 +1,1 @@
-module.exports = {
-  ...require('@globalfishingwatch/linting/typescript'),
-  // TODO couldn't disable prettier so I copied & pasted this from the linting package
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'react-app',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-  ],
-}
+module.exports = require('@globalfishingwatch/linting/typescript')
