1c33a19a7d8a22491752a802c828c469b2afb96d
j8seangel <jseangel@gmail.com>
2021-04-29 20:45:46 +0200

    fix ramps

--- packages/layer-composer/src/generators/heatmap/modes/blob.ts
+++ packages/layer-composer/src/generators/heatmap/modes/blob.ts
@@ -52,7 +52,7 @@ const blob = (
 
   const paint = { ...BASE_PAINT }
   paint['heatmap-weight'] = exprPick as any
-  const hStops = [0, 0.002, 0.004, 0.01, 0.05, 0.1, 0.2, 1]
+  const hStops = [0, 0.002, 0.004, 0.01, 0.05, 0.07, 0.1, 0.15, 0.2, 1]
   const heatmapColorRamp = zip(hStops, colorRamp).flat()
   paint['heatmap-color'] = [
     'interpolate',
