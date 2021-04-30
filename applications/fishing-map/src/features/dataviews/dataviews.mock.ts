394c8fbbf4244dce91e3b05c9db1088eaebafd89
j8seangel <jseangel@gmail.com>
2021-04-30 11:14:22 +0200

    clean dataviews mock

--- applications/fishing-map/src/features/dataviews/dataviews.mock.ts
+++ applications/fishing-map/src/features/dataviews/dataviews.mock.ts
@@ -1,18 +1,5 @@
-import { Dataview, DataviewCategory } from '@globalfishingwatch/api-types/dist'
+import { Dataview } from '@globalfishingwatch/api-types/dist'
 
-export const dataviews: Dataview[] = [
-  {
-    id: 999,
-    name: 'User track',
-    description: 'User track',
-    app: 'fishing-map',
-    config: {
-      type: 'TRACK',
-      color: '#F95E5E',
-    },
-    category: DataviewCategory.Environment,
-    datasetsConfig: [],
-  },
-]
+export const dataviews: Dataview[] = []
 
 export default dataviews
