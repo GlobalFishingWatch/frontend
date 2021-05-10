0dd15a87d4c8ebcedb6ade483a28129a0879040a
j8seangel <jseangel@gmail.com>
2021-05-10 11:30:33 +0200

    fix context metadata order

--- packages/layer-composer/src/types/index.ts
+++ packages/layer-composer/src/types/index.ts
@@ -45,6 +45,7 @@ export enum Group {
   Heatmap = 'heatmap', // Fill/gradient-based heatmaps
   BasemapFill = 'basemapFill', // Landmass
   OutlinePolygons = 'outlinePolygons', // Context layers with an outlined/hollow style such as EEZ, RFMOs, etc
+  OutlinePolygonsFill = 'outlinePolygonsFill', // User context layers with a filled styles, below OutlinePolygons
   OutlinePolygonsBackground = 'OutlinePolygonsBackground', // Polygons  that need to be rendered below landmass
   OutlinePolygonsHighlighted = 'outlinePolygonsHighlighted', // Context layers with selected features
   Default = 'default', // Default stack position when group is not specified
