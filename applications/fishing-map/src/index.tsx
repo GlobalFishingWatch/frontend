f6ea0e062de8eca92a48f3479d78c1ca5c81f9bd
Erik Escoffier <erik@satellitestud.io>
2021-05-12 12:05:50 +0200

    add basic error boundary

--- applications/fishing-map/src/index.tsx
+++ applications/fishing-map/src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component, ErrorInfo, Fragment } from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { RecoilRoot } from 'recoil'
@@ -8,6 +8,7 @@ import store from './store'
 import './features/i18n/i18n'
 import '@globalfishingwatch/ui-components/dist/base.css'
 
+
 async function loadPolyfills() {
   if (typeof window.IntersectionObserver === 'undefined') {
     await import('intersection-observer')
@@ -16,13 +17,45 @@ async function loadPolyfills() {
 
 loadPolyfills()
 
+class ErrorBoundary extends Component<any, { hasError: boolean, error: string | null }> {
+  constructor() {
+    super({});
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true, error: error.message };
+  }
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // console.log(error, errorInfo)
+    this.setState({ error: [error.message, errorInfo.componentStack].join('\n') })
+  }
+  render() {
+    if (this.state.hasError) {
+      return <Fragment>
+        <h1>Something went wrong.</h1>
+        <pre>
+          {this.state.error}
+        </pre>
+        <p>
+          Please <a href="mailto:tony@globalfishingwatch.org">contact us</a> to report the problem
+        </p>
+      </Fragment>
+      
+    }
+    return this.props.children; 
+  }
+}
+
 render(
   <React.StrictMode>
-    <RecoilRoot>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </RecoilRoot>
+    <ErrorBoundary>
+      <RecoilRoot>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </RecoilRoot>
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 )
