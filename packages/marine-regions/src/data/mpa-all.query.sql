29167c0105e9a75df2693421067eeb8df98d4ac4
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2021-06-21 09:26:15 -0300

    Delete marine-regions package

--- packages/marine-regions/src/data/mpa-all.query.sql
+++
@@ -1,9 +0,0 @@
-
-  SELECT jsonb_build_object(
-    "id",
-	jsonb_build_object(
-		'id', "id",
-	    'label',      "label"
-		)
-  ) as region FROM (SELECT distinct gfw_id as "id", name as "label" FROM public."wdpa_may_2021_marine_1623923366398" ORDER BY "name" ASC) row;
-
--- packages/marine-regions/src/marine-regions.test.ts
+++
@@ -1,5 +0,0 @@
-import { getEEZ, getMPA } from './marine-regions'
-
-test('list eez', () => {
-  expect(Object.keys(getMPA())).toEqual([])
-})
