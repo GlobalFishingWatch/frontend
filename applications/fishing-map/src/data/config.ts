8d7ba5054106e9204d7f6154ae495c62084b792e
j8seangel <jseangel@gmail.com>
2021-07-08 13:30:40 +0200

    move LATEST_CARRIER_DATASET_ID to env

--- applications/fishing-map/src/data/config.ts
+++ applications/fishing-map/src/data/config.ts
@@ -10,6 +10,8 @@ export const IS_PRODUCTION =
 export const API_GATEWAY = process.env.API_GATEWAY || process.env.REACT_APP_API_GATEWAY || ''
 export const CARRIER_PORTAL_URL =
   process.env.REACT_APP_CARRIER_PORTAL_URL || 'https://carrier-portal.dev.globalfishingwatch.org'
+export const LATEST_CARRIER_DATASET_ID =
+  process.env.REACT_APP_LATEST_CARRIER_DATASET_ID || 'carriers:latest'
 
 export const GOOGLE_UNIVERSAL_ANALYTICS_ID = process.env.REACT_APP_GOOGLE_UNIVERSAL_ANALYTICS_ID
 export const GOOGLE_UNIVERSAL_ANALYTICS_INIT_OPTIONS: ReactGA.InitializeOptions = IS_PRODUCTION
