b67cb641d5121a50fe8d15da7f68faaf63d465a4
j8seangel <jseangel@gmail.com>
2021-07-28 18:46:45 +0200

    use esbuild to build app

--- applications/fishing-map/craco.config.js
+++ applications/fishing-map/craco.config.js
@@ -1,7 +1,22 @@
+const CracoEsbuildPlugin = require('craco-esbuild')
+
 module.exports = {
   webpack: {
     alias: {
       'mapbox-gl': '@globalfishingwatch/mapbox-gl',
     },
   },
+  plugins: [
+    {
+      plugin: CracoEsbuildPlugin,
+      options: {
+        // includePaths: ['/external/dir/with/components'], // Optional. If you want to include components which are not in src folder
+        enableSvgr: true,
+        esbuildLoaderOptions: {
+          loader: 'tsx',
+          target: 'es2015',
+        },
+      },
+    },
+  ],
 }
