b0336518cddc602fc1064e6da8f6c6fea5739c66
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2021-08-03 10:24:55 -0300

    Reoirder field in activity info popup

--- applications/vessel-history/src/features/profile/components/activity/ActivityModalContentDetailsFishing.tsx
+++ applications/vessel-history/src/features/profile/components/activity/ActivityModalContentDetailsFishing.tsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react'
 import { useTranslation } from 'react-i18next'
 import { DEFAULT_EMPTY_VALUE } from 'data/config'
-import { ActivityEvent } from 'types/activity'
+import { RenderedEvent } from 'features/vessels/activity/vessels-activity.slice'
+import ActivityModalContentDetails from './ActivityModalContentDetails'
 import ActivityModalContentField from './ActivityModalContentField'
+import styles from './ActivityModalDetails.module.css'
 
 interface ActivityModalContentProps {
-  event: ActivityEvent
+  event: RenderedEvent
 }
 
 const ActivityModalContentDetailsFishing: React.FC<ActivityModalContentProps> = (
@@ -16,26 +18,29 @@ const ActivityModalContentDetailsFishing: React.FC<ActivityModalContentProps> = 
 
   return (
     <Fragment>
-      <ActivityModalContentField
-        label={t('event.distance', 'Distance')}
-        value={
-          event.fishing?.totalDistanceKm
-            ? t('event.formatDistanceKm', '{{value}} km', {
-                value: event.fishing?.totalDistanceKm.toFixed(2),
-              })
-            : DEFAULT_EMPTY_VALUE
-        }
-      />
-      <ActivityModalContentField
-        label={t('event.avgSpeed', 'Avg Speed')}
-        value={
-          event.fishing?.averageSpeedKnots
-            ? t('event.formatSpeedKnots', '{{value}} knots', {
-                value: event.fishing?.averageSpeedKnots.toFixed(2),
-              })
-            : DEFAULT_EMPTY_VALUE
-        }
-      />
+      <div className={styles.row}>
+        <ActivityModalContentField
+          label={t('event.distance', 'Distance')}
+          value={
+            event.fishing?.totalDistanceKm
+              ? t('event.formatDistanceKm', '{{value}} km', {
+                  value: event.fishing?.totalDistanceKm.toFixed(2),
+                })
+              : DEFAULT_EMPTY_VALUE
+          }
+        />
+        <ActivityModalContentField
+          label={t('event.avgSpeed', 'Avg Speed')}
+          value={
+            event.fishing?.averageSpeedKnots
+              ? t('event.formatSpeedKnots', '{{value}} knots', {
+                  value: event.fishing?.averageSpeedKnots.toFixed(2),
+                })
+              : DEFAULT_EMPTY_VALUE
+          }
+        />
+      </div>
+      <ActivityModalContentDetails event={event} />
     </Fragment>
   )
 }
--- applications/vessel-history/src/features/profile/components/activity/ActivityModalContentDetailsLoitering.tsx
+++ applications/vessel-history/src/features/profile/components/activity/ActivityModalContentDetailsLoitering.tsx
@@ -1,13 +1,14 @@
 import React, { Fragment } from 'react'
 import { useTranslation } from 'react-i18next'
 import { DEFAULT_EMPTY_VALUE } from 'data/config'
-import { ActivityEvent } from 'types/activity'
+import { RenderedEvent } from 'features/vessels/activity/vessels-activity.slice'
 import { toFixed } from 'utils/shared'
+import ActivityModalContentDetails from './ActivityModalContentDetails'
 import ActivityModalContentField from './ActivityModalContentField'
 import styles from './ActivityModalDetails.module.css'
 
 interface ActivityModalContentProps {
-  event: ActivityEvent
+  event: RenderedEvent
 }
 
 const ActivityModalContentDetailsLoitering: React.FC<ActivityModalContentProps> = (
@@ -18,31 +19,34 @@ const ActivityModalContentDetailsLoitering: React.FC<ActivityModalContentProps> 
 
   return (
     <Fragment>
-      <ActivityModalContentField
-        label={t('event.distance', 'Distance')}
-        value={
-          event.loitering?.totalDistanceKilometers
-            ? t('event.formatDistanceKm', '{{value}} km', {
-                value: event.loitering?.totalDistanceKilometers.toFixed(2),
-              })
-            : DEFAULT_EMPTY_VALUE
-        }
-      />
-      <ActivityModalContentField
-        label={t('event.medianSpeed', 'Median Speed')}
-        value={
-          event.loitering?.medianSpeedKnots
-            ? t('event.formatSpeedKnots', '{{value}} knots', {
-                value: event.loitering?.medianSpeedKnots.toFixed(2),
-              })
-            : DEFAULT_EMPTY_VALUE
-        }
-      />
-      <ActivityModalContentField
-        className={styles.oneColumnField}
-        label={t('event.position', 'Position')}
-        value={`${toFixed(event.position.lat, 4)}, ${toFixed(event.position.lon, 4)}`}
-      />
+      <div className={styles.row}>
+        <ActivityModalContentField
+          label={t('event.distance', 'Distance')}
+          value={
+            event.loitering?.totalDistanceKilometers
+              ? t('event.formatDistanceKm', '{{value}} km', {
+                  value: event.loitering?.totalDistanceKilometers.toFixed(2),
+                })
+              : DEFAULT_EMPTY_VALUE
+          }
+        />
+        <ActivityModalContentField
+          label={t('event.medianSpeed', 'Median Speed')}
+          value={
+            event.loitering?.medianSpeedKnots
+              ? t('event.formatSpeedKnots', '{{value}} knots', {
+                  value: event.loitering?.medianSpeedKnots.toFixed(2),
+                })
+              : DEFAULT_EMPTY_VALUE
+          }
+        />
+        <ActivityModalContentField
+          label={t('event.position', 'Position')}
+          value={`${toFixed(event.position.lat, 4)}, ${toFixed(event.position.lon, 4)}`}
+        />
+      </div>
+
+      <ActivityModalContentDetails event={event} />
     </Fragment>
   )
 }
