e05d0656810cefc93eece4845f5047f02fddacaa
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2021-08-05 17:46:20 -0300

    Select vessel event on click

--- applications/vessel-history/src/features/map/Map.tsx
+++ applications/vessel-history/src/features/map/Map.tsx
@@ -1,18 +1,20 @@
 import { ReactElement } from 'react'
 import { useSelector } from 'react-redux'
 import { InteractiveMap } from '@globalfishingwatch/react-map-gl'
-import { InteractionEvent, useLayerComposer, useMapClick } from '@globalfishingwatch/react-hooks'
+import { useLayerComposer, useMapClick } from '@globalfishingwatch/react-hooks'
 import { ExtendedStyleMeta } from '@globalfishingwatch/layer-composer'
 import { selectResourcesLoading } from 'features/resources/resources.slice'
 import { useGeneratorsConnect } from './map.hooks'
 import useMapInstance from './map-context.hooks'
 import useViewport from './map-viewport.hooks'
 import MapControls from './controls/MapControls'
+import useMapEvents from './map-events.hooks'
 import styles from './Map.module.css'
 import '@globalfishingwatch/mapbox-gl/dist/mapbox-gl.css'
 
 const Map = (): ReactElement => {
   const map = useMapInstance()
+  const { selectVesselEventOnClick } = useMapEvents()
 
   const { generatorsConfig, globalConfig } = useGeneratorsConnect()
   const { viewport, onViewportChange } = useViewport()
@@ -22,10 +24,12 @@ const Map = (): ReactElement => {
   const mapOptions = {
     customAttribution: '© Copyright Global Fishing Watch 2020',
   }
-  const dispatchClickedEvent = (event: InteractionEvent | null) => {
-    console.log(event)
-  }
-  const onMapClick = useMapClick(dispatchClickedEvent, style?.metadata as ExtendedStyleMeta, map)
+
+  const onMapClick = useMapClick(
+    selectVesselEventOnClick,
+    style?.metadata as ExtendedStyleMeta,
+    map
+  )
 
   return (
     <div className={styles.container}>
