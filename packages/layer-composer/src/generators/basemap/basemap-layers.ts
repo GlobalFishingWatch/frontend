6be4797b4e9172528b06f6881292e9dc4bb760f1
Erik Escoffier <erik@satellitestud.io>
2021-08-09 15:58:44 +0200

    use GFW gtiles API

--- packages/layer-composer/src/generators/basemap/basemap-layers.ts
+++ packages/layer-composer/src/generators/basemap/basemap-layers.ts
@@ -87,9 +87,7 @@ export const sources: Dictionary<Record<string, Partial<any>>> = {
   [BasemapType.Satellite]: {
     satellite: {
       tiles: [
-        'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
-        'https://mt2.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
-        'https://mt3.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+        'https://gtiles.api.dev.globalfishingwatch.org/v1/tileset/sat/tile?x={x}&y={y}&z={z}',
       ],
       type: 'raster',
       tileSize: 256,
