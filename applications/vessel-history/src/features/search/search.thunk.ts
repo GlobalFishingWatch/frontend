a3ea4e501de6b23f6fe66e249619f6700dccdd00
erik <erik@satellitestud.io>
2021-08-26 16:37:23 +0200

    clean up thunk

--- applications/vessel-history/src/features/search/search.thunk.ts
+++ applications/vessel-history/src/features/search/search.thunk.ts
@@ -1,4 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
+import { stringify } from 'qs'
 import GFWAPI from '@globalfishingwatch/api-client'
 import { VesselSearch } from '@globalfishingwatch/api-types'
 import { BASE_DATASET, RESULTS_PER_PAGE, SEARCH_MIN_CHARACTERS } from 'data/constants'
@@ -6,14 +7,21 @@ import { RootState } from 'store'
 import { CachedVesselSearch } from './search.slice'
 
 const fetchData = async (query: string, offset: number, signal?: AbortSignal | null) => {
-  return await GFWAPI.fetch<any>(
-    `/v1/vessels/search?datasets=${encodeURIComponent(
-      BASE_DATASET
-    )}&limit=${RESULTS_PER_PAGE}&offset=${offset}&query=${encodeURIComponent(query)}&useTMT=true`,
-    {
-      signal,
-    }
-  )
+  const endpoint = true ? 'search' : 'advanced-search'
+
+  const urlQuery = stringify({
+    datasets: BASE_DATASET,
+    limit: RESULTS_PER_PAGE,
+    offset,
+    query,
+    useTMT: true,
+  })
+
+  const url = `/v1/vessels/${endpoint}?${urlQuery}`
+
+  return await GFWAPI.fetch<any>(url, {
+    signal,
+  })
     .then((json: any) => {
       const resultVessels: Array<VesselSearch> = json.entries
 
