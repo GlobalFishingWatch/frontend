ac80e15ea0fdbc809cc7adfb42b715bbe05951bd
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2021-08-26 16:49:02 -0300

    Added some tests for highlighting events selector

---
+++ applications/vessel-history/src/features/vessels/activity/vessels-highlight.selectors.test.ts
@@ -0,0 +1,220 @@
+import { anyRegion } from 'features/regions/regions.slice'
+import { Settings } from 'features/settings/settings.slice'
+import { RenderedEvent } from './vessels-activity.selectors'
+import { selectActivityHighlightEvents } from './vessels-highlight.selectors'
+import { fishingEvents, emptySettings } from './__mocks__/highlight.mock'
+import {
+  loiteringAndEncounterEvents,
+  portVisitEvents,
+} from './__mocks__/selectEventsWithRenderingInfo.mock'
+
+describe('selectActivityHighlightEvents', () => {
+  const cases: [string, RenderedEvent[][], Settings, number][] = [
+    [
+      'fishing events taking place in any eez',
+      [...loiteringAndEncounterEvents, ...portVisitEvents, ...fishingEvents] as any,
+      {
+        ...emptySettings,
+        fishingEvents: {
+          eezs: [anyRegion.id],
+        },
+      },
+      9,
+    ],
+    [
+      'no fishing events',
+      [...loiteringAndEncounterEvents, ...portVisitEvents, ...fishingEvents] as any,
+      {
+        ...emptySettings,
+        fishingEvents: {},
+      },
+      0,
+    ],
+    [
+      'fishing events filtered by duration > 5',
+      [...loiteringAndEncounterEvents, ...portVisitEvents, ...fishingEvents] as any,
+      {
+        ...emptySettings,
+        fishingEvents: {
+          duration: 5,
+        },
+      },
+      6,
+    ],
+    [
+      'fishing events filtered by distance from port > 17',
+      [...loiteringAndEncounterEvents, ...portVisitEvents, ...fishingEvents] as any,
+      {
+        ...emptySettings,
+        fishingEvents: {
+          distancePortLonger: 18,
+        },
+      },
+      4,
+    ],
+    [
+      'fishing events filtered by duration > 5 in an EEZ without events',
+      [...loiteringAndEncounterEvents, ...portVisitEvents, ...fishingEvents] as any,
+      {
+        ...emptySettings,
+        fishingEvents: {
+          eezs: ['-99999'],
+          duration: 5,
+        },
+      },
+      0,
+    ],
+    [
+      'fishing events filtered by duration > 5 inside FINNISH EEZ',
+      [...loiteringAndEncounterEvents, ...portVisitEvents, ...fishingEvents] as any,
+      {
+        ...emptySettings,
+        fishingEvents: {
+          eezs: ['5676'],
+          duration: 5,
+        },
+      },
+      5,
+    ],
+    [
+      'fishing events filtered by duration > 5 and distance from shore > 20 inside FINNISH EEZ',
+      [...loiteringAndEncounterEvents, ...portVisitEvents, ...fishingEvents] as any,
+      {
+        ...emptySettings,
+        fishingEvents: {
+          eezs: ['5676'],
+          duration: 5,
+          distanceShoreLonger: 20,
+        },
+      },
+      1,
+    ],
+    [
+      'fishing events filtered by distance from shore > 20 in any eez',
+      [...loiteringAndEncounterEvents, ...portVisitEvents, ...fishingEvents] as any,
+      {
+        ...emptySettings,
+        fishingEvents: {
+          eezs: [anyRegion.id],
+          distanceShoreLonger: 24,
+        },
+      },
+      2,
+    ],
+    [
+      'fishing events filtered inside FINNISH EEZ',
+      [...loiteringAndEncounterEvents, ...portVisitEvents, ...fishingEvents] as any,
+      {
+        ...emptySettings,
+        fishingEvents: {
+          eezs: ['5676'],
+        },
+      },
+      8,
+    ],
+    [
+      'fishing events filtered inside FINNISH EEZ omitting other region filters',
+      [...loiteringAndEncounterEvents, ...portVisitEvents, ...fishingEvents] as any,
+      {
+        ...emptySettings,
+        fishingEvents: {
+          eezs: ['5676'],
+          rfmos: ['-9'],
+          mpas: ['-9'],
+        },
+      },
+      8,
+    ],
+    [
+      'port visit events on NOR country',
+      [...loiteringAndEncounterEvents, ...portVisitEvents, ...fishingEvents] as any,
+      {
+        ...emptySettings,
+        portVisits: {
+          flags: ['NOR'],
+        },
+      },
+      11,
+    ],
+    [
+      'port visit events on NOR country and duration > 4',
+      [...loiteringAndEncounterEvents, ...portVisitEvents, ...fishingEvents] as any,
+      {
+        ...emptySettings,
+        portVisits: {
+          flags: ['NOR'],
+          duration: 4,
+        },
+      },
+      6,
+    ],
+    [
+      'port visit events on NOR country and duration > 4 and distance from shore > 0.1',
+      [
+        ...loiteringAndEncounterEvents,
+        ...portVisitEvents,
+        ...fishingEvents,
+        {
+          // adding a custom port visit to test this filter
+          ...(() => {
+            const last = portVisitEvents.slice(-1).pop()
+            return {
+              ...last,
+              duration: 4,
+              distances: {
+                ...last?.distances,
+                startDistanceFromShoreKm: 0.1,
+                endDistanceFromShoreKm: 0.3,
+              },
+              port_visit: {
+                ...last?.port_visit,
+                startAnchorage: { ...last?.port_visit.startAnchorage, distance_from_shore_km: 0.1 },
+                intermediateAnchorage: {
+                  ...last?.port_visit.intermediateAnchorage,
+                  distance_from_shore_km: 0.2,
+                },
+                endAnchorage: { ...last?.port_visit.endAnchorage, distance_from_shore_km: 0.3 },
+              },
+            }
+          })(),
+        },
+      ] as any,
+      {
+        ...emptySettings,
+        portVisits: {
+          flags: ['NOR'],
+          duration: 4,
+          distanceShoreLonger: 0.1,
+        },
+      },
+      1,
+    ],
+    [
+      'port visit events with duration > 4',
+      [...loiteringAndEncounterEvents, ...portVisitEvents, ...fishingEvents] as any,
+      {
+        ...emptySettings,
+        portVisits: {
+          duration: 4,
+        },
+      },
+      6,
+    ],
+  ]
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test.each(cases)(
+    'highlights %p',
+    (testCaseDescription, selectEventsWithRenderingInfo, selectSettings, expectedCount) => {
+      const result = selectActivityHighlightEvents.resultFunc(
+        selectEventsWithRenderingInfo,
+        selectSettings
+      )
+      expect(result.length).toEqual(expectedCount)
+      expect(result).toMatchSnapshot()
+    }
+  )
+})
--- applications/vessel-history/src/features/vessels/activity/vessels-highlight.selectors.ts
+++ applications/vessel-history/src/features/vessels/activity/vessels-highlight.selectors.ts
@@ -8,10 +8,13 @@ import {
 } from 'features/settings/settings.slice'
 import { RenderedEvent, selectEventsWithRenderingInfo } from './vessels-activity.selectors'
 
-const isAnyFilterSet = (filter: SettingsEvents) =>
+const isAnyRegionFilterSet = (filter: SettingsEvents) =>
   (filter.eezs !== undefined && filter.eezs.length > 0) ||
   (filter.rfmos !== undefined && filter.rfmos.length > 0) ||
-  (filter.mpas !== undefined && filter.mpas.length > 0) ||
+  (filter.mpas !== undefined && filter.mpas.length > 0)
+
+const isAnyFilterSet = (filter: SettingsEvents) =>
+  isAnyRegionFilterSet(filter) ||
   filter.duration !== undefined ||
   filter.distanceShoreLonger !== undefined ||
   filter.distancePortLonger !== undefined
@@ -27,9 +30,7 @@ const matchAnyDistanceLonger = (eventDistances: number[], distanceLongerThan?: n
     Math.max(...eventDistances) >= distanceLongerThan)
 
 const matchAnyRegion = (eventRegions: string[] = [], regions: string[] = []) =>
-  // when there is no region defined to highlight
-  regions.length === 0 ||
-  // or when there are regions defined to highlight
+  // when there are regions defined to highlight
   // if ANY option was selected and the event is assigned to one region at least
   (regions.includes(anyRegion.id) && eventRegions.length > 0) ||
   // or at least one of the selected regions is assigned to the event
@@ -37,14 +38,18 @@ const matchAnyRegion = (eventRegions: string[] = [], regions: string[] = []) =>
 
 const filterActivityEvent = (event: RenderedEvent, filter: SettingsEvents) =>
   isAnyFilterSet(filter) &&
-  // any eez is matched
-  (matchAnyRegion(event.eezs, filter.eezs) ||
-    // any rfmo is matched
-    matchAnyRegion(event.rfmos, filter.rfmos) ||
-    // any mpa is matched (VERIFY/CONFIRM IF THIS IS CORRECT)
-    matchAnyRegion(event.regions.mpant, filter.mpas) ||
-    matchAnyRegion(event.regions.mparu, filter.mpas) ||
-    matchAnyRegion(event.regions.mregion, filter.mpas)) &&
+  // no region filter is set
+  (!isAnyRegionFilterSet(filter) ||
+    // or if any is set, then at least one of them should match
+    (isAnyRegionFilterSet(filter) &&
+      // any eez is matched
+      (matchAnyRegion(event.regions.eez, filter.eezs) ||
+        // any rfmo is matched
+        matchAnyRegion(event.regions.rfmo, filter.rfmos) ||
+        // any mpa is matched (VERIFY/CONFIRM IF THIS IS CORRECT)
+        matchAnyRegion(event.regions.mpant, filter.mpas) ||
+        matchAnyRegion(event.regions.mparu, filter.mpas) ||
+        matchAnyRegion(event.regions.mregion, filter.mpas)))) &&
   matchDurationLonger(event.duration, filter.duration) &&
   matchAnyDistanceLonger(
     [
@@ -61,6 +66,9 @@ const filterActivityEvent = (event: RenderedEvent, filter: SettingsEvents) =>
     filter.distancePortLonger
   )
 
+const isAnyFlagFilterSet = (filter: SettingsPortVisits) =>
+  filter.flags !== undefined && filter.flags.length > 0
+
 const isAnyPortFilterSet = (filter: SettingsPortVisits) =>
   (filter.flags !== undefined && filter.flags.length > 0) ||
   filter.duration !== undefined ||
@@ -71,10 +79,14 @@ const matchAnyPortFlag = (port: Anchorage | undefined, regions: string[] = []) =
 
 const filterPortEvent = (event: RenderedEvent, filter: SettingsPortVisits) =>
   isAnyPortFilterSet(filter) &&
-  // any port country is matched
-  (matchAnyPortFlag(event.port_visit?.startAnchorage, filter.flags) ||
-    matchAnyPortFlag(event.port_visit?.intermediateAnchorage, filter.flags) ||
-    matchAnyPortFlag(event.port_visit?.endAnchorage, filter.flags)) &&
+  // no flag filter is set
+  (!isAnyFlagFilterSet(filter) ||
+    // or when flag filter is set
+    (isAnyFlagFilterSet(filter) &&
+      // any port country is matched
+      (matchAnyPortFlag(event.port_visit?.startAnchorage, filter.flags) ||
+        matchAnyPortFlag(event.port_visit?.intermediateAnchorage, filter.flags) ||
+        matchAnyPortFlag(event.port_visit?.endAnchorage, filter.flags)))) &&
   matchDurationLonger(event.duration, filter.duration) &&
   matchAnyDistanceLonger(
     [
