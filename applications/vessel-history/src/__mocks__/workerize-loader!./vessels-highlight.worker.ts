0f1f23ac594cbaabf476cecc4a224175a0777b4b
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2021-09-01 18:21:06 -0300

    Fixed failing tests

--- applications/vessel-history/src/__mocks__/workerize-loader!./vessels-highlight.worker.ts
+++
@@ -1,9 +0,0 @@
-export function filterActivityHighlightEvents(events: any, settings: any) {
-  return []
-}
-
-export function isAnyHighlightsSettingDefined(settings: any) {
-  return true
-}
-const mod = { filterActivityHighlightEvents, isAnyHighlightsSettingDefined }
-export default mod
---
+++ applications/vessel-history/src/features/vessels/activity/__mocks__/vessels-highlight.worker.ts
@@ -0,0 +1,11 @@
+import {
+  filterActivityHighlightEvents,
+  isAnyHighlightsSettingDefined,
+} from '../vessels-highlight.worker'
+
+const mod = () => ({
+  filterActivityHighlightEvents,
+  isAnyHighlightsSettingDefined,
+})
+
+export default mod
