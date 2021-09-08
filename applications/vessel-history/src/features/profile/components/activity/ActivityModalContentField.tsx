1ffb8946f7a525342fad41136c73014569f81020
Fernando Saez <fertandil87@gmail.com>
2021-09-07 23:02:30 -0300

    added confidence help

--- applications/vessel-history/src/features/profile/components/activity/ActivityModalContentField.tsx
+++ applications/vessel-history/src/features/profile/components/activity/ActivityModalContentField.tsx
@@ -6,18 +6,21 @@ interface ActivityModalContentFieldProps {
   className?: string
   label: string
   value: string | ReactNode
+  help?: string | ReactNode
 }
 
 const ActivityModalContentField: React.FC<ActivityModalContentFieldProps> = ({
   className,
   label,
   value,
+  help,
 }: ActivityModalContentFieldProps): React.ReactElement => {
   return (
     <Fragment>
       <div className={cx(className, styles.field)}>
         <label className={styles.label}>{label}</label>
         <span className={styles.value}>{value}</span>
+        <span className={styles.help}>{help}</span>
       </div>
     </Fragment>
   )
