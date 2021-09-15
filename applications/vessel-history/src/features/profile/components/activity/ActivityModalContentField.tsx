4f5e4f5c81f9c39cfedf608f67aae6e8a6cf242f
Fernando Saez <fertandil87@gmail.com>
2021-09-15 01:49:36 -0300

    added toggle help

--- applications/vessel-history/src/features/profile/components/activity/ActivityModalContentField.tsx
+++ applications/vessel-history/src/features/profile/components/activity/ActivityModalContentField.tsx
@@ -1,26 +1,29 @@
 import React, { Fragment, ReactNode } from 'react'
 import cx from 'classnames'
+import { IconButton } from '@globalfishingwatch/ui-components'
 import styles from './ActivityModalContentField.module.css'
 
 interface ActivityModalContentFieldProps {
   className?: string
   label: string
   value: string | ReactNode
-  help?: string | ReactNode
+  onHelpClick?: () => void
 }
 
 const ActivityModalContentField: React.FC<ActivityModalContentFieldProps> = ({
   className,
   label,
   value,
-  help,
+  onHelpClick
 }: ActivityModalContentFieldProps): React.ReactElement => {
   return (
     <Fragment>
       <div className={cx(className, styles.field)}>
         <label className={styles.label}>{label}</label>
-        <span className={styles.value}>{value}</span>
-        <span className={styles.help}>{help}</span>
+        <span className={styles.value}>
+          {value}
+          {onHelpClick && <IconButton icon="info" size="tiny" onClick={onHelpClick} />}
+        </span>
       </div>
     </Fragment>
   )
