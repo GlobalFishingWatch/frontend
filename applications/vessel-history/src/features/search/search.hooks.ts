9e6e6a3b584629724cde334b841b391bd37f235b
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2021-09-16 09:46:06 -0300

    remove comments

--- applications/vessel-history/src/features/search/search.hooks.ts
+++ applications/vessel-history/src/features/search/search.hooks.ts
@@ -1,7 +1,4 @@
 import { useCallback, useMemo, useState } from 'react'
-// import { useTranslation } from 'react-i18next'
-// import { DEFAULT_EMPTY_VALUE } from 'data/config'
-// import { VesselAPISource } from 'types'
 
 // Maximum number of vessels that can be merged
 const LIMIT_VESSELS_MERGE_TO = 3
@@ -19,9 +16,6 @@ export const useSearchConnect = () => {
         setSelectedVessels(selectedVessels.filter((i) => index !== i))
       } else if (isVesselsMergeEnabled) {
         setSelectedVessels([...selectedVessels, index])
-      } else {
-        // TODO Display message to the user to inform the limit
-        console.log('Can not select more than 3 vessels to merge')
       }
     },
     [isVesselsMergeEnabled, selectedVessels]
