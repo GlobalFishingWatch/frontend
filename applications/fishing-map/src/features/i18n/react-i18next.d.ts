cbdfa16a2279b54fad78397503d6a6b7d631f2cc
j8seangel <jseangel@gmail.com>
2021-09-21 12:46:41 +0200

    update i18next dependencies

--- applications/fishing-map/src/features/i18n/react-i18next.d.ts
+++ applications/fishing-map/src/features/i18n/react-i18next.d.ts
@@ -4,12 +4,14 @@ import 'react-i18next'
 import translations from '../../../public/locales/source/translations.json'
 import datasets from '../../../../../packages/i18n-labels/src/datasets.json'
 import flags from '../../../../../packages/i18n-labels/src/flags.json'
+import timebar from '../../../../../packages/i18n-labels/src/timebar.json'
 
 declare module 'react-i18next' {
   // and extend them!
   interface Resources {
     translations: typeof translations
     datasets: typeof datasets
+    timebar: typeof timebar
     flags: typeof flags
   }
 }
