e83cff26fa4ebc449ebc7eb79a6af98c3779670a
j8seangel <jseangel@gmail.com>
2021-09-23 17:25:21 +0200

    use package definition types

--- applications/fishing-map/src/features/i18n/react-i18next.d.ts
+++ applications/fishing-map/src/features/i18n/react-i18next.d.ts
@@ -2,19 +2,16 @@
 import 'react-i18next'
 // import all namespaces (for the default language, only)
 import type datasets from '@globalfishingwatch/i18n-labels/source/datasets.json'
-import type githubDatasets from '@globalfishingwatch/frontend/packages/i18n-labels/source/datasets.json'
 import type flags from '@globalfishingwatch/i18n-labels/source/flags.json'
-import type githubFlags from '@globalfishingwatch/frontend/packages/i18n-labels/source/flags.json'
 import type timebar from '@globalfishingwatch/i18n-labels/source/timebar.json'
-import type githubTimebar from '@globalfishingwatch/frontend/packages/i18n-labels/source/timebar.json'
 import type translations from '../../../public/locales/source/translations.json'
 
 declare module 'react-i18next' {
   // and extend them!
   interface Resources {
     translations: typeof translations
-    datasets: typeof datasets | typeof githubDatasets
-    timebar: typeof timebar | typeof githubTimebar
-    flags: typeof flags | typeof githubFlags
+    datasets: typeof datasets
+    timebar: typeof timebar
+    flags: typeof flags
   }
 }
--- applications/vessel-history/src/features/i18n/react-i18next.d.ts
+++ applications/vessel-history/src/features/i18n/react-i18next.d.ts
@@ -2,13 +2,12 @@
 import 'react-i18next'
 // import all namespaces (for the default language, only)
 import type flags from '@globalfishingwatch/i18n-labels/source/flags.json'
-import type githubFlags from '@globalfishingwatch/frontend/packages/i18n-labels/source/flags.json'
 import type translations from '../../../public/locales/source/translations.json'
 
 declare module 'react-i18next' {
   // and extend them!
   interface Resources {
     translations: typeof translations
-    flags: typeof flags | typeof githubFlags
+    flags: typeof flags
   }
 }
