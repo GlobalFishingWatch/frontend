909713bc66e3c57656a065eaa549ed32cdec9225
j8seangel <jseangel@gmail.com>
2021-10-20 13:43:35 +0200

    save decoders package in github

--- packages/pbf-decoders/index.ts
+++ packages/pbf-decoders/index.ts
@@ -1,7 +1,2 @@
-// import * as tile from './decoders/4wings-tile'
-// import * as vessel from './decoders/vessels'
-
-const vessels = {} as any
-const tile = {} as any
-
-export { vessels, tile }
+export { default as tile } from './decoders/4wings-tile'
+export { vessels } from './decoders/vessels'
--- packages/pbf-decoders/rollup.config.js
+++
@@ -1,9 +0,0 @@
-const nrwlConfig = require('@nrwl/react/plugins/bundle-rollup')
-
-module.exports = (config) => {
-  nrwlConfig(config)
-  return {
-    ...config,
-    external: ['@globalfishingwatch/pbf', '@globalfishingwatch/pbf/decoders/vessels.js'],
-  }
-}
