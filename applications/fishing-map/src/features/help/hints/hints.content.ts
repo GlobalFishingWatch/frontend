023ee38e696fb252a7bbcf494fd7cc48a5536d9f
satellitestudiodesign <juancarlos@satellitestud.io>
2021-10-25 17:17:50 +0200

    ensure all hints appear in crowdin

--- applications/fishing-map/src/features/help/hints/hints.content.ts
+++ applications/fishing-map/src/features/help/hints/hints.content.ts
@@ -1,10 +1,15 @@
 import type { Placement } from 'tippy.js'
-import { Locale } from 'types'
 import fishingEffortHeatmapImg from 'assets/images/hints/fishingEffortHeatmap.png'
 import howToFilterActivityLayersImg from 'assets/images/hints/howToFilterActivityLayers.png'
 import clickingOnAGridCellToShowVesselsImg from 'assets/images/hints/clickingOnAGridCellToShowVessels.png'
 import changingTheTimeRangeImg from 'assets/images/hints/changingTheTimeRange.png'
 import areaSearchImg from 'assets/images/hints/areaSearch.png'
+
+// t('helpHints:fishingEffortHeatmap', 'The map shows an interactive heat map of apparent fishing effort. The lighter grid cells are the most intensely fished areas.')
+// t('helpHints:filterActivityLayers', 'Click the filter icon to filter activity using different available criteria, including data source, flag and gear types.')
+// t('helpHints:clickingOnAGridCellToShowVessels', 'Click on any grid cell to show the most active vessels in that area over the chosen time range.')
+// t('helpHints:changingTheTimeRange', 'Click and drag to change the time range you are viewing in the time bar.')
+// t('helpHints:areaSearch', 'Search by country, sea or ocean to focus on an area you're interested in.')
 
 export type HintId =
   | 'fishingEffortHeatmap'
