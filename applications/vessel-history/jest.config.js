2486345c723387994ea8e76375862baf119f898c
j8seangel <jseangel@gmail.com>
2021-11-05 18:05:28 +0100

    try testing using nrwl:jest executor

--- applications/vessel-history/jest.config.js
+++ applications/vessel-history/jest.config.js
@@ -3,11 +3,13 @@ module.exports = {
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': './jest.transform.js',
+    '^.+\\.[tj]sx?$': 'babel-jest',
   },
+  transformIgnorePatterns: [],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   moduleNameMapper: {
     '^workerize-loader(\\?.*)?!(.*)/([^/]*)$': '$2/__mocks__/$3',
   },
   coverageDirectory: '../../coverage/applications/vessel-history',
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/types/**/*.*'],
 }
