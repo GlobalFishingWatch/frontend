e39ed57fb31747bb5b1b6d94981324ddae88be98
j8seangel <jseangel@gmail.com>
2021-11-26 19:27:39 +0100

    fix worker running

--- apps/vessel-history/offline/constants.ts
+++ apps/vessel-history/offline/constants.ts
@@ -1,9 +1,7 @@
 export const BASE_URL =
   process.env.NEXT_PUBLIC_URL || (process.env.NODE_ENV === 'production' ? '/vessel-viewer' : '')
 
-export const LANDMASS_OFFLINE_GEOJSON = `${
-  process.env.NEXT_PUBLIC_URL ?? ''
-}/data/ne_10m_admin_0_countries_gj.geojson`
+export const LANDMASS_OFFLINE_GEOJSON = `/data/ne_10m_admin_0_countries_gj.geojson`
 
 export const API_GATEWAY = process.env.NEXT_PUBLIC_API_GATEWAY
 
