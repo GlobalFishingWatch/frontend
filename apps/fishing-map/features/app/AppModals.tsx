cfda9cbb357679fbb55b26adec3a9d580ae663e2
j8seangel <jseangel@gmail.com>
2021-12-21 15:53:46 +0100

    reuse secret menu hook

--- apps/fishing-map/features/app/AppModals.tsx
+++ apps/fishing-map/features/app/AppModals.tsx
@@ -3,27 +3,40 @@ import { useSelector } from 'react-redux'
 import { useLocalStorage } from '@globalfishingwatch/react-hooks'
 import { Modal } from '@globalfishingwatch/ui-components'
 import { WorkspaceCategories } from 'data/workspaces'
-import useDebugMenu from 'features/debug/debug.hooks'
-import useEditorMenu from 'features/editor/editor.hooks'
 import { isGFWUser } from 'features/user/user.slice'
 import Welcome, { DISABLE_WELCOME_POPUP } from 'features/welcome/Welcome'
 import { selectLocationCategory } from 'routes/routes.selectors'
 import { selectReadOnly } from 'features/app/app.selectors'
-import DebugMenu from 'features/debug/DebugMenu'
+import { selectDebugActive, toggleDebugMenu } from 'features/debug/debug.slice'
+import { selectEditorActive, toggleEditorMenu } from 'features/editor/editor.slice'
 import EditorMenu from 'features/editor/EditorMenu'
 import DownloadActivityModal from 'features/download/DownloadActivityModal'
 import DownloadTrackModal from 'features/download/DownloadTrackModal'
 import { ROOT_DOM_ELEMENT } from 'data/config'
+import useSecretMenu from 'hooks/secret-menu.hooks'
+import DebugMenu from 'features/debug/DebugMenu'
 import styles from './App.module.css'
 
 const MARINE_MANAGER_LAST_VISIT = 'MarineManagerLastVisit'
+
+const DebugMenuConfig = {
+  key: 'd',
+  onToggle: toggleDebugMenu,
+  selectMenuActive: selectDebugActive,
+}
+
+const EditorMenuConfig = {
+  key: 'e',
+  onToggle: toggleEditorMenu,
+  selectMenuActive: selectEditorActive,
+}
 
 const AppModals = () => {
   const isFirstTimeVisit = !localStorage.getItem(MARINE_MANAGER_LAST_VISIT)
   const readOnly = useSelector(selectReadOnly)
   const gfwUser = useSelector(isGFWUser)
-  const { debugActive, dispatchToggleDebugMenu } = useDebugMenu()
-  const { editorActive, dispatchToggleEditorMenu } = useEditorMenu()
+  const [debugActive, dispatchToggleDebugMenu] = useSecretMenu(DebugMenuConfig)
+  const [editorActive, dispatchToggleEditorMenu] = useSecretMenu(EditorMenuConfig)
   const [disabledWelcomePopup] = useLocalStorage(DISABLE_WELCOME_POPUP, false)
 
   const locationIsMarineManager =
