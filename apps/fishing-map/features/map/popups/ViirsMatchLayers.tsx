09121432481cd36dae639ec52a21cc954bd8d530
j8seangel <jseangel@gmail.com>
2021-12-23 11:33:30 +0100

    show not matched detections in tooltip title

--- apps/fishing-map/features/map/popups/ViirsMatchLayers.tsx
+++ apps/fishing-map/features/map/popups/ViirsMatchLayers.tsx
@@ -11,6 +11,19 @@ type ViirsMatchTooltipRowProps = {
 }
 function ViirsMatchTooltipRow({ feature, showFeaturesDetails }: ViirsMatchTooltipRowProps) {
   const { t } = useTranslation()
+  // Avoid showing not matched detections
+  const matchedVessels = (feature.vesselsInfo?.vessels || []).filter((v) => v.id !== null)
+  const matchedDetections = matchedVessels
+    ? matchedVessels.reduce((acc, vessel) => acc + vessel.detections, 0)
+    : 0
+  const featureVesselsFilter = {
+    ...feature,
+    vesselsInfo: {
+      ...feature.vesselsInfo,
+      vessels: matchedVessels,
+    },
+  }
+  const notMatchedDetections = parseInt(feature.value) - matchedDetections
   return (
     <div className={styles.popupSection}>
       <span className={styles.popupSectionColor} style={{ backgroundColor: feature.color }} />
@@ -21,10 +34,13 @@ function ViirsMatchTooltipRow({ feature, showFeaturesDetails }: ViirsMatchToolti
             <I18nNumber number={feature.value} />{' '}
             {t([`common.${feature.temporalgrid?.unit}` as any, 'common.detection'], 'detections', {
               count: parseInt(feature.value), // neded to select the plural automatically
-            })}
+            })}{' '}
+            (<I18nNumber number={notMatchedDetections} /> {t('vessel.unmatched', 'unmatched')})
           </span>
         </div>
-        {showFeaturesDetails && <VesselsTable feature={feature} vesselProperty="detections" />}
+        {showFeaturesDetails && (
+          <VesselsTable feature={featureVesselsFilter} vesselProperty="detections" />
+        )}
       </div>
     </div>
   )
