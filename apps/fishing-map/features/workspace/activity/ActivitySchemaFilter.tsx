bceeb191e7f64cdb98e8fd16a1024dadd6b03240
j8seangel <jseangel@gmail.com>
2021-12-23 14:01:19 +0100

    debounce slider change

--- apps/fishing-map/features/workspace/activity/ActivitySchemaFilter.tsx
+++ apps/fishing-map/features/workspace/activity/ActivitySchemaFilter.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useCallback, useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
+import { debounce } from 'lodash'
 import { MultiSelect, MultiSelectOption, Slider } from '@globalfishingwatch/ui-components'
 import { getPlaceholderBySelections } from 'features/i18n/utils'
 import { SchemaFilter } from 'features/datasets/datasets.utils'
@@ -15,28 +16,34 @@ export const showSchemaFilter = (schemaFilter: SchemaFilter) => {
   return schemaFilter.active && schemaFilter.options.length > 1
 }
 
+const getRangeBySchema = (schemaFilter: SchemaFilter): [number, number] => {
+  const { options, optionsSelected } = schemaFilter
+  const optionValues = options.map(({ id }) => parseInt(id)).sort((a, b) => a - b)
+  const rangeValues =
+    optionsSelected?.length > 0
+      ? optionsSelected.map(({ id }) => parseInt(id)).sort((a, b) => a - b)
+      : optionValues
+
+  return [rangeValues[0], rangeValues[rangeValues.length - 1]]
+}
+
 function ActivitySchemaFilter({
   schemaFilter,
   onSelect,
   onRemove,
   onClean,
 }: ActivitySchemaFilterProps): React.ReactElement {
-  const { t } = useTranslation()
-  if (!showSchemaFilter(schemaFilter)) {
-    return null
-  }
   const { id, tooltip, disabled, options, optionsSelected, type } = schemaFilter
-  if (type === 'number') {
-    const optionValues = options.map(({ id }) => parseInt(id)).sort((a, b) => a - b)
-    const rangeValues =
-      optionsSelected?.length > 0
-        ? optionsSelected.map(({ id }) => parseInt(id)).sort((a, b) => a - b)
-        : optionValues
+  const [range, setRange] = useState<number[] | null>(
+    type === 'number' ? getRangeBySchema(schemaFilter) : null
+  )
+  const { t } = useTranslation()
 
-    const range = [rangeValues[0], rangeValues[rangeValues.length - 1]]
-
-    const onSliderChange = (rangeSelected) => {
-      if (rangeSelected[0] === range[0] && rangeSelected[1] === range[1]) {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedSliderCb = useCallback(
+    debounce((rangeSelected) => {
+      const filterRange = getRangeBySchema(schemaFilter)
+      if (rangeSelected[0] === filterRange[0] && rangeSelected[1] === filterRange[1]) {
         onClean(id)
       } else {
         const selection = rangeSelected.map((id) => ({
@@ -45,7 +52,23 @@ function ActivitySchemaFilter({
         }))
         onSelect(id, selection)
       }
+    }, 300),
+    []
+  )
+
+  useEffect(() => {
+    if (type === 'number') {
+      debouncedSliderCb(range)
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [range])
+
+  if (!showSchemaFilter(schemaFilter)) {
+    return null
+  }
+
+  if (type === 'number') {
+    const optionValues = options.map(({ id }) => parseInt(id)).sort((a, b) => a - b)
     return (
       <Slider
         className={styles.multiSelect}
@@ -56,7 +79,7 @@ function ActivitySchemaFilter({
           min: optionValues?.[0],
           max: optionValues?.[optionValues.length - 1],
         }}
-        onChange={onSliderChange}
+        onChange={setRange}
       ></Slider>
     )
   }
