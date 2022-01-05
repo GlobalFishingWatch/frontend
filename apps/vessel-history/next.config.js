fd10ea0a81a512072ed6298a1cf07bfe9e64614a
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2022-01-05 16:41:36 -0300

    cleanup next-pwa configuration

--- apps/vessel-history/next.config.js
+++ apps/vessel-history/next.config.js
@@ -57,27 +57,28 @@ module.exports = withPlugins(
       withPWA,
       {
         pwa: {
-          // disable: process.env.NODE_ENV === 'development',
-          // register: true,
+          // next-pwa Configuration
+          // see https://github.com/shadowwalker/next-pwa#configuration for more details
           dest: 'public',
-          // customWorkerDir: 'offline',
           buildExcludes: [/middleware-manifest.json$/],
           runtimeCaching,
           scope: '/vessel-viewer',
+
+          // Given that next-pwa only looks inside the public folder (relative path), when compiling from a
+          // different working directory other than the apps, the files are not found and not precached.
+          // So we need to build the list of compiled files to include and pass it to next-pwa
+          // (https://dev.to/sfiquet/precaching-pages-with-next-pwa-31f2)
           additionalManifestEntries: [
             ...getStaticPrecacheEntries({
               ...nextConfig,
               // exclude icon-related files from the precache since they are platform specific
               // note: no need to pass publicExcludes to next-pwa, it's not used for anything else
               publicExcludes: ['!*.png', '!*.ico', '!browserconfig.xml'],
-              // set the public folder path
+              // set the public folder path (Very Important, otherwise it does not include
+              // static files to precache)
               publicPath: `${cwd()}/apps/vessel-history/public`,
             }),
           ],
-          // dynamicStartUrl: true,
-          // scope: '/vessel-viewer',
-          // sw: 'sw.js',
-          //...
         },
         workbox: {
           // swSrc: 'offline/service-worker.ts',
