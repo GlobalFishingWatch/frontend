a9f53b6a796518376e9a3b2c199316d56d53fce8
j8seangel <jseangel@gmail.com>
2022-01-07 17:16:07 +0100

    fix vv build

--- apps/vessel-history/features/profile/components/GuideAndVideo.tsx
+++ apps/vessel-history/features/profile/components/GuideAndVideo.tsx
@@ -3,33 +3,46 @@ import { event as uaEvent } from 'react-ga'
 import { useTranslation } from 'react-i18next'
 import styles from './Info.module.css'
 
-
 interface GuideAndVideoProps {
-    source?: string
+  source?: string
 }
-const GuideAndVideo: React.FC<GuideAndVideoProps> = ({
-    source = '',
-}): React.ReactElement => {
-    const { t } = useTranslation()
+const GuideAndVideo: React.FC<GuideAndVideoProps> = ({ source = '' }): React.ReactElement => {
+  const { t } = useTranslation()
 
-    const trackClick = useCallback((link) => {
-        uaEvent({
-            category: 'General VV features',
-            action: `User click on ${link}`,
-            label: source,
-        })
-    }, [])
-    return (
-        <div>
-            <a className={styles.faqButton} onClick={trackClick('How to Guide')} target="_blank" href={t('common.howToGuideLink', 'https://www.localized-link-to-guide.com')} rel="noreferrer">
-                {t('common.howToGuide', 'How to Guide')}
-            </a>
-            {' | '}
-            <a className={styles.faqButton} onClick={trackClick('Basic Video')} target="_blank" href={t('common.basicVideoLink', 'https://www.localized-link-to-video.com')} rel="noreferrer">
-                {t('common.basicVideo', 'Basic video')}
-            </a>
-        </div>
-    )
+  const trackClick = useCallback(
+    (link) => {
+      uaEvent({
+        category: 'General VV features',
+        action: `User click on ${link}`,
+        label: source,
+      })
+    },
+    [source]
+  )
+
+  return (
+    <div>
+      <a
+        className={styles.faqButton}
+        onClick={() => trackClick('How to Guide')}
+        target="_blank"
+        href={t('common.howToGuideLink', 'https://www.localized-link-to-guide.com')}
+        rel="noreferrer"
+      >
+        {t('common.howToGuide', 'How to Guide')}
+      </a>
+      {' | '}
+      <a
+        className={styles.faqButton}
+        onClick={() => trackClick('Basic Video')}
+        target="_blank"
+        href={t('common.basicVideoLink', 'https://www.localized-link-to-video.com')}
+        rel="noreferrer"
+      >
+        {t('common.basicVideo', 'Basic video')}
+      </a>
+    </div>
+  )
 }
 
 export default GuideAndVideo
