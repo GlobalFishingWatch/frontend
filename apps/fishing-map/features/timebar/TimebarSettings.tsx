597ce244cb87326eb1c97783e183228400731a48
erik <erik@satellitestud.io>
2022-01-11 17:44:45 +0100

    added icons

--- apps/fishing-map/features/timebar/TimebarSettings.tsx
+++ apps/fishing-map/features/timebar/TimebarSettings.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { Fragment, useState, ComponentType } from 'react'
 import cx from 'classnames'
 import { useSelector } from 'react-redux'
 import { event as uaEvent } from 'react-ga'
@@ -13,8 +13,39 @@ import {
   selectActiveTrackDataviews,
   selectActiveVesselsDataviews,
 } from 'features/dataviews/dataviews.slice'
+import { ReactComponent as AreaIcon } from 'assets/icons/timebar-area.svg'
+import { ReactComponent as TracksIcon } from 'assets/icons/timebar-tracks.svg'
+import { ReactComponent as TrackSpeedIcon } from 'assets/icons/timebar-track-speed.svg'
+import { ReactComponent as TrackDepthIcon } from 'assets/icons/timebar-track-depth.svg'
+import { COLOR_PRIMARY_BLUE } from 'features/app/App'
 import { useTimebarVisualisationConnect, useTimebarGraphConnect } from './timebar.hooks'
 import styles from './TimebarSettings.module.css'
+
+const Icon = ({
+  SvgIcon,
+  label,
+  color,
+  disabled,
+}: {
+  SvgIcon: ComponentType
+  label: string
+  color: string
+  disabled: boolean
+}) => {
+  const svgProps = {
+    fill: color,
+    stroke: color,
+  }
+  return (
+    <Fragment>
+      <SvgIcon
+        className={cx(styles.icon, { [styles.iconDisabled]: disabled })}
+        {...(svgProps as any)}
+      />
+      {label}
+    </Fragment>
+  )
+}
 
 const TimebarSettings = () => {
   const { t } = useTranslation()
@@ -89,22 +120,36 @@ const TimebarSettings = () => {
       />
       {optionsPanelOpen && (
         <div className={styles.optionsContainer}>
-          <h1>Timebar settings</h1>
+          <h1>{t('timebarSettings.title', 'Timebar settings')}</h1>
           <div className={styles.radiosContainer}>
             <Radio
-              label={activityLabel}
-              active={timebarVisualisation === TimebarVisualisations.Heatmap}
+              label={
+                <Icon
+                  SvgIcon={AreaIcon}
+                  label={activityLabel}
+                  color={activeHeatmapDataviews[0]?.config.color || COLOR_PRIMARY_BLUE}
+                  disabled={!activeHeatmapDataviews?.length}
+                />
+              }
               disabled={!activeHeatmapDataviews?.length}
+              active={timebarVisualisation === TimebarVisualisations.Heatmap}
               tooltip={activityTooltipLabel}
               onClick={setHeatmapActive}
             />
             <Radio
-              label={t('timebarSettings.tracks', 'Tracks')}
+              label={
+                <Icon
+                  SvgIcon={TracksIcon}
+                  label={t('timebarSettings.tracks', 'Tracks')}
+                  color={activeTrackDataviews[0]?.config.color || COLOR_PRIMARY_BLUE}
+                  disabled={!activeTrackDataviews?.length}
+                />
+              }
+              disabled={!activeTrackDataviews?.length}
               active={
                 timebarVisualisation === TimebarVisualisations.Vessel &&
                 (timebarGraph === TimebarGraphs.None || !timebarGraphEnabled)
               }
-              disabled={!activeTrackDataviews?.length}
               tooltip={
                 !activeTrackDataviews?.length
                   ? t('timebarSettings.tracksDisabled', 'Select at least one vessel')
@@ -113,13 +158,20 @@ const TimebarSettings = () => {
               onClick={setVesselActive}
             />
             <Radio
-              label={t('timebarSettings.graphSpeed', 'Track Speed')}
+              label={
+                <Icon
+                  SvgIcon={TrackSpeedIcon}
+                  label={t('timebarSettings.graphSpeed', 'Track Speed')}
+                  color={activeTrackDataviews[0]?.config.color || COLOR_PRIMARY_BLUE}
+                  disabled={!activeTrackDataviews?.length || !timebarGraphEnabled}
+                />
+              }
+              disabled={!activeTrackDataviews?.length || !timebarGraphEnabled}
               active={
                 timebarVisualisation === TimebarVisualisations.Vessel &&
                 timebarGraph === TimebarGraphs.Speed &&
                 timebarGraphEnabled
               }
-              disabled={!activeTrackDataviews?.length || !timebarGraphEnabled}
               tooltip={
                 !activeTrackDataviews?.length
                   ? t('timebarSettings.tracksDisabled', 'Select at least one vessel')
@@ -133,13 +185,20 @@ const TimebarSettings = () => {
               onClick={setVesselGraphSpeed}
             />
             <Radio
-              label={t('timebarSettings.graphDepth', 'Track Depth')}
+              label={
+                <Icon
+                  SvgIcon={TrackDepthIcon}
+                  label={t('timebarSettings.graphDepth', 'Track Depth')}
+                  color={activeTrackDataviews[0]?.config.color || COLOR_PRIMARY_BLUE}
+                  disabled={!activeTrackDataviews?.length || !timebarGraphEnabled}
+                />
+              }
+              disabled={!activeTrackDataviews?.length || !timebarGraphEnabled}
               active={
                 timebarVisualisation === TimebarVisualisations.Vessel &&
                 timebarGraph === TimebarGraphs.Depth &&
                 timebarGraphEnabled
               }
-              disabled={!activeTrackDataviews?.length || !timebarGraphEnabled}
               tooltip={
                 !activeTrackDataviews?.length
                   ? t('timebarSettings.tracksDisabled', 'Select at least one vessel')
