cb516a9ec289a890da7ed2169fe4754301500e47
j8seangel <jseangel@gmail.com>
2022-01-11 11:17:03 +0100

    simplify getSourceMetadata

--- apps/fishing-map/features/map/map-sources.utils.ts
+++ apps/fishing-map/features/map/map-sources.utils.ts
@@ -1,9 +1,5 @@
 import { ExtendedStyle } from '@globalfishingwatch/layer-composer'
 
 export const getSourceMetadata = (style: ExtendedStyle, id: string) => {
-  const generatorsMetadata = style?.metadata?.generatorsMetadata
-  if (!generatorsMetadata) return null
-
-  const activityHeatmapMetadata = generatorsMetadata[id]
-  return activityHeatmapMetadata
+  return style?.metadata?.generatorsMetadata?.[id]
 }
