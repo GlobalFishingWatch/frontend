b00476ac3a6fd5891510e3b5c3152046ac2827c8
j8seangel <jseangel@gmail.com>
2022-02-02 17:48:02 +0100

    fix hooks build

--- libs/react-hooks/src/use-memo-compare/use-memo-compare.ts
+++ libs/react-hooks/src/use-memo-compare/use-memo-compare.ts
@@ -2,21 +2,21 @@ import { useEffect, useRef } from 'react'
 import { isEqual } from 'lodash'
 
 // https://usehooks.com/useMemoCompare/
-export function useMemoCompare<N>(next: N, compare = isEqual) {
+export function useMemoCompare<N = unknown>(next: N, compare = isEqual) {
   // Ref for storing previous value
   const previousRef = useRef<N>()
   const previous = previousRef.current
   // Pass previous and next value to compare function
   // to determine whether to consider them equal.
-  const isEqual = compare(previous, next)
+  const equal = compare(previous, next)
   // If not equal update previousRef to next value.
   // We only update if not equal so that this hook continues to return
   // the same old value if compare keeps returning true.
   useEffect(() => {
-    if (!isEqual) {
+    if (!equal) {
       previousRef.current = next
     }
   })
   // Finally, if equal then return the previous value
-  return isEqual ? previous : next
+  return equal ? previous : next
 }
