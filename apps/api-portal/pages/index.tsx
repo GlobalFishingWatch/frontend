da757c8c5329093fc876ef2b1d3dfb0168d1aa9d
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2022-02-02 14:38:01 -0300

    Fixed styles

--- apps/api-portal/pages/index.tsx
+++ apps/api-portal/pages/index.tsx
@@ -10,7 +10,7 @@ const Layout = dynamic(() => import('components/layout'), {
 const Home: NextPage = () => {
   return (
     <Layout>
-      <p>
+      <p className={styles.description}>
         You need an acccess token to call Global Fishing Watch API endpoints like Vessel search or
         4wings activity tiles. Read more about API access tokens in{' '}
         <a href="#api-documentation">our documentation</a>
