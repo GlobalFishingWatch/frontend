fc73347c1dd4853d81d1d65adb8f822338661823
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2022-02-22 18:22:19 -0300

    Remove unused fields terms and intended use

--- apps/api-portal/components/access-token/access-token-create/access-token-create.tsx
+++ apps/api-portal/components/access-token/access-token-create/access-token-create.tsx
@@ -1,12 +1,6 @@
 import { useCallback, useState, Fragment } from 'react'
 import cx from 'classnames'
-import {
-  Button,
-  Checkbox,
-  InputText,
-  Select,
-  SelectOption,
-} from '@globalfishingwatch/ui-components'
+import { Button, InputText } from '@globalfishingwatch/ui-components'
 import { useCreateUserApplications } from 'features/user-applications/user-applications.hooks'
 import { UserApplicationCreateArguments } from 'features/user-applications/user-applications.slice'
 import styles from './access-token-create.module.css'
@@ -26,19 +20,6 @@ export function AccessTokenCreate(props: AccessTokenCreateProps) {
   const { isAllowed, dispatchCreate, isSaving, isError } = useCreateUserApplications()
 
   const [token, setToken] = useState<UserApplicationCreateArguments>(emptyToken)
-  const displayIntendedUse = false
-  const displayAcceptTerms = false
-
-  const intendedUseOptions: SelectOption[] = [
-    {
-      id: 'commercial',
-      label: 'Commercial',
-    },
-    {
-      id: 'non-commercial',
-      label: 'Non Commercial',
-    },
-  ]
 
   const create = useCallback(async () => {
     const response = await dispatchCreate({
@@ -71,44 +52,6 @@ export function AccessTokenCreate(props: AccessTokenCreateProps) {
                 onChange={(e) => setToken({ ...token, name: e.target.value })}
               />
             </div>
-            {displayIntendedUse && (
-              <div className={styles.field}>
-                <Select
-                  label={'Intended Use'}
-                  placeholder={'Select an option'}
-                  options={intendedUseOptions}
-                  className={styles.select}
-                  containerClassName={styles.selectContainer}
-                  selectedOption={intendedUseOptions.find(
-                    (option) => option.id === token?.intendedUse
-                  )}
-                  onSelect={(selected) => {
-                    setToken({ ...token, intendedUse: selected.id })
-                  }}
-                  onRemove={() => {
-                    setToken({ ...token, intendedUse: null })
-                  }}
-                />
-              </div>
-            )}
-            {displayAcceptTerms && (
-              <div className={styles.fieldsRow}>
-                <div className={styles.field}>
-                  <Checkbox
-                    label="I agree to the Global Fishing Watch API Terms of Use and Attribution. If I am
-                registering for use by an organization, I represent that I have the authority to
-                bind that organization to these terms."
-                    active={!!token?.termsAccepted}
-                    onClick={() =>
-                      setToken({
-                        ...token,
-                        termsAccepted: !token?.termsAccepted,
-                      })
-                    }
-                  />
-                </div>
-              </div>
-            )}
             <div className={cx([styles.field, styles.fieldsColumn])}>
               <label className={styles.label} htmlFor="new-token-description">
                 Description
