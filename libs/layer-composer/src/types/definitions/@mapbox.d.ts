31199c8cff98fc012e7f69c6caa03fe023c804ce
j8seangel <jseangel@gmail.com>
2022-03-01 13:09:25 +0100

    fix maplibre-gl-style-spec validation

--- libs/layer-composer/src/types/definitions/@mapbox.d.ts
+++ libs/layer-composer/src/types/definitions/@mapbox.d.ts
@@ -1,4 +1,4 @@
-declare module '@globalfishingwatch/maplibre-gl-style-spec'
+declare module '@globalfishingwatch/maplibre-gl/dist/style-spec'
 declare module '@mapbox/vector-tile'
 declare module '@mapbox/tilebelt'
 declare module '@mapbox/geojsonhint'
