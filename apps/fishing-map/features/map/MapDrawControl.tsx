36f5a792e266c3025a9fec13f5a2212e59545dac
j8seangel <jseangel@gmail.com>
2022-03-17 12:28:30 +0100

    style draw layers

--- apps/fishing-map/features/map/MapDrawControl.tsx
+++ apps/fishing-map/features/map/MapDrawControl.tsx
@@ -24,7 +24,6 @@ type DrawControlProps = {
   touchBuffer?: number
   controls?: Partial<{ [name in ControlTypes]: boolean }>
   displayControlsDefault?: boolean
-  styles?: any
   modes?: any
   defaultMode?: string
   userProperties?: boolean
@@ -38,6 +37,106 @@ type DrawControlProps = {
 }
 
 const defaultFn = () => {}
+const styles = [
+  // line stroke
+  {
+    id: 'gl-draw-line',
+    type: 'line',
+    filter: ['all', ['==', '$type', 'LineString'], ['!=', 'mode', 'static']],
+    layout: {
+      'line-cap': 'round',
+      'line-join': 'round',
+    },
+    paint: {
+      'line-color': 'rgb(38, 181, 242)',
+      'line-dasharray': [0.2, 2],
+      'line-width': 2,
+    },
+  },
+  // polygon fill
+  {
+    id: 'gl-draw-polygon-fill',
+    type: 'fill',
+    filter: ['all', ['==', '$type', 'Polygon'], ['!=', 'mode', 'static']],
+    paint: {
+      'fill-color': 'rgb(189,189,189)',
+      'fill-outline-color': 'transparent',
+      'fill-opacity': 0.1,
+    },
+  },
+  // polygon mid points
+  {
+    id: 'gl-draw-polygon-midpoint',
+    type: 'circle',
+    filter: ['all', ['==', '$type', 'Point'], ['==', 'meta', 'midpoint']],
+    paint: {
+      'circle-radius': 3,
+      'circle-color': 'rgb(122,202,67)',
+    },
+  },
+  // polygon outline stroke
+  // This doesn't style the first edge of the polygon, which uses the line stroke styling instead
+  {
+    id: 'gl-draw-polygon-stroke-active',
+    type: 'line',
+    filter: ['all', ['==', '$type', 'Polygon'], ['!=', 'mode', 'static']],
+    layout: {
+      'line-cap': 'round',
+      'line-join': 'round',
+    },
+    paint: {
+      'line-color': 'rgb(38, 181, 242)',
+      'line-dasharray': [0.2, 2],
+      'line-width': 2,
+    },
+  },
+  // vertex point halos
+  {
+    id: 'gl-draw-polygon-and-line-vertex-halo-active',
+    type: 'circle',
+    filter: ['all', ['==', 'meta', 'vertex'], ['==', '$type', 'Point'], ['!=', 'mode', 'static']],
+    paint: {
+      'circle-radius': 4,
+      'circle-color': '#FFF',
+    },
+  },
+  // vertex points
+  {
+    id: 'gl-draw-polygon-and-line-vertex-active',
+    type: 'circle',
+    filter: ['all', ['==', 'meta', 'vertex'], ['==', '$type', 'Point'], ['!=', 'mode', 'static']],
+    paint: {
+      'circle-radius': 3,
+      'circle-color': 'rgb(122,202,67)',
+    },
+  },
+  // polygon fill
+  {
+    id: 'gl-draw-polygon-fill-active',
+    type: 'fill',
+    filter: ['all', ['==', '$type', 'Polygon'], ['==', 'mode', 'direct_select']],
+    paint: {
+      'fill-color': 'rgb(189,189,189)',
+      'fill-outline-color': 'transparent',
+      'fill-opacity': 0.3,
+    },
+  },
+  // polygon outline
+  {
+    id: 'gl-draw-polygon-stroke-static',
+    type: 'line',
+    filter: ['all', ['==', '$type', 'Polygon'], ['==', 'mode', 'static']],
+    layout: {
+      'line-cap': 'round',
+      'line-join': 'round',
+    },
+    paint: {
+      'line-color': '#000',
+      'line-width': 3,
+    },
+  },
+]
+
 export default function useDrawControl(props: DrawControlProps) {
   const {
     onCreate = defaultFn,
@@ -51,7 +150,8 @@ export default function useDrawControl(props: DrawControlProps) {
       map.on('draw.update', onUpdate)
       map.on('draw.modechange', onModeChange)
       map.on('draw.selectionchange', onSelectionChange)
-      return new MapboxDraw(props)
+      window.mapap = map
+      return new MapboxDraw({ ...props, styles })
     },
     ({ map }: { map: MapRef }) => {
       map.off('draw.create', onCreate)
