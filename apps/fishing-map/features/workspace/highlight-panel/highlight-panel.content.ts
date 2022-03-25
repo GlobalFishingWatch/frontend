831220804d96e7414537f8a58ba3ac0f8073be72
Rodrigo Fuentes <rdgfuentes@users.noreply.github.com>
2022-03-25 12:13:30 -0300

    Belize VMS Highlight popup. MAP-793 (#1314)
    * Setup Belize VMS popup
    
    * Updated Belize popup description in pt
    
    * updated belize highlight popup description in fr
    
    * Use HIGHLIGHT_DATAVIEW_INSTANCE_ID in prod workspace
    
    * Updated belize vms popup image

--- apps/fishing-map/features/workspace/highlight-panel/highlight-panel.content.ts
+++ apps/fishing-map/features/workspace/highlight-panel/highlight-panel.content.ts
@@ -2,7 +2,7 @@ import { PATH_BASENAME } from 'routes/routes'
 import { Locale } from 'types'
 
 // Update with a new id when a new release
-export const HIGHLIGHT_DATAVIEW_INSTANCE_ID = 'highlight-vms-with-costarica'
+export const HIGHLIGHT_DATAVIEW_INSTANCE_ID = 'highlight-vms-with-belize'
 
 type HighlighPanelConfigLocale = {
   [locale in Locale]?: {
@@ -20,28 +20,28 @@ export type HighlighPanelConfig = {
 
 const highlightPanelConfig: HighlighPanelConfig = {
   dataviewInstanceId: HIGHLIGHT_DATAVIEW_INSTANCE_ID,
-  imageUrl: `${PATH_BASENAME}/images/costa-rica-vms.jpg`,
-  learnMoreUrl: 'https://globalfishingwatch.org/costa-rica',
+  imageUrl: `${PATH_BASENAME}/images/belize-vms.jpg`,
+  learnMoreUrl: 'https://globalfishingwatch.org/belize',
   en: {
-    title: 'Costa Rica VMS data is now available',
+    title: 'Belize VMS data is now available',
     description:
-      'Vessel monitoring system (VMS) data is provided by the Costa Rican Fisheries and Aquaculture Institute. It is possible to track the position and activity of 100+ vessels comprising Costa Rica’s industrial and international fishing fleets.',
+      'Vessel monitoring system (VMS) data is provided by the Belize High Seas Fisheries Unit (BHSFU). It is possible to track the position and activity of 50+ vessels comprising Belize’s international fishing fleets.',
   },
   es: {
-    title: 'Los datos de VMS de Costa Rica ya están disponibles',
+    title: 'Los datos de VMS de Belize ya están disponibles',
     description:
-      'Los datos del sistema de seguimiento de embarcaciones (VMS) son proporcionados por el Instituto Costarricense de Pesca y Acuicultura. Es posible rastrear la posición y actividad de más de 100 embarcaciones que comprenden las flotas pesqueras industriales e internacionales de Costa Rica.',
-    learnMoreUrl: 'https://globalfishingwatch.org/es/costa-rica',
+      'Los datos del sistema de seguimiento de embarcaciones (VMS) son proporcionados por la Unidad de Pesca de Alta Mar de Belice (BHSFU, por sus siglas en inglés). Es posible rastrear la posición y actividad de más de 50 embarcaciones que comprenden las flotas pesqueras internacionales de Belice.',
+    learnMoreUrl: 'https://globalfishingwatch.org/es/belize',
   },
   fr: {
-    title: 'Les données VMS du Costa Rica sont maintenant disponibles',
+    title: 'Les données VMS du Bélize sont maintenant disponibles',
     description:
-      "Les données du système de suivi des navires par satellite (VMS) sont fournies par l'Institut costaricien des pêches et de l'aquaculture. Il est possible de suivre la position et l'activité de plus de 100 navires qui composent les flottes industrielles et internationales de pêche du Costa Rica.",
+      "Les données du système de suivi des navires par satellite (VMS) sont fournies par Belize High Seas Fisheries Unit (BHSFU). Il est possible de suivre la position et l'activité de plus de 50 navires qui composent la flotte de pêche en haute mer de Bélize.",
   },
   pt: {
-    title: 'Rastreamento de embarcações da Costa Rica está disponível',
+    title: 'Rastreamento de embarcações da Belice está disponível',
     description:
-      'Os dados de rastreamento de embarcações de pesca são fornecidos pelo Instituto Costarriquenho de Pesca e Aquicultura. É possível visualizar a rota e as atividades de mais de 100 embarcações industriais e artesanais.',
+      'Os dados de rastreamento de embarcações de pesca são fornecidos pela Unidade de Pesca de Alto Mar de Belize. É possível visualizar a rota e as atividades de mais de 50 embarcações da frota pesqueira internacional de Belize.',
   },
 }
 
