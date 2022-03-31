ef46f67c4f213a8e2552ca543b38c199267d2378
Erik Escoffier <erik@satellitestud.io>
2022-03-31 15:06:45 +0200

    rebased master

--- apps/fishing-map/features/workspace/highlight-panel/highlight-panel.content.ts
+++ apps/fishing-map/features/workspace/highlight-panel/highlight-panel.content.ts
@@ -21,24 +21,29 @@ export type HighlighPanelConfig = {
 
 const highlightPanelConfig: HighlighPanelConfig = {
   dataviewInstanceId: HIGHLIGHT_DATAVIEW_INSTANCE_ID,
-  imageUrl: `${PATH_BASENAME}/TODO`,
+  delayed: true,
+  imageUrl: `${PATH_BASENAME}/images/viirs-match.webp`,
   learnMoreUrl: 'https://globalfishingwatch.org/belize',
   en: {
-    title: 'TODO',
-    description: 'TODO',
+    title: 'New VIIRS-AIS match layer',
+    description:
+      'Identity of fishing vessels using bright lights at night added through advanced matching process.',
   },
   es: {
-    title: 'TODO',
-    description: 'TODO',
+    title: 'New VIIRS-AIS match layer',
+    description:
+      'Identity of fishing vessels using bright lights at night added through advanced matching process.',
     learnMoreUrl: 'https://globalfishingwatch.org/es/belize',
   },
   fr: {
-    title: 'TODO',
-    description: 'TODO',
+    title: 'New VIIRS-AIS match layer',
+    description:
+      'Identity of fishing vessels using bright lights at night added through advanced matching process.',
   },
   pt: {
-    title: 'TODO',
-    description: 'TODO',
+    title: 'New VIIRS-AIS match layer',
+    description:
+      'Identity of fishing vessels using bright lights at night added through advanced matching process.',
   },
 }
 
