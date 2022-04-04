b2c53afb8a63bc28199e07c723084a01bcf56f48
Erik Escoffier <erik@satellitestud.io>
2022-04-04 11:47:23 +0200

    added second highlight popup

--- apps/fishing-map/features/workspace/highlight-panel/highlight-panel.content.ts
+++ apps/fishing-map/features/workspace/highlight-panel/highlight-panel.content.ts
@@ -12,16 +12,21 @@ type HighlighPanelConfigLocale = {
   }
 }
 
-export type HighlighPanelConfig = {
+export type HighlightPanelConfig = {
   dataviewInstanceId: string
+  localStorageKey: string
   imageUrl: string
   learnMoreUrl: string
   delayed?: boolean
+  workspaceUrl?: string
 } & HighlighPanelConfigLocale
 
-const highlightPanelConfig: HighlighPanelConfig = {
+const HIGHLIGHT_POPUP_KEY = 'HighlightPopup'
+
+const DEFAULT_HIGHLIGHT_CONFIG: HighlightPanelConfig = {
   dataviewInstanceId: HIGHLIGHT_DATAVIEW_INSTANCE_ID,
-  delayed: true,
+  localStorageKey: HIGHLIGHT_POPUP_KEY,
+  // delayed: true,
   imageUrl: `${PATH_BASENAME}/images/viirs-match.webp`,
   learnMoreUrl: 'https://globalfishingwatch.org/belize',
   en: {
@@ -32,7 +37,7 @@ const highlightPanelConfig: HighlighPanelConfig = {
   es: {
     title: 'New VIIRS-AIS match layer',
     description:
-      'Identity of fishing vessels using bright lights at night added through advanced matching process.',
+      'La identidad de los buques pesqueros que usan luces brillantes por la noche se agrega a través de un proceso de comparación avanzado.',
     learnMoreUrl: 'https://globalfishingwatch.org/es/belize',
   },
   fr: {
@@ -47,4 +52,13 @@ const highlightPanelConfig: HighlighPanelConfig = {
   },
 }
 
-export default highlightPanelConfig
+export const HIGHLIGHT_PANEL_CONFIG_ACTIVITY_SWITCH_ID = `${HIGHLIGHT_POPUP_KEY}_ActivitySwtch`
+export const HIGHLIGHT_PANEL_CONFIG_ACTIVITY_SWITCH: HighlightPanelConfig = {
+  ...DEFAULT_HIGHLIGHT_CONFIG,
+  dataviewInstanceId: HIGHLIGHT_PANEL_CONFIG_ACTIVITY_SWITCH_ID,
+  localStorageKey: HIGHLIGHT_PANEL_CONFIG_ACTIVITY_SWITCH_ID,
+  workspaceUrl:
+    '?start=2022-01-01T01%3A00%3A00.000Z&end=2022-04-01T00%3A00%3A00.000Z&latitude=19&longitude=26&zoom=1.5&activityCategory=~0&dvIn[0][id]=~0&dvIn[0][cfg][vis]=false&dvIn[1][id]=highlight-viirs-match&dvIn[1][cfg][vis]=true&tk[0]=presence',
+}
+
+export default DEFAULT_HIGHLIGHT_CONFIG
