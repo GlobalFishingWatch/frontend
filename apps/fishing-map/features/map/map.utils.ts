86a6c92eba85eda94d0d94b517eadd5b5ef98111
j8seangel <jseangel@gmail.com>
2022-04-27 13:08:29 +0200

    fix missing features filtered by viewport in timebar

--- apps/fishing-map/features/map/map.utils.ts
+++ apps/fishing-map/features/map/map.utils.ts
@@ -16,17 +16,20 @@ export const filterByViewport = <P = unknown>(
     return []
   }
   const { north, east, south, west } = bounds
-  const leftWorldCopy = east >= 180
-  const rightWorldCopy = west <= -180
+  const rightWorldCopy = east >= 180
+  const leftWorldCopy = west <= -180
   return features.filter((f) => {
     const [lon, lat] = (f.geometry as any).coordinates[0][0]
-    const rightOffset = rightWorldCopy && !leftWorldCopy && lon > 0 ? -360 : 0
-    const leftOffset = leftWorldCopy && !rightWorldCopy && lon < 0 ? 360 : 0
-    return (
-      lon + rightOffset + leftOffset > west &&
-      lon + rightOffset + leftOffset < east &&
-      lat > south &&
-      lat < north
-    )
+    if (lat < south || lat > north) {
+      return false
+    }
+    // This tries to translate features longitude for a proper comparison against the viewport
+    // when they fall in a left or right copy of the world but not in the center one
+    // but... https://c.tenor.com/YwSmqv2CZr8AAAAd/dog-mechanic.gif
+    const featureInLeftCopy = lon > 0 && lon - 360 >= west
+    const featureInRightCopy = lon < 0 && lon + 360 <= east
+    const leftOffset = leftWorldCopy && !rightWorldCopy && featureInLeftCopy ? -360 : 0
+    const rightOffset = rightWorldCopy && !leftWorldCopy && featureInRightCopy ? 360 : 0
+    return lon + leftOffset + rightOffset > west && lon + leftOffset + rightOffset < east
   })
 }
