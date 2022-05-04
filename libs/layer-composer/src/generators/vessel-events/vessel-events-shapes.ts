b2153bce5df23e540168c0559d5d5ddef4df53ee
satellitestudiodesign <juancarlos@satellitestud.io>
2022-05-04 11:19:34 +0200

    sort highlighted fishing events

--- libs/layer-composer/src/generators/vessel-events/vessel-events-shapes.ts
+++ libs/layer-composer/src/generators/vessel-events/vessel-events-shapes.ts
@@ -127,6 +127,9 @@ class VesselsEventsShapesGenerator {
       type: 'circle',
       id: `${config.id}_fishingEvents`,
       source: `${config.id}_fishingEvents`,
+      layout: {
+        'circle-sort-key': getExpression(1, 0),
+      },
       paint: {
         'circle-color': ['get', 'color'],
         'circle-radius': [
