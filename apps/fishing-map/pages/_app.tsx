28464a93fed4c4db71d9462d5bafdb160d78df6f
j8seangel <jseangel@gmail.com>
2022-05-10 15:18:35 +0200

    update app dependencies

--- apps/fishing-map/pages/_app.tsx
+++ apps/fishing-map/pages/_app.tsx
@@ -3,19 +3,32 @@ import { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { RecoilRoot } from 'recoil'
 import { MapProvider } from 'react-map-gl'
+// import dynamic from 'next/dynamic'
+// import { useEffect, useState } from 'react'
 import store from '../store'
+
 import './styles.css'
 import '../../../libs/ui-components/src/base.css'
 import '../../../libs/timebar/src/timebar-settings.css'
 import '@globalfishingwatch/maplibre-gl/dist/maplibre-gl.css'
+
+// const RecoilizeDebugger = dynamic(() => import('recoilize'), { ssr: false })
 
 // function SafeHydrate({ children }) {
 //   return <div suppressHydrationWarning>{typeof window === 'undefined' ? null : children}</div>
 // }
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  // const [root, setRoot] = useState(null)
+  // useEffect(() => {
+  //   if (typeof window.document !== 'undefined') {
+  //     setRoot(document.getElementById('__next'))
+  //   }
+  // }, [root])
+
   return (
     <RecoilRoot>
+      {/* <RecoilizeDebugger root={root} /> */}
       <Provider store={store}>
         <MapProvider>
           <Component {...pageProps} />
