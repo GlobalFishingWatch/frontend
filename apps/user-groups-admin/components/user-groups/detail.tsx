a32e8dc7831fb99bd7e8fb5f3a55c155ab033e28
j8seangel <jseangel@gmail.com>
2022-05-11 12:17:26 +0200

    polish styles

--- apps/user-groups-admin/components/user-groups/detail.tsx
+++ apps/user-groups-admin/components/user-groups/detail.tsx
@@ -56,6 +56,7 @@ export function UserGroupDetail({ groupId }: { groupId: number }) {
     return () => {
       setEmail('')
       setGroup(undefined)
+      setFutureUsers(undefined)
     }
   }, [fetchGroup, groupId])
 
