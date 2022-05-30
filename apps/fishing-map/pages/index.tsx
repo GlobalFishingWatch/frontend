708519d80eb0e264dc365f534b8e728426b1319d
j8seangel <jseangel@gmail.com>
2022-05-30 21:18:51 +0200

    don't load MapProvider in ssr

--- apps/fishing-map/pages/index.tsx
+++ apps/fishing-map/pages/index.tsx
@@ -1,10 +1,18 @@
 import dynamic from 'next/dynamic'
 
+const MapProvider = dynamic(() => import('react-map-gl').then((module) => module.MapProvider), {
+  ssr: false,
+})
+
 const AppNoSSRComponent = dynamic(() => import('../features/app/App'), {
   ssr: false,
 })
 
 const Index = () => {
-  return <AppNoSSRComponent />
+  return (
+    <MapProvider>
+      <AppNoSSRComponent />
+    </MapProvider>
+  )
 }
 export default Index
