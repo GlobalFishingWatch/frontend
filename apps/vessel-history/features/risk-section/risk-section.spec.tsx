6157ea325f0a4f2a03bf2b0787eb553a7f13b612
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2022-05-31 16:54:58 -0300

    Removed unused tests

--- apps/vessel-history/features/risk-section/risk-section.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import RiskSection from './risk-section'
-
-describe('RiskSection', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<RiskSection />)
-    expect(baseElement).toBeTruthy()
-  })
-})
--- apps/vessel-history/features/risk-summary/risk-summary.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import RiskSummary from './risk-summary'
-
-describe('RiskSummary', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<RiskSummary />)
-    expect(baseElement).toBeTruthy()
-  })
-})
