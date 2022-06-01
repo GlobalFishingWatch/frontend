81502c657b16af7d93e416a99f98e13f5c56aa99
j8seangel <jseangel@gmail.com>
2022-06-01 12:31:22 +0200

    replace stats proto to v1

--- apps/fishing-map/queries/stats-api.ts
+++ apps/fishing-map/queries/stats-api.ts
@@ -38,7 +38,7 @@ export const dataviewStatsApi = createApi({
   reducerPath: 'dataviewStatsApi',
   serializeQueryArgs: serializeStatsDataviewKey,
   baseQuery: gfwBaseQuery({
-    baseUrl: '/proto/4wings/stats',
+    baseUrl: '/v1/4wings/stats',
   }),
   endpoints: (builder) => ({
     getStatsByDataview: builder.query<StatFields, FetchDataviewStatsParams>({
