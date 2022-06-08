8f6d45729b4ade6bdd6467078c8cb57caee90576
j8seangel <jseangel@gmail.com>
2022-06-08 14:10:19 +0200

    fix bivariate on layer title switch

--- apps/fishing-map/features/workspace/activity/ActivityLayerPanel.tsx
+++ apps/fishing-map/features/workspace/activity/ActivityLayerPanel.tsx
@@ -131,7 +131,7 @@ function ActivityLayerPanel({
       className={styles.name}
       classNameActive={styles.active}
       dataview={dataview}
-      onToggle={onToggle}
+      onToggle={onLayerSwitchToggle}
     />
   )
 
