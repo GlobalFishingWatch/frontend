663cf1feadce9a4a31519b727105317c3877d3ba
satellitestudiodesign <juancarlos@satellitestud.io>
2022-06-08 16:17:59 +0200

    remove unnecesary source label in analysis description

--- apps/fishing-map/features/analysis/analysisDescription.hooks.ts
+++ apps/fishing-map/features/analysis/analysisDescription.hooks.ts
@@ -43,8 +43,10 @@ const getCommonProperties = (dataviews?: UrlDataviewInstance[], showTimeComparis
     if (showTimeComparison) {
       titleChunks.push({ label: t('analysis.changeIn', 'Change in') })
     }
+    console.log(dataviews)
 
     if (dataviews?.every((dataview) => dataview.category === dataviews[0].category)) {
+      console.log('every')
       const activityDataview = isActivityDataview(dataviews[0])
       const detectionsDataview = isDetectionsDataview(dataviews[0])
       if (activityDataview || detectionsDataview) {
@@ -61,6 +63,7 @@ const getCommonProperties = (dataviews?: UrlDataviewInstance[], showTimeComparis
         }
         titleChunks.push({ label: mainLabel, strong: true })
       } else {
+        commonProperties.push('dataset')
         titleChunks.push({ label: dataviews[0].name || '', strong: true })
       }
     }
