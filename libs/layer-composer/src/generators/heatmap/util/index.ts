d642c95d58d0c398c5a6bc925be6108f8ba9e2b4
j8seangel <jseangel@gmail.com>
2022-06-21 12:59:27 +0200

    generate final url in generator side

--- libs/layer-composer/src/generators/heatmap/util/index.ts
+++ libs/layer-composer/src/generators/heatmap/util/index.ts
@@ -2,8 +2,8 @@ import { TimeChunk } from './time-chunks'
 
 export const toURLArray = (paramName: string, arr: string[]) => {
   return arr
-    .map((element, i) => {
-      if (!element) return ''
+    .flatMap((element, i) => {
+      if (!element) return []
       return `${paramName}[${i}]=${element}`
     })
     .join('&')
