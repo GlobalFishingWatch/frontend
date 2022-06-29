bd31a520c566ac0920caf1a51b0e7360f40f3c10
j8seangel <jseangel@gmail.com>
2022-06-29 13:30:55 +0200

    show disclaimer only for gfwUsers

--- apps/fishing-map/features/user/GFWOnly.tsx
+++ apps/fishing-map/features/user/GFWOnly.tsx
@@ -1,13 +1,16 @@
 import { useTranslation } from 'react-i18next'
+import { useSelector } from 'react-redux'
+import { isGFWUser } from 'features/user/user.slice'
 import styles from './GFWOnly.module.css'
 
 function GFWOnly() {
   const { t } = useTranslation()
-  return (
+  const gfwUser = useSelector(isGFWUser)
+  return gfwUser ? (
     <span className={styles.GFWOnly}>
       🐟 {t('common.onlyVisibleForGFW', 'Only visible for GFW users')}
     </span>
-  )
+  ) : null
 }
 
 export default GFWOnly
