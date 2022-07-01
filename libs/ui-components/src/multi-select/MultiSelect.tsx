e6f2b4c16374f83a694f5a364f3da66006e61c5d
j8seangel <jseangel@gmail.com>
2022-07-01 14:51:04 +0200

    fix build

--- libs/ui-components/src/multi-select/MultiSelect.tsx
+++ libs/ui-components/src/multi-select/MultiSelect.tsx
@@ -77,7 +77,7 @@ const getPlaceholderBySelections = (
         .join(', ')
     : selections.length > 1
     ? `${selections.length} selected`
-    : selections[0]?.label
+    : selections[0]?.label.toString()
 }
 
 const isItemSelected = (selectedItems: MultiSelectOption[], item: MultiSelectOption) => {
