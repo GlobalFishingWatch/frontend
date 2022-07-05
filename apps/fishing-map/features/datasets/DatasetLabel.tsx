fa44bb92b5e37ac11c0f9bcb658bfeaf2b7f1950
j8seangel <jseangel@gmail.com>
2022-07-05 15:25:25 +0200

    fix ref warning for dataset label tooltip

--- apps/fishing-map/features/datasets/DatasetLabel.tsx
+++ apps/fishing-map/features/datasets/DatasetLabel.tsx
@@ -1,11 +1,15 @@
+import { forwardRef, Ref } from 'react'
 import { getDatasetLabel, GFW_ONLY_SUFFIX } from 'features/datasets/datasets.utils'
 import GFWOnly from 'features/user/GFWOnly'
 
-function DatasetLabel({ dataset }: { dataset: { id: string; name?: string } }) {
+function DatasetLabel(
+  { dataset }: { dataset: { id: string; name?: string } },
+  ref: Ref<HTMLSpanElement>
+) {
   const label = getDatasetLabel(dataset)
   if (label.endsWith(GFW_ONLY_SUFFIX))
     return (
-      <span>
+      <span ref={ref}>
         <GFWOnly type="only-icon" />
         {label.replace(GFW_ONLY_SUFFIX, '')}
       </span>
@@ -13,4 +17,4 @@ function DatasetLabel({ dataset }: { dataset: { id: string; name?: string } }) {
   return <span>{label}</span>
 }
 
-export default DatasetLabel
+export default forwardRef(DatasetLabel)
