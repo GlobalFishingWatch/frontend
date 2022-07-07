4179cf1311baac1f60864da9e988694738b48009
j8seangel <jseangel@gmail.com>
2022-07-07 16:24:43 +0200

    don't show icons for default context layers

--- apps/fishing-map/features/workspace/common/Title.tsx
+++ apps/fishing-map/features/workspace/common/Title.tsx
@@ -3,6 +3,7 @@ import cx from 'classnames'
 import { UrlDataviewInstance } from '@globalfishingwatch/dataviews-client'
 import { Icon } from '@globalfishingwatch/ui-components'
 import { getDatasetIcon } from 'features/datasets/datasets.utils'
+import { CONTEXT_LAYERS_DATAVIEWS } from 'data/workspaces'
 import { useDataviewInstancesConnect } from '../workspace.hook'
 
 type TitleProps = {
@@ -14,10 +15,12 @@ type TitleProps = {
 }
 
 const Title = (props: TitleProps, ref: Ref<HTMLHeadingElement>) => {
+  console.log(props)
   const { dataview, className, classNameActive, title, onToggle } = props
   const { upsertDataviewInstance } = useDataviewInstancesConnect()
   const layerActive = dataview?.config?.visible ?? true
   const datasetIcon = getDatasetIcon(dataview?.datasets?.[0])
+  console.log(datasetIcon)
 
   const onToggleLayerActive = () => {
     upsertDataviewInstance({
@@ -37,7 +40,9 @@ const Title = (props: TitleProps, ref: Ref<HTMLHeadingElement>) => {
       onClick={onToggleLayerActive}
     >
       <span>
-        {datasetIcon && <Icon icon={datasetIcon} style={{ transform: 'translateY(25%)' }} />}
+        {datasetIcon && !CONTEXT_LAYERS_DATAVIEWS.includes(dataview.dataviewId) && (
+          <Icon icon={datasetIcon} style={{ transform: 'translateY(25%)' }} />
+        )}
         {title}
       </span>
     </h3>
