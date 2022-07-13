d9563e2d990b8662d58f73e2cd928ec0bafab4db
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2022-07-13 10:21:39 -0300

    Added google tag manager snippet

--- apps/api-portal/components/data/config.ts
+++ apps/api-portal/components/data/config.ts
@@ -3,3 +3,5 @@ export const ROOT_DOM_ELEMENT = '__next'
 export const APPLICATION_ID = 'api-portal'
 export const APPLICATION_NAME = 'API Portal'
 export const PATH_BASENAME = process.env.NEXT_PUBLIC_URL || (IS_PRODUCTION ? '/api-portal' : '')
+export const GOOGLE_TAG_MANAGER_KEY =
+  process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_KEY || 'GTM-KK5ZFST'
