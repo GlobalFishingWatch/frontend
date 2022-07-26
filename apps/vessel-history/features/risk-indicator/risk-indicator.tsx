54c8360b94723a22ffd34a61b6dd8c9b6fe37a50
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2022-07-26 15:47:07 -0300

    Fixed indicators styles

--- apps/vessel-history/features/risk-indicator/risk-indicator.tsx
+++ apps/vessel-history/features/risk-indicator/risk-indicator.tsx
@@ -1,4 +1,5 @@
-import { Fragment, useCallback, useMemo, useState } from 'react'
+import { useCallback, useState } from 'react'
+import cx from 'classnames'
 import { IconButton } from '@globalfishingwatch/ui-components'
 import ActivityEvent from 'features/profile/components/activity/ActivityEvent'
 import { RenderedEvent } from 'features/vessels/activity/vessels-activity.selectors'
@@ -10,6 +11,7 @@ export interface RiskIndicatorProps {
   onEventInfoClick: (event: RenderedEvent) => void
   onEventMapClick: (event: RenderedEvent | Voyage) => void
   title: string
+  subtitle?: string
 }
 
 export function RiskIndicator({
@@ -17,16 +19,20 @@ export function RiskIndicator({
   onEventInfoClick,
   onEventMapClick,
   title,
+  subtitle,
 }: RiskIndicatorProps) {
   const [expanded, setExpanded] = useState(false)
-  const hasEvents = useMemo(() => events.length > 0, [events.length])
+  const hasEvents = events.length > 0
   const onToggle = useCallback(() => setExpanded(!expanded), [expanded])
 
   return (
     <div className={styles['container']}>
-      <div className={styles.title} onClick={onToggle}>
-        <div>{title}</div>
-        {events.length > 0 ? (
+      <div className={cx(styles.title, { [styles.expandable]: hasEvents })} onClick={onToggle}>
+        <div>
+          {title}
+          {!!subtitle && <span className={styles.subtitle}> {subtitle}</span>}
+        </div>
+        {hasEvents ? (
           <IconButton
             icon={expanded ? 'arrow-top' : 'arrow-down'}
             size="small"
