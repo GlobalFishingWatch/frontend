adcc30a6e9044421d2a04aa89a7b1e76bbcbf1e5
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2022-08-03 17:27:10 -0300

    Improve responsiveness of events selector to not wait for track

--- apps/vessel-history/features/vessels/activity/vessels-activity.selectors.ts
+++ apps/vessel-history/features/vessels/activity/vessels-activity.selectors.ts
@@ -90,15 +90,16 @@ export const selectEventsForTracks = createSelector(
     if (Object.keys(resources).length === 0) return []
 
     const vesselsEvents = trackDataviews.map((dataview) => {
-      const { url: tracksUrl } = resolveDataviewDatasetResource(dataview, DatasetTypes.Tracks)
+      // const { url: tracksUrl } = resolveDataviewDatasetResource(dataview, DatasetTypes.Tracks)
       const eventsResources = resolveDataviewDatasetResources(dataview, DatasetTypes.Events)
       const hasEventData =
         eventsResources?.length && eventsResources.every(({ url }) => resources[url]?.data)
-      const tracksResourceResolved =
-        tracksUrl && resources[tracksUrl]?.status === ResourceStatus.Finished
+      // const tracksResourceResolved =
+      // tracksUrl && resources[tracksUrl]?.status === ResourceStatus.Finished
 
-      // Waiting for the tracks resource to be resolved to show the events
-      if (!hasEventData || !tracksResourceResolved) {
+      if (!hasEventData) {
+        // stop Waiting for the tracks resource to be resolved to show the events
+        //} || !tracksResourceResolved) {
         return { dataview, data: [] }
       }
 
