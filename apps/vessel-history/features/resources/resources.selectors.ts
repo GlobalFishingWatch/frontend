707df3c26231d82cc383a0a8af216986eca4ca86
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2022-08-03 17:27:58 -0300

    Fix event resources loading sleector

--- apps/vessel-history/features/resources/resources.selectors.ts
+++ apps/vessel-history/features/resources/resources.selectors.ts
@@ -19,23 +19,8 @@ export const selectVisibleResources = createSelector(
   }
 )
 
-const getDatasetEventType = (dataset: Dataset): EventType => {
-  const datasetToEventTypeMapping = {
-    vessel: {
-      [EventTypes.Encounter]: EventTypes.Encounter,
-      [EventTypes.Fishing]: EventTypes.Fishing,
-      [EventTypes.Loitering]: EventTypes.Loitering,
-      [EventTypes.Port]: EventTypes.Port,
-    },
-  }
-  return (
-    (datasetToEventTypeMapping[dataset?.category] &&
-      datasetToEventTypeMapping[dataset?.category][dataset?.subcategory]) ??
-    undefined
-  )
-}
 export const selectEventResourcesLoading = createSelector([selectResources], (resources) => {
   return Object.entries(resources)
     .filter(([url, resource]) => resource.status === ResourceStatus.Loading)
-    .map(([url, resource]) => getDatasetEventType(resource.dataset))
+    .map(([url, resource]) => resource?.dataset?.subcategory)
 })
