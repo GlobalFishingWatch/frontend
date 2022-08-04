2dd0033d9bc95bbbb4390d802a7435791a573aed
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2022-08-04 09:05:04 -0300

    Updated terminology for iuu indicator

--- apps/vessel-history/features/terminology/terminology-iuu.tsx
+++ apps/vessel-history/features/terminology/terminology-iuu.tsx
@@ -1,14 +1,23 @@
 import { Trans } from 'react-i18next'
+import styles from './terminology.module.css'
 
 /* eslint-disable-next-line */
 export interface TerminologyIuuProps {}
 
 export function TerminologyIuu(props: TerminologyIuuProps) {
   return (
-    <p>
-      <Trans i18nKey="vessel.iuuStatusDescription">
+    <p className={styles.container}>
+      <Trans i18nKey="risk.iuuStatusDescription">
         Illegal, unreported and unregulated (IUU) listing status according to official RFMO IUU
-        vessel lists - vessel is currently listed, was previously listed or has never been listed
+        vessel lists. There are two statuses:{' '}
+        <ol>
+          <li>vessel is currently listed: when a vessel is currently listed on an RFMO IUU list</li>
+          <li>
+            vessel is not currently listed: when a vessel is not currently listed on an RFMO IUU
+            list. This includes if a vessel was previously on an RFMO IUU list but has since been
+            removed.
+          </li>
+        </ol>
       </Trans>
     </p>
   )
