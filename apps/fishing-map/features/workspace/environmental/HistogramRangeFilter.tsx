f28c10294ff7c3e5189f4b34056b3c6b8d61f7f4
j8seangel <jseangel@gmail.com>
2022-09-07 13:04:00 +0200

    histogram to globel env layers filter

--- apps/fishing-map/features/workspace/environmental/HistogramRangeFilter.tsx
+++ apps/fishing-map/features/workspace/environmental/HistogramRangeFilter.tsx
@@ -1,15 +1,15 @@
 import { useCallback } from 'react'
-// import { BarChart, Bar, ResponsiveContainer } from 'recharts'
+import { BarChart, Bar, ResponsiveContainer } from 'recharts'
 import { useTranslation } from 'react-i18next'
 import { Slider } from '@globalfishingwatch/ui-components'
 import { UrlDataviewInstance } from '@globalfishingwatch/dataviews-client'
-// import { useLayerHistogram } from 'features/map/histogram.hooks'
 import {
   Dataset,
   DatasetTypes,
   EnviromentalDatasetConfiguration,
 } from '@globalfishingwatch/api-types'
 import { useDataviewInstancesConnect } from 'features/workspace/workspace.hook'
+import { useDataviewHistogram } from 'features/workspace/environmental/histogram.hooks'
 import styles from './HistogramRangeFilter.module.css'
 
 type HistogramRangeFilterProps = {
@@ -23,6 +23,7 @@ export const getLayerDatasetRange = (dataset: Dataset) => {
     scale = 1,
     offset = 0,
   } = dataset?.configuration as EnviromentalDatasetConfiguration
+  console.log(dataset?.configuration)
 
   // Using Math.max to ensure we don't show negative values as 4wings doesn't support them yet
   const cleanMin = Math.max(0, Math.floor(min * scale + offset))
@@ -36,7 +37,7 @@ export const getLayerDatasetRange = (dataset: Dataset) => {
 function HistogramRangeFilter({ dataview }: HistogramRangeFilterProps) {
   const { t } = useTranslation()
   const { upsertDataviewInstance } = useDataviewInstancesConnect()
-  // const histogram = useLayerHistogram(layer)
+  const histogram = useDataviewHistogram(dataview)
   const dataset = dataview.datasets?.find(
     (d) => d.type === DatasetTypes.Fourwings || d.type === DatasetTypes.Context
   )
@@ -72,13 +73,13 @@ function HistogramRangeFilter({ dataview }: HistogramRangeFilterProps) {
 
   return (
     <div className={styles.container}>
-      {/* <div className={styles.histogram}>
+      <div className={styles.histogram}>
         <ResponsiveContainer width="100%" height="100%">
           <BarChart width={150} height={40} data={histogram}>
             <Bar dataKey="data" fill="#C7D8DC" />
           </BarChart>
         </ResponsiveContainer>
-      </div> */}
+      </div>
       <Slider
         className={styles.slider}
         initialRange={[minSliderValue, maxSliderValue]}
