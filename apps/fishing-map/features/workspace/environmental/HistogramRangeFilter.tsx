2e9f19c2f636f6447ca69bbcb1c9aafa66670228
j8seangel <jseangel@gmail.com>
2022-10-05 19:05:00 +0200

    fix showing env filters

--- apps/fishing-map/features/workspace/environmental/HistogramRangeFilter.tsx
+++ apps/fishing-map/features/workspace/environmental/HistogramRangeFilter.tsx
@@ -41,7 +41,8 @@ function HistogramRangeFilter({ dataview }: HistogramRangeFilterProps) {
     (d) => d.type === DatasetTypes.Fourwings || d.type === DatasetTypes.Context
   )
   const { max, min } = dataset?.configuration
-  const showRange = max !== undefined && min !== undefined && max !== null && min !== null
+  const showRange =
+    max !== undefined && min !== undefined && max !== null && min !== null && max !== 0 && min !== 0
   const layerRange = getLayerDatasetRange(dataset)
   const minSliderValue = dataview.config?.minVisibleValue ?? layerRange.min
   const maxSliderValue = dataview.config?.maxVisibleValue ?? layerRange.max
