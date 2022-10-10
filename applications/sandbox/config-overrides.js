0793274962fe1400238178af1fbb5fe9035318bd
j8seangel <jseangel@gmail.com>
2022-10-10 13:48:44 +0200

    remove all legacy applications

--- applications/sandbox/config-overrides.js
+++
@@ -1,6 +0,0 @@
-// This allows using deps directly with @gfw/xxx/src/yyy
-
-const getYarnWorkspaces = require('get-yarn-workspaces')
-const { override, babelInclude } = require('customize-cra')
-
-module.exports = override(babelInclude(getYarnWorkspaces()))
