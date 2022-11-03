242de4371d0338800ecb8e617479dd9b4af9bf47
satellitestudiodesign <juancarlos@satellitestud.io>
2022-11-03 11:55:36 +0100

    animate banner entrance

--- apps/fishing-map/features/download/DownloadActivityProductsBanner.tsx
+++ apps/fishing-map/features/download/DownloadActivityProductsBanner.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next'
+import cx from 'classnames'
 import { Button } from '@globalfishingwatch/ui-components'
 import { useTimerangeConnect } from 'features/timebar/timebar.hooks'
 import { Format } from 'features/download/downloadActivity.config'
@@ -14,33 +15,41 @@ function DownloadActivityProductsBanner({ format }: DownloadActivityProductsBann
   const { start, end } = useTimerangeConnect()
   const isDownloadReportSupported = getDownloadReportSupported(start, end)
 
-  return !isDownloadReportSupported ? (
-    <div className={styles.downloadFooter}>
-      <p className={styles.downloadLabel}>
-        {t('download.fullDataset', 'Do you need the full dataset?')}
-      </p>
-      <Button
-        className={styles.downloadBtn}
-        href="https://globalfishingwatch.org/data-download/datasets/public-fishing-effort"
-        target="_blank"
-      >
-        {t('download.dataPortal', 'See data download portal')}
-      </Button>
+  return (
+    <div
+      className={cx(styles.downloadFooterContainer, {
+        [styles.open]: !isDownloadReportSupported || format === Format.Json,
+      })}
+    >
+      {!isDownloadReportSupported ? (
+        <div className={styles.downloadFooter}>
+          <p className={styles.downloadLabel}>
+            {t('download.fullDataset', 'Do you need the full dataset?')}
+          </p>
+          <Button
+            className={styles.downloadBtn}
+            href="https://globalfishingwatch.org/data-download/datasets/public-fishing-effort"
+            target="_blank"
+          >
+            {t('download.dataPortal', 'See data download portal')}
+          </Button>
+        </div>
+      ) : format === Format.Json ? (
+        <div className={styles.downloadFooter}>
+          <p className={styles.downloadLabel}>
+            {t('download.doYouNeedAnAPI', 'Your application need constantly updated data?')}
+          </p>
+          <Button
+            className={styles.downloadBtn}
+            href="https://globalfishingwatch.org/our-apis/documentation#create-a-report-of-a-specified-region"
+            target="_blank"
+          >
+            {t('download.apiPortal', 'See our APIs here')}
+          </Button>
+        </div>
+      ) : null}
     </div>
-  ) : format === Format.Json ? (
-    <div className={styles.downloadFooter}>
-      <p className={styles.downloadLabel}>
-        {t('download.doYouNeedAnAPI', 'Your application need constantly updated data?')}
-      </p>
-      <Button
-        className={styles.downloadBtn}
-        href="https://globalfishingwatch.org/our-apis/documentation#create-a-report-of-a-specified-region"
-        target="_blank"
-      >
-        {t('download.apiPortal', 'See our APIs here')}
-      </Button>
-    </div>
-  ) : null
+  )
 }
 
 export default DownloadActivityProductsBanner
