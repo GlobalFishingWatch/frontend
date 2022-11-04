e002e85990829dac16f478701bd46dc1e5f0a379
j8seangel <jseangel@gmail.com>
2022-11-04 14:06:14 +0100

    fix typing

--- apps/deck-playground/layers/vessel/VesselPortVisitsLayer.ts
+++ apps/deck-playground/layers/vessel/VesselPortVisitsLayer.ts
@@ -1,24 +1,37 @@
-import { ScatterplotLayer } from "@deck.gl/layers/typed";
+import { AccessorFunction, DefaultProps } from '@deck.gl/core/typed'
+import { ScatterplotLayer, ScatterplotLayerProps } from '@deck.gl/layers/typed'
+
 const SHAPES = {
   SQUARE: 0,
   DIAMOND: 1,
   CIRCLE: 2,
 }
-class VesselPortVisitsLayer extends ScatterplotLayer {
+
+export type _VesselEventsLayerProps<DataT = any> = {
+  getShape?: AccessorFunction<DataT, number>
+}
+
+export type VesselEventsLayerProps<DataT = any> = _VesselEventsLayerProps<DataT> &
+  ScatterplotLayerProps<DataT>
+
+const defaultProps: DefaultProps<VesselEventsLayerProps> = {
+  getShape: { type: 'accessor', value: (d) => SHAPES.CIRCLE },
+}
+
+class VesselPortVisitsLayer<DataT = any, ExtraProps = {}> extends ScatterplotLayer<
+  DataT,
+  Required<VesselEventsLayerProps> & ExtraProps
+> {
   static layerName = 'VesselPortVisitsLayer'
-  static defaultProps = {
-    getShape: {type: 'access', value: SHAPES.CIRCLE}
-  }
-
-
+  static defaultProps = defaultProps
 
   initializeState() {
     super.initializeState()
     this.getAttributeManager().addInstanced({
       instanceShapes: {
         size: 1,
-        accessor: 'getShape'
-      }
+        accessor: 'getShape',
+      },
     })
   }
 
@@ -52,11 +65,10 @@ class VesselPortVisitsLayer extends ScatterplotLayer {
           } else {
             color.rgb = vec3(0.0, 1.0, 0.0);
           }
-        `
-      }
+        `,
+      },
     }
   }
-
 }
 
 export default VesselPortVisitsLayer
