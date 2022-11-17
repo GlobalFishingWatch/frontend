b43da1ae8e559269062ad7fe86ce08d04de0b062
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2022-11-17 10:59:03 -0300

    fix map build failures

--- apps/fishing-map/features/dataviews/dataviews.slice.ts
+++ apps/fishing-map/features/dataviews/dataviews.slice.ts
@@ -62,7 +62,6 @@ export const fetchDataviewsByIdsThunk = createAsyncThunk(
   'dataviews/fetch',
   async (ids: (Dataview['id'] | Dataview['slug'])[], { signal, rejectWithValue, getState }) => {
     const state = getState() as RootState
-    const existingIds = selectIds(state) as ((number | string) | string)[]
     const existingIds = selectIds(state) as (number | string)[]
     const uniqIds = ids.filter((id) => !existingIds.includes(id))
 
