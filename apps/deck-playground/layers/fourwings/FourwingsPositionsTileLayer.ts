ab0021b314c434891a55b63763ee184470aeec1c
satellitestudiodesign <juancarlos@satellitestud.io>
2022-12-05 17:54:01 +0100

    combine tiles to show all positions as points and last as vessel icon

--- apps/deck-playground/layers/fourwings/FourwingsPositionsTileLayer.ts
+++ apps/deck-playground/layers/fourwings/FourwingsPositionsTileLayer.ts
@@ -1,10 +1,12 @@
-import { Color, CompositeLayer, Layer, LayersList } from '@deck.gl/core/typed'
+import { Color, CompositeLayer, Layer, LayerContext, LayersList } from '@deck.gl/core/typed'
 import { MVTLayer, TileLayerProps } from '@deck.gl/geo-layers/typed'
+import { IconLayer, ScatterplotLayer } from '@deck.gl/layers/typed'
+import { MVTLoader } from '@loaders.gl/mvt'
 import { ckmeans, sample, mean, standardDeviation } from 'simple-statistics'
 import { ACTIVITY_SWITCH_ZOOM_LEVEL, getDateRangeParam } from 'layers/fourwings/fourwings.utils'
-import { FourwingsPositionsLayer } from 'layers/fourwings/FourwingsPositionsLayer'
+import { groupBy, orderBy } from 'lodash'
 import { COLOR_RAMP_DEFAULT_NUM_STEPS } from '@globalfishingwatch/layer-composer'
-import { FourwingsColorRamp, POSITIONS_ID } from './FourwingsLayer'
+import { FourwingsColorRamp } from './FourwingsLayer'
 
 export type FourwingsPositionsTileLayerProps<DataT = any> = {
   minFrame: number
@@ -18,15 +20,32 @@ export type FourwingsPositionsTileLayerProps<DataT = any> = {
   onVesselClick?: (vesselId: string) => void
 }
 
+const ICON_MAPPING = {
+  vessel: { x: 0, y: 0, width: 22, height: 40, mask: true },
+  vesselHighlight: { x: 24, y: 0, width: 22, height: 40, mask: false },
+}
+
 export class FourwingsPositionsTileLayer extends CompositeLayer<
   FourwingsPositionsTileLayerProps & TileLayerProps
 > {
   static layerName = 'FourwingsPositionsTileLayer'
 
-  getColorRamp() {
-    const data = this.getPositionsData()
-    if (data?.length > 0) {
-      const hours = data.map((d) => d.properties.value)
+  initializeState(context: LayerContext) {
+    super.initializeState(context)
+    this.state = {
+      colorScale: {
+        colorRange: [],
+        colorDomain: [],
+      },
+      allPositions: [],
+      lastPositions: [],
+      highlightVessels: [],
+    }
+  }
+
+  getColorRamp(positions) {
+    if (positions?.length > 0) {
+      const hours = positions.map((d) => d?.properties?.value).filter(Number)
       const dataSampled = hours.length > 1000 ? sample(hours, 1000, Math.random) : hours
       // filter data to 2 standard deviations from mean to remove outliers
       const meanValue = mean(dataSampled)
@@ -47,44 +66,146 @@ export class FourwingsPositionsTileLayer extends CompositeLayer<
     }
   }
 
-  getColorDomain() {
-    return []
+  getFillColor(d) {
+    const { highlightedVesselId } = this.props
+    const { colorDomain, colorRange } = this.state.colorScale
+    const colorIndex = colorDomain.findIndex((domain, i) => {
+      if (colorDomain[i + 1]) {
+        return d.properties?.value > domain && d.properties?.value <= colorDomain[i + 1]
+      }
+      return i
+    })
+    const color = colorIndex >= 0 ? colorRange[colorIndex] : [0, 0, 0, 0]
+    if (!highlightedVesselId) {
+      return color
+    } else if (d.properties.vesselId !== highlightedVesselId) {
+      return [color[0], color[1], color[2], 0]
+    }
+    return color
   }
 
-  renderLayers(): Layer<{}> | LayersList {
-    const MVTLayerClass = this.getSubLayerClass('positions', MVTLayer)
-    return new MVTLayerClass(this.props, {
-      id: 'positions',
-      data: `https://gateway.api.dev.globalfishingwatch.org/v2/4wings/tile/position/{z}/{x}/{y}?datasets[0]=public-global-fishing-effort%3Av20201001&${getDateRangeParam(
-        this.props.minFrame,
-        this.props.maxFrame
-      )}`,
-      binary: false,
-      minZoom: ACTIVITY_SWITCH_ZOOM_LEVEL,
-      maxZoom: 12,
-      // loadOptions: {
-      //   fetch: {
-      //     headers: {
-      //       Authorization: `Bearer ${API_TOKEN}`,
-      //     },
-      //   },
-      // },
-      renderSubLayers: (props) => {
-        return new FourwingsPositionsLayer(this.getSubLayerProps(props))
-      },
+  getPickingInfo({ info, mode }) {
+    if (this.props.onVesselHighlight) {
+      const vesselId = info.object?.properties?.vesselId
+      if (vesselId) {
+        this.props.onVesselHighlight(vesselId)
+        if (mode === 'query') {
+          this.props.onVesselClick(vesselId)
+        }
+      } else if (this.props.highlightedVesselId) {
+        this.props.onVesselHighlight(undefined)
+      }
+    }
+    if (mode === 'query') {
+      console.log(info.object?.properties)
+    }
+    return info
+  }
+
+  onViewportLoad = (tiles) => {
+    const allPositions = orderBy(
+      tiles.flatMap((tile) => tile.dataInWGS84),
+      'properties.htime'
+    ).filter(Boolean)
+    const positionsByVessel = groupBy(allPositions, 'properties.vesselId')
+    const lastPositions = []
+    Object.keys(positionsByVessel)
+      .filter((p) => p !== 'undefined')
+      .forEach((vesselId) => {
+        const vesselPositions = positionsByVessel[vesselId]
+        lastPositions.push(...vesselPositions.slice(-1))
+      })
+    const colorScale = this.getColorRamp(allPositions)
+    this.setState({
+      allPositions,
+      lastPositions,
+      colorScale,
     })
   }
 
+  renderLayers(): Layer<{}> | LayersList {
+    const { allPositions, lastPositions, colorScale } = this.state
+    return [
+      new MVTLayer(this.props, {
+        id: 'position-tiles',
+        data: `https://gateway.api.dev.globalfishingwatch.org/v2/4wings/tile/position/{z}/{x}/{y}?datasets[0]=public-global-fishing-effort%3Av20201001&${getDateRangeParam(
+          this.props.minFrame,
+          this.props.maxFrame
+        )}`,
+        minZoom: ACTIVITY_SWITCH_ZOOM_LEVEL,
+        maxZoom: ACTIVITY_SWITCH_ZOOM_LEVEL,
+        loaders: [MVTLoader],
+        onViewportLoad: this.onViewportLoad,
+      }),
+      new ScatterplotLayer(this.props, {
+        id: 'allPositions',
+        data: allPositions,
+        getPosition: (d) => d.geometry.coordinates,
+        filled: true,
+        stroked: true,
+        getFillColor: (d) => this.getFillColor(d),
+        getLineColor: [0, 0, 0, 25],
+        radiusMinPixels: 3,
+        lineWidthMinPixels: 1,
+        updateTriggers: {
+          getFillColor: [colorScale.colorDomain, allPositions],
+        },
+        pickable: true,
+        getPickingInfo: this.getPickingInfo,
+      }),
+      new IconLayer(this.props, {
+        id: 'lastPositions',
+        data: lastPositions,
+        iconAtlas: '/vessel-sprite.png',
+        iconMapping: ICON_MAPPING,
+        getIcon: () => 'vessel',
+        sizeScale: 1,
+        getPosition: (d) => d.geometry.coordinates,
+        getAngle: (d) => d.properties.bearing,
+        getColor: [255, 255, 255, 255],
+        getSize: 21,
+        updateTriggers: {
+          getFillColor: [colorScale.colorDomain.join('-'), lastPositions],
+        },
+      }),
+      new IconLayer(this.props, {
+        id: 'lastPositionsOver',
+        data: lastPositions,
+        iconAtlas: '/vessel-sprite.png',
+        iconMapping: ICON_MAPPING,
+        getIcon: () => 'vessel',
+        sizeScale: 1,
+        getPosition: (d) => d.geometry.coordinates,
+        getAngle: (d) => d.properties.bearing,
+        getColor: (d) => this.getFillColor(d),
+        getSize: 19,
+        updateTriggers: {
+          getColor: [colorScale.colorDomain, lastPositions],
+        },
+        pickable: true,
+        getPickingInfo: this.getPickingInfo,
+      }),
+    ]
+  }
+
   getPositionsData() {
-    const layer = this.getSubLayers().find((l) => l.id === POSITIONS_ID) as MVTLayer
-    if (layer) {
-      return layer.getSubLayers().flatMap((l: FourwingsPositionsLayer) => l.getTileData())
-    }
+    return this.state.allPositions
   }
 
   getData() {
-    const data = this.getPositionsData()
-    return data
+    return this.getPositionsData()
+  }
+
+  getColorScale() {
+    return this.state.colorScale
+  }
+
+  getColorDomain() {
+    return this.state.colorScale.colorDomain
+  }
+
+  getColorRange() {
+    return this.state.colorScale.colorRange
   }
 
   getTimeseries() {
