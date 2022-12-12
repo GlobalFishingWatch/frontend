95af194a37935886bc3f56d1a5208c4fb00ca4f5
j8seangel <jseangel@gmail.com>
2022-12-12 16:50:40 +0100

    fix crash when no dataset configuration present

--- apps/fishing-map/features/workspace/activity/ActivityFilters.tsx
+++ apps/fishing-map/features/workspace/activity/ActivityFilters.tsx
@@ -77,7 +77,7 @@ const cleanDataviewFiltersNotAllowed = (
 
 export const isHistogramDataviewSupported = (dataview: UrlDataviewInstance) => {
   const dataset = dataview.datasets?.find((d) => d.type === DatasetTypes.Fourwings)
-  const { max, min } = dataset?.configuration
+  const { max, min } = dataset?.configuration || {}
   return max !== undefined && min !== undefined && max !== null && min !== null
 }
 
