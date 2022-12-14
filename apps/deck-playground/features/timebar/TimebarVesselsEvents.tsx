be758d59411e1ec8f30f2f797e8228c4269f5b85
weberjavi <weberjavi@gmail.com>
2022-12-14 13:12:03 +0100

    Center map on event on timebar click

--- apps/deck-playground/features/timebar/TimebarVesselsEvents.tsx
+++ apps/deck-playground/features/timebar/TimebarVesselsEvents.tsx
@@ -1,10 +1,12 @@
 import { useMemo } from 'react'
 import { useVesselsLayerInstance, useVesselsLayerLoaded } from 'layers/vessel/vessels.hooks'
 import { TimebarTracksEvents } from '@globalfishingwatch/timebar'
+import { useViewport } from 'features/map/map-viewport.hooks'
 
 const TimebarVesselsEvents = () => {
   const vesselsLayerInstance = useVesselsLayerInstance()
   const vesselsLayerLoaded = useVesselsLayerLoaded()
+  const { setMapCoordinates } = useViewport()
 
   const eventsData = useMemo(() => {
     if (vesselsLayerLoaded && vesselsLayerInstance) {
@@ -16,7 +18,13 @@ const TimebarVesselsEvents = () => {
     return []
   }, [vesselsLayerLoaded, vesselsLayerInstance])
 
-  return <TimebarTracksEvents data={eventsData} onEventClick={(e) => console.log(e)} />
+  const handleEventClick = (e) => {
+    const { coordinates } = e
+    const [longitude, latitude] = coordinates
+    setMapCoordinates({ latitude, longitude, zoom: 9 })
+  }
+
+  return <TimebarTracksEvents data={eventsData} onEventClick={handleEventClick} />
 }
 
 export default TimebarVesselsEvents
