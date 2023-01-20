a7b03f8e32dc858391672f7c29bd0e471fd2c764
j8seangel <jseangel@gmail.com>
2023-01-20 16:07:38 +0100

    fetch user info using getServerSideProps

--- apps/fishing-map/pages/index.tsx
+++ apps/fishing-map/pages/index.tsx
@@ -1,5 +1,7 @@
 import path from 'path'
 import dynamic from 'next/dynamic'
+import { getToken } from 'next-auth/jwt'
+import { GFW } from '@globalfishingwatch/authjs-client'
 
 // This is needed by nx/next builder to run build the standalone next app properly
 // https://github.com/nrwl/nx/issues/9017#issuecomment-1140066503
@@ -13,10 +15,27 @@ const AppNoSSRComponent = dynamic(() => import('../features/app/App'), {
   ssr: false,
 })
 
-const Index = () => {
+export async function getServerSideProps(context) {
+  const token = (await getToken({
+    req: context.req,
+    secret: process.env.NEXTAUTH_SECRET,
+  })) as { bearerToken: string; refreshToken: string }
+
+  const client = new GFW({
+    bearerToken: token?.bearerToken,
+  })
+  try {
+    const user = token ? await client.fetchUser() : await client.fetchGuestUser()
+    return { props: { user } }
+  } catch (e) {
+    return {}
+  }
+}
+
+const Index = (props) => {
   return (
     <MapProvider>
-      <AppNoSSRComponent />
+      <AppNoSSRComponent user={props.user} />
     </MapProvider>
   )
 }
