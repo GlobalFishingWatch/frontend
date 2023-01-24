04ec760347fad0418a41e8308c9ccb5275b55174
weberjavi <weberjavi@gmail.com>
2023-01-24 13:58:31 +0100

    Picking hooks

---
+++ apps/deck-playground/features/map/map-picking.hooks.ts
@@ -0,0 +1,30 @@
+import { useCallback } from 'react'
+import { PickingInfo } from '@deck.gl/core/typed'
+import { atom, selector, useRecoilValue, useSetRecoilState } from 'recoil'
+
+export const mapHoveredFeaturesAtom = atom<PickingInfo[]>({
+  key: 'hoveredFeatures',
+  dangerouslyAllowMutability: true,
+  default: [],
+  //   effects: [urlSyncEffect({ refine: mixed(), history: 'replace' })],
+})
+
+const mapHoveredFeatures = selector({
+  key: 'mapHoveredFeatures',
+  get: ({ get }) => {
+    const features = get(mapHoveredFeaturesAtom)
+    return features
+  },
+})
+
+export function useAddMapHoveredFeatures() {
+  const setHoveredFeatures = useSetRecoilState(mapHoveredFeaturesAtom)
+  const addHoveredFeatures = (features: PickingInfo[]) => {
+    setHoveredFeatures(features)
+  }
+  return addHoveredFeatures
+}
+
+export const useMapHoveredFeatures = () => {
+  return useRecoilValue(mapHoveredFeatures)
+}
