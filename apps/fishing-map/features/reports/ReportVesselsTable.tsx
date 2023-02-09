03fb6b75ad9f5116a450e6eeb682c2b84e4fa336
satellitestudiodesign <juancarlos@satellitestud.io>
2023-02-09 12:25:39 +0100

    style vessel table

--- apps/fishing-map/features/reports/ReportVesselsTable.tsx
+++ apps/fishing-map/features/reports/ReportVesselsTable.tsx
@@ -74,90 +74,99 @@ export default function ReportVesselsTable({ activityUnit }: ReportVesselTablePr
   const isLastPaginationPage = pagination.offset + pagination.results >= pagination.total
   return (
     <div>
-      <table className={cx(styles.vesselsTable)}>
-        <thead>
-          <tr>
-            <th colSpan={2}>{t('common.name', 'Name')}</th>
-            <th>{t('vessel.mmsi', 'mmsi')}</th>
-            <th>{t('layer.flagState_one', 'Flag state')}</th>
-            <th>{t('vessel.gearType_short', 'gear')}</th>
-            {/* Disabled for detections to allocate some space for timestamps interaction */}
-            <th>
-              {activityUnit === 'hours'
-                ? t('common.hour_other', 'hours')
-                : t('common.detection_other', 'detections')}
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {vessels.map((vessel, i) => {
-            const vesselName = formatInfoField(vessel.shipName, 'name')
+      <div className={styles.tableContainer}>
+        <table className={cx(styles.vesselsTable)}>
+          <thead>
+            <tr>
+              <th colSpan={2}>{t('common.name', 'Name')}</th>
+              <th>{t('vessel.mmsi', 'mmsi')}</th>
+              <th>{t('layer.flagState_one', 'Flag state')}</th>
+              <th>{t('vessel.gearType_short', 'gear')}</th>
+              {/* Disabled for detections to allocate some space for timestamps interaction */}
+              <th className={styles.right}>
+                {activityUnit === 'hours'
+                  ? t('common.hour_other', 'hours')
+                  : t('common.detection_other', 'detections')}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {vessels.map((vessel, i) => {
+              const vesselName = formatInfoField(vessel.shipName, 'name')
 
-            const vesselGearType = `${t(
-              `vessel.gearTypes.${vessel.geartype}` as any,
-              vessel.geartype ?? EMPTY_FIELD_PLACEHOLDER
-            )}`
+              const vesselGearType = `${t(
+                `vessel.gearTypes.${vessel.geartype}` as any,
+                vessel.geartype ?? EMPTY_FIELD_PLACEHOLDER
+              )}`
 
-            const hasDatasets = true
-            // TODO get datasets from the vessel
-            // const hasDatasets = vessel.infoDataset !== undefined || vessel.trackDataset !== undefined
+              const hasDatasets = true
+              // TODO get datasets from the vessel
+              // const hasDatasets = vessel.infoDataset !== undefined || vessel.trackDataset !== undefined
 
-            const vesselInWorkspace = getVesselInWorkspace(vesselsInWorkspace, vessel.vesselId)
+              const vesselInWorkspace = getVesselInWorkspace(vesselsInWorkspace, vessel.vesselId)
 
-            const pinTrackDisabled = !hasDatasets
-            return (
-              <tr key={i}>
-                {!pinTrackDisabled && (
-                  <td className={styles.icon}>
-                    <IconButton
-                      icon={vesselInWorkspace ? 'pin-filled' : 'pin'}
-                      style={{
-                        color: vesselInWorkspace ? vesselInWorkspace.config.color : '',
-                      }}
-                      tooltip={
-                        vesselInWorkspace
-                          ? t(
-                              'search.vesselAlreadyInWorkspace',
-                              'This vessel is already in your workspace'
-                            )
-                          : t('search.seeVessel', 'See vessel')
-                      }
-                      onClick={(e) => onVesselClick(e, vessel)}
-                      size="small"
-                    />
+              const pinTrackDisabled = !hasDatasets
+              return (
+                <tr key={i}>
+                  {!pinTrackDisabled && (
+                    <td className={styles.icon}>
+                      <IconButton
+                        icon={vesselInWorkspace ? 'pin-filled' : 'pin'}
+                        style={{
+                          color: vesselInWorkspace ? vesselInWorkspace.config.color : '',
+                        }}
+                        tooltip={
+                          vesselInWorkspace
+                            ? t(
+                                'search.vesselAlreadyInWorkspace',
+                                'This vessel is already in your workspace'
+                              )
+                            : t('search.seeVessel', 'See vessel')
+                        }
+                        onClick={(e) => onVesselClick(e, vessel)}
+                        size="small"
+                      />
+                    </td>
+                  )}
+                  <td colSpan={pinTrackDisabled ? 2 : 1}>{vesselName}</td>
+                  <td colSpan={1} className={styles.columnSpace}>
+                    <span>{vessel.mmsi}</span>
                   </td>
-                )}
-                <td colSpan={pinTrackDisabled ? 2 : 1}>{vesselName}</td>
-                <td className={styles.columnSpace}>
-                  <span>{vessel.mmsi}</span>
-                </td>
-                <td className={styles.columnSpace}>
-                  <span>{t(`flags:${vessel.flag as string}` as any, EMPTY_FIELD_PLACEHOLDER)}</span>
-                </td>
-                <td className={styles.columnSpace}>{vesselGearType}</td>
-                <td className={cx(styles.columnSpace, styles.vesselsTableHour)}>
-                  <I18nNumber number={vessel[activityUnit]} />
-                </td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+                  <td colSpan={1} className={styles.columnSpace}>
+                    <span>
+                      {t(`flags:${vessel.flag as string}` as any, EMPTY_FIELD_PLACEHOLDER)}
+                    </span>
+                  </td>
+                  <td colSpan={1} className={styles.columnSpace}>
+                    {vesselGearType}
+                  </td>
+                  <td colSpan={1} className={cx(styles.columnSpace, styles.right)}>
+                    <I18nNumber number={vessel[activityUnit]} />
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
       <div className={styles.footer}>
         <div>
           <IconButton
             icon="arrow-left"
             disabled={pagination.page === 0}
+            className={cx({ [styles.disabled]: pagination.page === 0 })}
             onClick={onPrevPageClick}
+            size="medium"
           />
-          <span>{pagination.offset}</span> -{' '}
-          <span>
-            {isLastPaginationPage ? pagination.total : pagination.offset + pagination.results}
-          </span>
+          <span>{`${pagination.offset} - ${
+            isLastPaginationPage ? pagination.total : pagination.offset + pagination.results
+          }`}</span>
           <IconButton
             icon="arrow-right"
             onClick={onNextPageClick}
             disabled={isLastPaginationPage}
+            className={cx({ [styles.disabled]: isLastPaginationPage })}
+            size="medium"
           />
         </div>
         <div>
