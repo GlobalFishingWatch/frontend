ab7c74982925d4634d8163ae709550907a0eb1e9
j8seangel <jseangel@gmail.com>
2023-02-15 16:35:49 +0100

    restore showTImebar condition

--- apps/fishing-map/features/app/App.tsx
+++ apps/fishing-map/features/app/App.tsx
@@ -71,8 +71,7 @@ const Main = () => {
   const isTimeComparisonAnalysis = useSelector(selectShowTimeComparison)
 
   const showTimebar =
-    workspaceLocation ||
-    (reportLocation && workspaceStatus === AsyncReducerStatus.Finished) ||
+    ((workspaceLocation || reportLocation) && workspaceStatus === AsyncReducerStatus.Finished) ||
     (reportLocation && !isTimeComparisonAnalysis)
 
   return (
