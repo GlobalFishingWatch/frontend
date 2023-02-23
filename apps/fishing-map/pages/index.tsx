dd30b90d9b619443edfff1f2bfd0df415ec6439b
j8seangel <jseangel@gmail.com>
2023-02-23 18:14:25 +0100

    Revert "change page description for marine manager"

--- apps/fishing-map/pages/index.tsx
+++ apps/fishing-map/pages/index.tsx
@@ -1,8 +1,6 @@
 import path from 'path'
-import Head from 'next/head'
 import dynamic from 'next/dynamic'
 import { MapProvider } from 'react-map-gl'
-import { Component, Fragment } from 'react'
 
 // This is needed by nx/next builder to run build the standalone next app properly
 // https://github.com/nrwl/nx/issues/9017#issuecomment-1140066503
@@ -16,51 +14,11 @@ const AppNoSSRComponent = dynamic(() => import('../features/app/App'), {
   ssr: false,
 })
 
-const HeadDescriptionByLocation = ({ isMarineManagerLocation }) => {
+const Index = () => {
   return (
-    <Head>
-      <meta
-        property="og:description"
-        content={
-          isMarineManagerLocation
-            ? 'The portal provides dynamic interactive data on marine traffic, biology and ocean conditions to support marine protected area design and management.'
-            : 'The Global Fishing Watch map is the first open-access platform for visualization and analysis of marine traffic and vessel-based human activity at sea.'
-        }
-      />
-      <meta
-        name="twitter:description"
-        content={
-          isMarineManagerLocation
-            ? 'The portal provides dynamic interactive data on marine traffic, biology and ocean conditions to support marine protected area design and management.'
-            : 'The Global Fishing Watch map is the first open-access platform for visualization and analysis of marine traffic and vessel-based human activity at sea.'
-        }
-      />
-      <meta
-        name="description"
-        content={
-          isMarineManagerLocation
-            ? 'The portal provides dynamic interactive data on marine traffic, biology and ocean conditions to support marine protected area design and management.'
-            : 'The Global Fishing Watch map is the first open-access platform for visualization and analysis of marine traffic and vessel-based human activity at sea.'
-        }
-      />
-    </Head>
+    <MapProvider>
+      <AppNoSSRComponent />
+    </MapProvider>
   )
-}
-
-class Index extends Component<{ isMarineManagerLocation: boolean }> {
-  static async getInitialProps({ req }) {
-    return { isMarineManagerLocation: req.originalUrl.includes('marine-manager') }
-  }
-
-  render() {
-    return (
-      <Fragment>
-        <HeadDescriptionByLocation isMarineManagerLocation={this.props.isMarineManagerLocation} />
-        <MapProvider>
-          <AppNoSSRComponent />
-        </MapProvider>
-      </Fragment>
-    )
-  }
 }
 export default Index
