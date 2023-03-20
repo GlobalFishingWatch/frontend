5ee3cc914c46224063509c61ed27f0df9771f076
satellitestudiodesign <juancarlos@satellitestud.io>
2023-03-20 13:47:35 +0100

    add share

--- apps/fishing-map/features/sidebar/SidebarHeader.tsx
+++ apps/fishing-map/features/sidebar/SidebarHeader.tsx
@@ -93,6 +93,10 @@ function SaveWorkspaceButton() {
 
 function ShareWorkspaceButton() {
   const { t } = useTranslation()
+  const workspaceLocation = useSelector(selectIsWorkspaceLocation)
+  const shareTitle = workspaceLocation
+    ? t('common.share', 'Share map')
+    : t('analysis.share', 'Share report')
 
   const { showClipboardNotification, copyToClipboard } = useClipboardNotification()
 
@@ -112,7 +116,7 @@ function ShareWorkspaceButton() {
               'common.copiedToClipboard',
               'The link to share this view has been copied to your clipboard'
             )
-          : t('common.share', 'Share the current view')
+          : shareTitle
       }
       tooltipPlacement="bottom"
     />
@@ -132,19 +136,14 @@ function SidebarHeader() {
     if (locationCategory === WorkspaceCategories.MarineManager) subBrand = SubBrands.MarineManager
     return subBrand
   }, [locationCategory])
-
   return (
     <Sticky scrollElement=".scrollContainer">
       <div className={styles.sidebarHeader}>
         <a href="https://globalfishingwatch.org" className={styles.logoLink}>
           <Logo className={styles.logo} subBrand={getSubBrand()} />
         </a>
-        {workspaceLocation && !readOnly && (
-          <Fragment>
-            <SaveWorkspaceButton />
-            <ShareWorkspaceButton />
-          </Fragment>
-        )}
+        {workspaceLocation && !readOnly && <SaveWorkspaceButton />}
+        {(workspaceLocation || reportLocation) && !readOnly && <ShareWorkspaceButton />}
         {(reportLocation || (showBackToWorkspaceButton && lastVisitedWorkspace)) && (
           <Link
             className={styles.workspaceLink}
