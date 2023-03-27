901bda1a54ed80c81c11a0d920be9d0b59d56f09
j8seangel <jseangel@gmail.com>
2023-03-27 13:05:46 +0200

    show disclaimer for JAC users

--- apps/fishing-map/features/user/GFWOnly.tsx
+++ apps/fishing-map/features/user/GFWOnly.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from 'react-i18next'
 import cx from 'classnames'
 import { useSelector } from 'react-redux'
 import { Icon, IconProps } from '@globalfishingwatch/ui-components'
-import { isGFWUser } from 'features/user/user.slice'
+import { isGFWUser, isJACUser } from 'features/user/user.slice'
 import styles from './GFWOnly.module.css'
 
 type GFWOnlyProps = {
@@ -21,23 +21,30 @@ function GFWOnly(props: GFWOnlyProps) {
   const { type = 'default', style = {}, className = '' } = props
   const { t } = useTranslation()
   const gfwUser = useSelector(isGFWUser)
+  const jacUser = useSelector(isJACUser)
 
-  if (!gfwUser) return null
+  if (!gfwUser && !jacUser) return null
+
+  const disclaimerText = jacUser
+    ? t('common.onlyVisibleForJAC', 'Only visible for JAC users')
+    : t('common.onlyVisibleForGFW', 'Only visible for GFW users')
 
   if (type === 'only-icon') {
-    return (
+    return jacUser ? (
+      <span title={disclaimerText}>🔓</span>
+    ) : (
       <Icon
         {...defaultIconProps}
         style={{ ...defaultIconProps.style, ...style }}
         className={className}
-        tooltip={t('common.onlyVisibleForGFW', 'Only visible for GFW users')}
+        tooltip={disclaimerText}
       />
     )
   }
   return (
     <span className={cx(styles.GFWOnly, className)}>
-      <Icon {...defaultIconProps} />
-      {t('common.onlyVisibleForGFW', 'Only visible for GFW users')}
+      {jacUser ? `🔓` : <Icon {...defaultIconProps} />}
+      {disclaimerText}
     </span>
   )
 }
