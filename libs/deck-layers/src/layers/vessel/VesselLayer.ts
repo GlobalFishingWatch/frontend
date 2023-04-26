6d0f6cdad7c05ac7b3ba28d5e17f8bd68743468b
weberjavi <weberjavi@gmail.com>
2023-04-26 15:37:55 +0200

    Add sublayer load logic and types to vesselLayer

--- libs/deck-layers/src/layers/vessel/VesselLayer.ts
+++ libs/deck-layers/src/layers/vessel/VesselLayer.ts
@@ -1,7 +1,7 @@
 import { DataFilterExtension } from '@deck.gl/extensions'
 import { CompositeLayer, Layer, LayersList, LayerProps } from '@deck.gl/core/typed'
 // Layers
-import { Segment } from '@globalfishingwatch/api-types'
+import { ApiEvent, EventVessel, Segment, VesselTrackData } from '@globalfishingwatch/api-types'
 import { trackLoader } from '../../loaders/vessels/trackLoader'
 import { vesselEventsLoader } from '../../loaders/vessels/eventsLoader'
 import { hexToRgb } from '../../utils/layers'
@@ -9,17 +9,22 @@ import { VesselEventsLayer, _VesselEventsLayerProps } from './VesselEventsLayer'
 import { VesselTrackLayer, _VesselTrackLayerProps } from './VesselTrackLayer'
 // Loaders
 export type VesselLayerProps = _VesselTrackLayerProps &
-  _VesselEventsLayerProps & { themeColor: string }
+  _VesselEventsLayerProps & { themeColor: string; layersLoaded: string[] }
 
 export const TRACK_LAYER_PREFIX = 'track'
 export const EVENTS_LAYER_PREFIX = 'events'
 export class VesselLayer extends CompositeLayer<VesselLayerProps & LayerProps> {
-  layersLoaded: Layer[] = []
+  layersLoaded: string[] = []
   layers: Layer[] = []
   data = []
 
-  onDataLoad: LayerProps['onDataLoad'] = (data, context) => {
-    this.props.onDataLoad && this.props.onDataLoad(data, context)
+  onSublayerLoad: LayerProps['onDataLoad'] = (data, context) => {
+    const vesselLayer = context.layer.parent as VesselLayer
+    vesselLayer?.layersLoaded?.push(context.layer.id)
+    const isLastLayer = vesselLayer?.layersLoaded.length === vesselLayer?.layers.length - 1
+    if (isLastLayer) {
+      vesselLayer?.props.onDataLoad && vesselLayer?.props?.onDataLoad(data, context)
+    }
   }
 
   _getVesselTrackLayer() {
@@ -33,8 +38,8 @@ export class VesselLayer extends CompositeLayer<VesselLayerProps & LayerProps> {
         wrapLongitude: true,
         jointRounded: true,
         capRounded: true,
-        onDataLoad: this.onDataLoad,
-        getColor: (d) => {
+        onDataLoad: this.onSublayerLoad,
+        getColor: (d: VesselTrackData) => {
           return d.waypoints.map((p) => {
             if (
               this.props.highlightStartTime &&
@@ -63,7 +68,7 @@ export class VesselLayer extends CompositeLayer<VesselLayerProps & LayerProps> {
         this.getSubLayerProps({
           id: `${EVENTS_LAYER_PREFIX}-${this.props.id}-${index}`,
           data: `https://gateway.api.dev.globalfishingwatch.org${url}`,
-          onDataLoad: this.onDataLoad,
+          onDataLoad: this.onSublayerLoad,
           loaders: [vesselEventsLoader],
           pickable: true,
           startTime: this.props.startTime,
@@ -98,11 +103,16 @@ export class VesselLayer extends CompositeLayer<VesselLayerProps & LayerProps> {
   }
 
   getVesselEventsData() {
-    const events = this.getEventLayers().reduce((acc, l: VesselEventsLayer) => {
-      const events = l.props.eventType ? l.props.data : []
-      return [...acc, events]
-    }, [])
-    const sortedEvents = events.flat().sort((a, b) => a.start - b.start)
+    const events = this.getEventLayers().reduce(
+      (acc: ApiEvent[], l: VesselEventsLayer): ApiEvent<EventVessel>[] => {
+        const events = l.props.eventType ? l.props.data : []
+        return [...acc, events] as ApiEvent[]
+      },
+      []
+    )
+    const sortedEvents: ApiEvent[] = events
+      .flat()
+      .sort((a, b) => (a.start as number) - (b.start as number))
     return sortedEvents
   }
 
