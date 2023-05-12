9d6718a072bdf734969bc1fb7db8b7476f84bb0f
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2023-05-12 10:14:11 -0300

    Clear local storage before each app test

--- apps/fishing-map-e2e/src/e2e/app.cy.ts
+++ apps/fishing-map-e2e/src/e2e/app.cy.ts
@@ -1,7 +1,10 @@
 import { getGreeting } from '../support/app.po'
 
 describe('fishing-map', () => {
-  beforeEach(() => cy.visit('/'))
+  beforeEach(() => {
+    cy.clearLocalStorage()
+    cy.visit('/')
+  })
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
