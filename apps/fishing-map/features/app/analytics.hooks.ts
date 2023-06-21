ac8bd076c5ca04f8fec9be24c1841d63e6432f9d
j8seangel <jseangel@gmail.com>
2023-06-21 21:41:13 +0200

    fix circular dependencies

--- apps/fishing-map/features/app/analytics.hooks.ts
+++ apps/fishing-map/features/app/analytics.hooks.ts
@@ -2,11 +2,13 @@ import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { initialize as uaInitialize, set as uaSet, event as uaEvent, pageview } from 'react-ga'
 import { selectUserData } from 'features/user/user.slice'
-import { GOOGLE_UNIVERSAL_ANALYTICS_INIT_OPTIONS, IS_PRODUCTION } from 'data/config'
+import {
+  GOOGLE_TAG_MANAGER_ID,
+  GOOGLE_UNIVERSAL_ANALYTICS_INIT_OPTIONS,
+  GOOGLE_UNIVERSAL_ANALYTICS_ID,
+  IS_PRODUCTION,
+} from 'data/config'
 import { selectLocationCategory } from 'routes/routes.selectors'
-
-export const GOOGLE_UNIVERSAL_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_UNIVERSAL_ANALYTICS_ID
-export const GOOGLE_TAG_MANAGER_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID || 'GTM-KK5ZFST'
 
 export enum TrackCategory {
   ActivityData = 'Activity data',
