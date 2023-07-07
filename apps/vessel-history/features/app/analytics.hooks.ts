c0bda895f963585d31deb180647104f3fdd05c7d
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2023-07-07 17:12:50 -0300

    Refactor use analytics hook into a lib hook to be reused

--- apps/vessel-history/features/app/analytics.hooks.ts
+++ apps/vessel-history/features/app/analytics.hooks.ts
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react'
-import { snakeCase } from 'lodash'
-import ReactGA from 'react-ga4'
-import { InitOptions } from 'react-ga4/types/ga4'
+import {
+  trackEvent as trackEventBase,
+  useAnalytics as useAnalyticsBase,
+} from '@globalfishingwatch/react-hooks'
 import {
   GOOGLE_TAG_MANAGER_ID,
   GOOGLE_MEASUREMENT_ID,
@@ -24,82 +24,17 @@ export enum TrackCategory {
   VesselDetail = 'vessel_detail',
 }
 
-export type TrackEventParams = {
-  category: TrackCategory
-  action: string
-  label?: string
-  value?: any
-}
-
-export const trackEvent = ({ category, action, label, value }: TrackEventParams) => {
-  /**
-   * IMPORTANT
-   *
-   * To send the category and action in snake_case to GA4
-   * it is necessary to use this:
-   * ```
-   * ReactGA.event(name, params)
-   * ```
-   * method signature so they won't be converted to title case.
-   *
-   * https://github.com/codler/react-ga4/issues/15
-   */
-  ReactGA.event(snakeCase(action), {
-    category: snakeCase(category),
-    label: label,
-    value,
-  })
-}
+export const trackEvent = trackEventBase<TrackCategory>
 
 export const useAnalytics = () => {
-  useEffect(() => {
-    const config: InitOptions[] = []
-    const initGtagOptions: any = {
-      // Debug Mode enables the usage of Debug View in Google Analytics > Admin
-      ...(GOOGLE_ANALYTICS_DEBUG_MODE ? { debug_mode: 1 } : {}),
-    }
-    if (GOOGLE_TAG_MANAGER_ID) {
-      config.push({ trackingId: GOOGLE_TAG_MANAGER_ID })
-      initGtagOptions.gtagUrl = 'https://www.googletagmanager.com/gtm.js'
-    }
-    if (GOOGLE_MEASUREMENT_ID) {
-      config.push({ trackingId: GOOGLE_MEASUREMENT_ID })
-    }
-
-    if (config.length > 0) {
-      ReactGA.initialize(config, initGtagOptions)
-      // Tip: Uncomment this to prevent sending hits to GA
-      // ReactGA.set({ sendHitTask: null })
-    }
-  }, [])
-
-  useEffect(() => {
-    if (GOOGLE_MEASUREMENT_ID || GOOGLE_TAG_MANAGER_ID) {
-      ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search })
-    }
-  }, [])
   const { user, logged } = useUser()
-  const [trackLogin, setTrackLogin] = useState(true)
-
-  // Set to track login only when the user has logged out
-  useEffect(() => {
-    !logged && setTrackLogin(true)
-  }, [logged])
-
-  useEffect(() => {
-    if (user && (GOOGLE_MEASUREMENT_ID || GOOGLE_TAG_MANAGER_ID) && trackLogin) {
-      ReactGA.set({
-        user_country: user.country ?? '',
-        user_group: user.groups ?? '',
-        user_org_type: user.organizationType ?? '',
-        user_organization: user.organization ?? '',
-        user_language: user.language ?? '',
-      })
-      trackEvent({
-        category: TrackCategory.User,
-        action: 'Login',
-      })
-      setTrackLogin(false)
-    }
-  }, [user, trackLogin])
+  useAnalyticsBase({
+    debugMode: GOOGLE_ANALYTICS_DEBUG_MODE,
+    googleMeasurementId: GOOGLE_MEASUREMENT_ID,
+    googleTagManagerId: GOOGLE_TAG_MANAGER_ID,
+    gtagUrl: 'https://www.googletagmanager.com/gtm.js',
+    isLoading: false,
+    logged,
+    user,
+  })
 }
