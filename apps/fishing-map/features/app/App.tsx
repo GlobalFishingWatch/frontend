24fda5c4eac0ae0d99845520902a08549a1ebc68
j8seangel <jseangel@gmail.com>
2023-07-18 10:19:00 +0200

    useSetViewstate review

--- apps/fishing-map/features/app/App.tsx
+++ apps/fishing-map/features/app/App.tsx
@@ -25,7 +25,7 @@ import {
 import { fetchUserThunk } from 'features/user/user.slice'
 import { fetchHighlightWorkspacesThunk } from 'features/workspaces-list/workspaces-list.slice'
 import { AsyncReducerStatus } from 'utils/async-slice'
-import { useViewStateAtom, useMapFitBounds } from 'features/map/map-viewport.hooks'
+import { useMapFitBounds, useSetViewstate } from 'features/map/map-viewport.hooks'
 import { selectShowTimeComparison } from 'features/reports/reports.selectors'
 import { isUserLogged } from 'features/user/user.selectors'
 import { DEFAULT_WORKSPACE_ID } from 'data/workspaces'
@@ -134,7 +134,7 @@ function App() {
   }, [])
 
   const fitMapBounds = useMapFitBounds()
-  const { viewState, setViewState } = useViewStateAtom()
+  const setViewState = useSetViewstate()
   const { setTimerange } = useTimerangeConnect()
 
   const locationType = useSelector(selectLocationType)
@@ -162,9 +162,9 @@ function App() {
       const resolvedAction = await action
       if (fetchWorkspaceThunk.fulfilled.match(resolvedAction)) {
         const workspace = resolvedAction.payload as Workspace
-        const vs = urlViewport || workspace?.viewport
-        if (vs && !isReportLocation) {
-          setViewState({ ...viewState, vs })
+        const viewport = urlViewport || workspace?.viewport
+        if (viewport && !isReportLocation) {
+          setViewState((vs) => ({ ...vs, viewport }))
         }
         if (!urlTimeRange && workspace?.startAt && workspace?.endAt) {
           setTimerange({
@@ -197,7 +197,7 @@ function App() {
       if (reportAreaBounds) {
         fitMapBounds(reportAreaBounds, { padding: FIT_BOUNDS_REPORT_PADDING })
       } else {
-        setViewState({ ...viewState, latitude: 0, longitude: 0, zoom: 0 })
+        setViewState((vs) => ({ ...vs, latitude: 0, longitude: 0, zoom: 0 }))
       }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
