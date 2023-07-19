0d0c755cd3a4373ae73b555e9f4fbe155bcd0499
weberjavi <weberjavi@gmail.com>
2023-07-19 14:59:00 +0200

    Add classname prop to tooltip to allow customization

--- libs/ui-components/src/tooltip/Tooltip.tsx
+++ libs/ui-components/src/tooltip/Tooltip.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Tippy, { TippyProps } from '@tippyjs/react'
+import cx from 'classnames'
 import styles from './Tooltip.module.css'
 
 export function Tooltip(props: TippyProps) {
@@ -7,7 +8,7 @@ export function Tooltip(props: TippyProps) {
   if (!props.content) return <React.Fragment>{props.children}</React.Fragment>
   return (
     <Tippy
-      className={styles.tooltip}
+      className={cx(styles.tooltip, props.className)}
       duration={props.duration || 100}
       delay={props.delay || 500}
       {...props}
