15a1623d0b49065e5e472a0952d2aa64c61fbc84
j8seangel <jseangel@gmail.com>
2023-08-09 18:57:50 +0200

    useEventKeyListener

---
+++ libs/react-hooks/src/use-event-listener/use-event-key-listener.ts
@@ -0,0 +1,19 @@
+import { useRef, useEffect } from 'react'
+
+export function useEventKeyListener(keys: string[], callback?: (e: KeyboardEvent) => void) {
+  const parentNode = useRef<HTMLDivElement>()
+
+  useEffect(() => {
+    const eventHandler = (event: KeyboardEvent) => {
+      const isEventInParent = parentNode.current?.contains(event.target as Node)
+      if (isEventInParent && keys.includes(event.key) && callback) {
+        callback(event)
+      }
+    }
+    document.addEventListener('keydown', eventHandler)
+    return () => document.removeEventListener('keydown', eventHandler)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return parentNode as React.MutableRefObject<HTMLDivElement>
+}
