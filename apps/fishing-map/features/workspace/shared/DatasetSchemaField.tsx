1bd7e914dce61364ad7020ca432b6eb82f7f886f
j8seangel <jseangel@gmail.com>
2023-08-10 11:37:02 +0200

    remove VESSEL_GROUPS_DAYS_LIMIT

--- apps/fishing-map/features/workspace/shared/DatasetSchemaField.tsx
+++ apps/fishing-map/features/workspace/shared/DatasetSchemaField.tsx
@@ -1,6 +1,4 @@
 import { Fragment } from 'react'
-import cx from 'classnames'
-import { useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
 import { formatSliderNumber, TagList } from '@globalfishingwatch/ui-components'
 import { UrlDataviewInstance } from '@globalfishingwatch/dataviews-client'
@@ -13,9 +11,6 @@ import {
   SupportedDatasetSchema,
 } from 'features/datasets/datasets.utils'
 import { useVesselGroupsOptions } from 'features/vessel-groups/vessel-groups.hooks'
-import { selectTimeRange } from 'features/app/app.selectors'
-import { getTimeRangeDuration } from 'utils/dates'
-import { VESSEL_GROUPS_DAYS_LIMIT } from 'data/config'
 import { VALUE_TRANSFORMATIONS_BY_UNIT } from 'features/workspace/activity/ActivitySchemaFilter'
 
 type LayerPanelProps = {
@@ -26,8 +21,6 @@ type LayerPanelProps = {
 
 function DatasetSchemaField({ dataview, field, label }: LayerPanelProps): React.ReactElement {
   const { t } = useTranslation()
-  const timeRange = useSelector(selectTimeRange)
-  const duration = getTimeRangeDuration(timeRange, 'days')
   const vesselGroupsOptions = useVesselGroupsOptions()
   const filterOperation = getSchemaFilterOperationInDataview(dataview, field)
   const filterUnit = getSchemaFilterUnitInDataview(dataview, field)
@@ -75,17 +68,6 @@ function DatasetSchemaField({ dataview, field, label }: LayerPanelProps): React.
           <label>
             {label}
             {filterOperation === EXCLUDE_FILTER_ID && ` (${t('common.excluded', 'Excluded')})`}
-            {VESSEL_GROUPS_DAYS_LIMIT > 0 &&
-              field === 'vessel-groups' &&
-              duration!?.days > VESSEL_GROUPS_DAYS_LIMIT && (
-                <span className={cx(styles.dataWarning, styles.error)}>
-                  {' '}
-                  {t(
-                    'vesselGroup.timeRangeLimit',
-                    'Supported only for time ranges shorter than 3 months'
-                  )}
-                </span>
-              )}
           </label>
           <TagList
             tags={valuesSelected}
