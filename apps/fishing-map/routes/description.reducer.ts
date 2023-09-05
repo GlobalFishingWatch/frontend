9508fec63e578139c58050017271c48bb59de328
j8seangel <jseangel@gmail.com>
2023-09-05 19:27:27 +0200

    updates pages title and description

--- apps/fishing-map/routes/description.reducer.ts
+++ apps/fishing-map/routes/description.reducer.ts
@@ -1,6 +1,6 @@
 import { PayloadAction } from '@reduxjs/toolkit'
 import { t } from 'features/i18n/i18n'
-import { HOME, WORKSPACE, WORKSPACES_LIST } from './routes'
+import { HOME, SEARCH, WORKSPACE, WORKSPACES_LIST, WORKSPACE_SEARCH } from './routes'
 
 const DEFAULT = `Through our free and open data transparency platform, Global Fishing Watch enables research and innovation in support of ocean sustainability.`
 
@@ -11,6 +11,10 @@ const descriptionReducer = (state = DEFAULT, action: PayloadAction<{ category?: 
     case WORKSPACE:
     case WORKSPACES_LIST: {
       return t(`workspace.siteDescription.${action.payload.category}`, 'default description')
+    }
+    case SEARCH:
+    case WORKSPACE_SEARCH: {
+      return t(`workspace.siteDescription.search`, 'search description')
     }
     default:
       return state
