92738971710cad0fb0a0baa6b5d6d37d24d19b85
j8seangel <jseangel@gmail.com>
2023-09-19 18:41:40 +0200

    remove camelcase in helphints

--- apps/fishing-map/features/i18n/react-i18next.d.ts
+++ apps/fishing-map/features/i18n/react-i18next.d.ts
@@ -3,13 +3,13 @@ import 'react-i18next'
 // import all namespaces (for the default language, only)
 import type { datasets, flags, timebar } from '@globalfishingwatch/i18n-labels'
 import type translations from '../../public/locales/source/translations.json'
-import type helpHints from '../../public/locales/source/helpHints.json'
+import type helphints from '../../public/locales/source/helphints.json'
 
 declare module 'react-i18next' {
   // and extend them!
   interface Resources {
     translations: typeof translations
-    helpHints: typeof helpHints
+    helphints: typeof helphints
     datasets: typeof datasets
     timebar: typeof timebar
     flags: typeof flags
