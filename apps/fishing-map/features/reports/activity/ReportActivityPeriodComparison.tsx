6efe69e5dff7dc152c6764dfb771f38698669b0f
j8seangel <jseangel@gmail.com>
2023-09-28 08:04:48 +0200

    report.config file to avoid circular dependencies

--- apps/fishing-map/features/reports/activity/ReportActivityPeriodComparison.tsx
+++ apps/fishing-map/features/reports/activity/ReportActivityPeriodComparison.tsx
@@ -2,11 +2,7 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
 import { InputDate, InputText, Select } from '@globalfishingwatch/ui-components'
-import {
-  MAX_DAYS_TO_COMPARE,
-  MAX_MONTHS_TO_COMPARE,
-  useReportTimeCompareConnect,
-} from 'features/reports/reports-timecomparison.hooks'
+import { useReportTimeCompareConnect } from 'features/reports/reports-timecomparison.hooks'
 import { selectReportTimeComparison } from 'features/app/app.selectors'
 import { selectActiveHeatmapDataviews } from 'features/dataviews/dataviews.selectors'
 import { getSourcesSelectedInDataview } from 'features/workspace/activity/activity.utils'
@@ -15,6 +11,7 @@ import { selectDatasetAreaDetail } from 'features/areas/areas.slice'
 import Hint from 'features/help/Hint'
 import { COLOR_PRIMARY_BLUE } from 'features/app/App'
 import { TrackCategory, trackEvent } from 'features/app/analytics.hooks'
+import { MAX_MONTHS_TO_COMPARE, MAX_DAYS_TO_COMPARE } from 'features/reports/reports.config'
 import styles from './ReportActivityBeforeAfter.module.css'
 
 export default function ReportActivityGraph() {
---
+++ apps/fishing-map/features/reports/reports.config.ts
@@ -0,0 +1,9 @@
+export type LastReportStorage = { reportUrl: string; workspaceUrl: string }
+export const LAST_REPORTS_STORAGE_KEY = 'lastReports'
+
+export const EMPTY_API_VALUES = ['NULL', undefined, '']
+export const MAX_CATEGORIES = 5
+export const OTHERS_CATEGORY_LABEL = 'OTHERS'
+
+export const MAX_DAYS_TO_COMPARE = 100
+export const MAX_MONTHS_TO_COMPARE = 12
