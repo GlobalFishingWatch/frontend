9f0290d696310efec421c450b78c54cf97dc2f41
j8seangel <jseangel@gmail.com>
2023-10-03 09:04:37 +0200

    update next-i18n

--- tools/generators/app/files/features/i18n/react-i18next.d.ts
+++ tools/generators/app/files/features/i18n/react-i18next.d.ts
@@ -1,15 +1,20 @@
 // import the original type declarations
-import 'react-i18next'
+import 'i18next'
 // import all namespaces (for the default language, only)
 import type { datasets, flags, timebar } from '@globalfishingwatch/i18n-labels'
 import type translations from '../../public/locales/source/translations.json'
 
-declare module 'react-i18next' {
-  // and extend them!
-  interface Resources {
-    translations: typeof translations
-    datasets: typeof datasets
-    timebar: typeof timebar
-    flags: typeof flags
+declare module "i18next" {
+  // Extend CustomTypeOptions
+  interface CustomTypeOptions {
+    // custom namespace type, if you changed it
+    defaultNS: "translations";
+    // custom resources type
+    resources: {
+      translations: typeof translations
+      datasets: typeof datasets
+      timebar: typeof timebar
+      flags: typeof flags
+    };
   }
 }
