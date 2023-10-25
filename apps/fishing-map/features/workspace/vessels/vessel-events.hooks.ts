d397b1cc823b31ab52b133246c56766a5367cabc
j8seangel <jseangel@gmail.com>
2023-10-25 10:51:26 +0200

    fix events toggle race condition

--- apps/fishing-map/features/workspace/vessels/vessel-events.hooks.ts
+++ apps/fishing-map/features/workspace/vessels/vessel-events.hooks.ts
@@ -1,40 +1,30 @@
 import { useCallback, useMemo } from 'react'
-import { uniqBy } from 'lodash'
 import { useSelector } from 'react-redux'
 import { EventType } from '@globalfishingwatch/api-types'
 import { UrlDataviewInstance } from '@globalfishingwatch/dataviews-client'
 import { selectVisibleEvents } from 'features/app/app.selectors'
-import { getEventsDatasetsInDataview } from 'features/datasets/datasets.utils'
-import { selectVesselsDataviews } from 'features/dataviews/dataviews.slice'
 import { useLocationConnect } from 'routes/routes.hook'
+
+const ALL_EVENT_TYPES: EventType[] = ['fishing', 'loitering', 'encounter', 'port_visit']
 
 export const useVesselEvents = (dataviews = [] as UrlDataviewInstance[]) => {
   const { dispatchQueryParams } = useLocationConnect()
   const currentVisibleEvents = useSelector(selectVisibleEvents)
-  const vesselDataviews = useSelector(selectVesselsDataviews)
 
-  const eventDatasets = uniqBy(
-    (dataviews?.length ? dataviews : vesselDataviews)?.flatMap((dataview) =>
-      getEventsDatasetsInDataview(dataview)
-    ),
-    'id'
-  )
-
-  const allEventTypes = eventDatasets.flatMap((dataset) => dataset.subcategory || [])
   const setVesselEventVisibility = useCallback(
     ({ event, visible }: { event: EventType; visible: boolean }) => {
       if (visible) {
         const visibleEvents: any =
           currentVisibleEvents === 'all'
-            ? allEventTypes.filter((eventType) => eventType !== event)
+            ? ALL_EVENT_TYPES.filter((eventType) => eventType !== event)
             : [...(currentVisibleEvents === 'none' ? [] : currentVisibleEvents), event]
         dispatchQueryParams({
-          visibleEvents: visibleEvents?.length === allEventTypes.length ? 'all' : visibleEvents,
+          visibleEvents: visibleEvents?.length === ALL_EVENT_TYPES.length ? 'all' : visibleEvents,
         })
       } else {
         const currentVisibleEventsTypes =
           currentVisibleEvents === 'all'
-            ? allEventTypes
+            ? ALL_EVENT_TYPES
             : currentVisibleEvents === 'none'
             ? []
             : currentVisibleEvents
@@ -46,7 +36,7 @@ export const useVesselEvents = (dataviews = [] as UrlDataviewInstance[]) => {
         })
       }
     },
-    [dispatchQueryParams, allEventTypes, currentVisibleEvents]
+    [dispatchQueryParams, currentVisibleEvents]
   )
 
   return useMemo(
