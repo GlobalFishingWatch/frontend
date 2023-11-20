a22f469a29eb96ea0d7b27cbdf37acbce6f23701
j8seangel <jseangel@gmail.com>
2023-11-20 17:12:42 +0100

    extract file dropzone utils

--- apps/fishing-map/utils/files.ts
+++ apps/fishing-map/utils/files.ts
@@ -1,5 +1,46 @@
 import type { Feature, FeatureCollection } from 'geojson'
 
+export type FileType = 'geojson' | 'shapefile' | 'csv'
+export type MimeExtention = '.json' | '.geojson' | '.zip' | '.csv'
+export type MimeType = 'application/json' | 'application/zip' | 'text/csv'
+
+export const MIME_TYPES_BY_EXTENSION: Record<MimeExtention, MimeType> = {
+  '.json': 'application/json',
+  '.geojson': 'application/json',
+  '.zip': 'application/zip',
+  '.csv': 'text/csv',
+}
+
+type FileConfig = { id: string; files: string[]; icon: string }
+
+export const FILE_TYPES_CONFIG: Record<FileType, FileConfig> = {
+  geojson: {
+    id: 'geojson',
+    files: ['.json', '.geojson'],
+    icon: 'geojson',
+  },
+  shapefile: { id: 'shapefile', files: ['.zip'], icon: 'zip' },
+  csv: { id: 'csv', files: ['.csv'], icon: 'csv' },
+}
+
+export function getFilesAcceptedByMime(fileTypes: FileType[]) {
+  const fileTypesConfigs = fileTypes.map((fileType) => FILE_TYPES_CONFIG[fileType])
+  const filesAcceptedExtensions = fileTypesConfigs.flatMap(({ files }) => files as MimeExtention[])
+  const fileAcceptedByMime = filesAcceptedExtensions.reduce(
+    (acc, extension) => {
+      const mime = MIME_TYPES_BY_EXTENSION[extension]
+      if (!acc[mime]) {
+        acc[mime] = [extension]
+      } else {
+        acc[mime].push(extension)
+      }
+      return acc
+    },
+    {} as Record<MimeType, MimeExtention[]>
+  )
+  return fileAcceptedByMime
+}
+
 export function readBlobAs(blob: Blob, format: 'text' | 'arrayBuffer'): Promise<string>
 export function readBlobAs(blob: Blob, format: 'text' | 'arrayBuffer'): Promise<ArrayBuffer>
 export function readBlobAs(blob: Blob, format: 'text' | 'arrayBuffer'): any {
