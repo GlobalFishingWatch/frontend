4fca1ece252b7aaf774358fb6646eeb01fc898ac
satellitestudiodesign <juancarlos@satellitestud.io>
2023-12-11 13:11:13 +0100

    add fieldType icon to options

--- apps/fishing-map/features/datasets/upload/datasets-upload.hooks.tsx
+++ apps/fishing-map/features/datasets/upload/datasets-upload.hooks.tsx
@@ -1,5 +1,10 @@
 import { useCallback, useMemo, useState } from 'react'
-import { DatasetConfiguration, DatasetConfigurationUI } from '@globalfishingwatch/api-types'
+import {
+  DatasetConfiguration,
+  DatasetConfigurationUI,
+  DatasetSchema,
+  DatasetSchemaItem,
+} from '@globalfishingwatch/api-types'
 import { SelectOption } from '@globalfishingwatch/ui-components'
 import { MultiSelectOption } from '@globalfishingwatch/api-client'
 import { getDatasetConfigurationProperty } from '@globalfishingwatch/datasets-client'
@@ -49,11 +54,31 @@ export function useDatasetMetadata() {
   )
 }
 
+const OptionLabel = ({
+  field,
+  fieldSchema,
+}: {
+  field: string
+  fieldSchema: DatasetSchema | DatasetSchemaItem
+}) => {
+  return (
+    <span>
+      <label style={{ display: 'inline', marginRight: 'var(--space-XS)' }}>
+        {fieldSchema.type === 'range' ? '123' : 'ABC'}
+      </label>
+      {field}
+    </span>
+  )
+}
+
 export function useDatasetMetadataOptions(datasetMetadata?: DatasetMetadata) {
   const fieldsOptions: SelectOption[] | MultiSelectOption[] = useMemo(() => {
     const options = datasetMetadata?.schema
-      ? Object.keys(datasetMetadata.schema).map((field) => {
-          return { id: field, label: field }
+      ? Object.entries(datasetMetadata.schema).map(([field, fieldSchema]) => {
+          return {
+            id: field,
+            label: <OptionLabel field={field} fieldSchema={fieldSchema} />,
+          }
         })
       : []
     return options.sort(sortFields)
@@ -77,10 +102,10 @@ export function useDatasetMetadataOptions(datasetMetadata?: DatasetMetadata) {
 
   const schemaRangeOptions: SelectOption[] | MultiSelectOption[] = useMemo(() => {
     const options = datasetMetadata?.schema
-      ? Object.keys(datasetMetadata.schema)
-          .filter((field) => datasetMetadata.schema?.[field].type === 'range')
-          .map((field) => {
-            return { id: field, label: field }
+      ? Object.entries(datasetMetadata.schema)
+          .filter(([_, fieldSchema]) => fieldSchema.type === 'range')
+          .map(([field, fieldSchema]) => {
+            return { id: field, label: <OptionLabel field={field} fieldSchema={fieldSchema} /> }
           })
       : []
     return options.sort(sortFields)
@@ -93,7 +118,9 @@ export function useDatasetMetadataOptions(datasetMetadata?: DatasetMetadata) {
           const isEnumAllowed =
             (schema?.type === 'string' || schema?.type === 'boolean') && schema?.enum?.length
           const isRangeAllowed = schema?.type === 'range' && schema?.min && schema?.max
-          return isEnumAllowed || isRangeAllowed ? { id: field, label: field } : []
+          return isEnumAllowed || isRangeAllowed
+            ? { id: field, label: <OptionLabel field={field} fieldSchema={schema} /> }
+            : []
         })
       : []
     return options
