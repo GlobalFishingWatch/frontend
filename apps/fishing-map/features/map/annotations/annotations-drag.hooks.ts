bfbb3869973aa1e327d6bed460d4cda8d99bd8be
j8seangel <jseangel@gmail.com>
2023-12-20 12:56:45 +0100

    block guestUsers to edit existing annotations

--- apps/fishing-map/features/map/annotations/annotations-drag.hooks.ts
+++ apps/fishing-map/features/map/annotations/annotations-drag.hooks.ts
@@ -7,10 +7,12 @@ import useMapInstance from 'features/map/map-context.hooks'
 import { ANNOTATIONS_GENERATOR_ID } from 'features/map/map.config'
 import { selectMapAnnotations } from 'features/app/app.selectors'
 import { useLocationConnect } from 'routes/routes.hook'
+import { selectIsGuestUser } from 'features/user/user.slice'
 
 export function useMapAnnotationDrag() {
   const map = useMapInstance()
   const annotations = useSelector(selectMapAnnotations)
+  const isGuestUser = useSelector(selectIsGuestUser)
   const { dispatchQueryParams } = useLocationConnect()
 
   const currentAnnotationRef = useRef<MapAnnotation | null>(null)
@@ -66,7 +68,7 @@ export function useMapAnnotationDrag() {
   )
 
   useEffect(() => {
-    if (map) {
+    if (map && !isGuestUser) {
       map.on('mousedown', ANNOTATIONS_GENERATOR_ID, onDown)
     }
     return () => {
@@ -74,5 +76,5 @@ export function useMapAnnotationDrag() {
         map.off('mousedown', ANNOTATIONS_GENERATOR_ID, onDown)
       }
     }
-  }, [map, onDown])
+  }, [isGuestUser, map, onDown])
 }
