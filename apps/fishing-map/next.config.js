ac53421c2cf2300e357e6b11ea0c9c8e3c0aa2ea
j8seangel <jseangel@gmail.com>
2024-03-07 19:37:57 +0100

    remove unneded jotai code

--- apps/fishing-map/next.config.js
+++ apps/fishing-map/next.config.js
@@ -89,9 +89,9 @@ const nextConfig = {
     }
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
-      ...(!IS_PRODUCTION && {
-        jotai: path.resolve(__dirname, 'node_modules/jotai'),
-      }),
+      // ...(!IS_PRODUCTION && {
+      //   jotai: path.resolve(__dirname, 'node_modules/jotai'),
+      // }),
     }
     config.externals = [...config.externals, 'mapbox-gl']
     // config.optimization.minimize = false
