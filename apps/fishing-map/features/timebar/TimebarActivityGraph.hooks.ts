4973a4a9d41d652386934206971cd9a4d31d32ff
j8seangel <jseangel@gmail.com>
2024-03-12 17:27:14 +0100

    re-calculate timebar graph on viewport changes

--- apps/fishing-map/features/timebar/TimebarActivityGraph.hooks.ts
+++ apps/fishing-map/features/timebar/TimebarActivityGraph.hooks.ts
@@ -4,12 +4,14 @@ import { useGetDeckLayer } from '@globalfishingwatch/deck-layer-composer'
 import { FourwingsLayer } from '@globalfishingwatch/deck-layers'
 import { getMergedDataviewId } from '@globalfishingwatch/dataviews-client'
 import { ActivityTimeseriesFrame } from '@globalfishingwatch/timebar'
+import { useDebounce } from '@globalfishingwatch/react-hooks'
 import { TimebarVisualisations } from 'types'
 import {
   selectActiveActivityDataviews,
   selectActiveDetectionsDataviews,
 } from 'features/dataviews/selectors/dataviews.selectors'
 import { selectTimebarVisualisation } from 'features/app/selectors/app.timebar.selectors'
+import { useMapViewport } from 'features/map/map-viewport.hooks'
 import { getGraphDataFromFourwingsFeatures } from './timebar.utils'
 
 const EMPTY_ACTIVITY_DATA = [] as ActivityTimeseriesFrame[]
@@ -18,6 +20,12 @@ export const useHeatmapActivityGraph = () => {
   const timebarVisualisation = useSelector(selectTimebarVisualisation)
   const detectionsDataviews = useSelector(selectActiveDetectionsDataviews)
   const activityDataviews = useSelector(selectActiveActivityDataviews)
+  const viewport = useMapViewport()
+  const viewportChangeHash = useMemo(() => {
+    if (!viewport) return ''
+    return [viewport.zoom, viewport.latitude, viewport.longitude].map((v) => v.toFixed(5)).join(',')
+  }, [viewport])
+  const debouncedViewportChangeHash = useDebounce(viewportChangeHash, 400)
   const dataviews =
     timebarVisualisation === TimebarVisualisations.HeatmapDetections
       ? detectionsDataviews
@@ -33,7 +41,7 @@ export const useHeatmapActivityGraph = () => {
     }
     return EMPTY_ACTIVITY_DATA
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [loaded])
+  }, [loaded, debouncedViewportChangeHash])
 
   return useMemo(() => ({ loading: !loaded, heatmapActivity }), [heatmapActivity, loaded])
 }
