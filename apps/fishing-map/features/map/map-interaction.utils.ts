153aff6dc720d98ac0ef0333085f8f60f0f0a1f8
weberjavi <weberjavi@gmail.com>
2024-03-21 15:43:57 +0100

    Fix build because missing module

--- apps/fishing-map/features/map/map-interaction.utils.ts
+++ apps/fishing-map/features/map/map-interaction.utils.ts
@@ -1,4 +1,4 @@
-import { PickingInfo } from '@deck.gl/core/typed'
+import { PickingInfo } from '@deck.gl/core'
 
 export const isRulerLayerPoint = (info: PickingInfo) =>
   info.sourceLayer?.id === 'RulersLayer-ruler-layer' && info.object.geometry.type === 'Point'
