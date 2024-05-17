efc58e1b80a04db1d072bbda7a2df70ee6f72ba0
satellitestudiodesign <juancarlos@satellitestud.io>
2024-05-17 15:52:57 +0200

    fix vessel profile access

--- apps/fishing-map/features/vessel/selectors/vessel.selectors.ts
+++ apps/fishing-map/features/vessel/selectors/vessel.selectors.ts
@@ -5,20 +5,20 @@ import { RootState } from 'store'
 import { IdentityVesselData } from 'features/vessel/vessel.slice'
 
 export const selectVessel = (state: RootState) => {
-  const vesselId = selectVesselId(state as any) as string
-  return state.vessel.data?.[vesselId]
+  const vesselId = selectVesselId(state)
+  // TODO:deck review why the vessel info is no longer saved in state.vessel.data
+  //   return state.vessel.data?.[vesselId]
+  return state.vessel[vesselId]
 }
 export const selectVesselInfoData = createSelector(
   [selectVessel],
   (vessel) => vessel?.data as IdentityVesselData
 )
 export const selectVesselInfoDataId = createSelector([selectVessel], (vessel) => vessel?.data?.id)
-export const selectSelfReportedVesselIds = createSelector(
-  [selectVessel],
-  (vessel) =>
-    vessel?.data?.identities
-      ?.filter((i) => i.identitySource === VesselIdentitySourceEnum.SelfReported)
-      .map((i) => i.id)
+export const selectSelfReportedVesselIds = createSelector([selectVessel], (vessel) =>
+  vessel?.data?.identities
+    ?.filter((i) => i.identitySource === VesselIdentitySourceEnum.SelfReported)
+    .map((i) => i.id)
 )
 export const selectVesselInfoStatus = createSelector([selectVessel], (vessel) => vessel?.status)
 export const selectVesselInfoError = createSelector([selectVessel], (vessel) => vessel?.error)
