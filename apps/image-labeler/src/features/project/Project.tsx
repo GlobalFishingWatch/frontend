6ae226b762968932a6347e1c42d5961591a884e8
satellitestudiodesign <juancarlos@satellitestud.io>
2024-06-05 16:27:40 +0200

    load more tasks, keep scroll position and project info

--- apps/image-labeler/src/features/project/Project.tsx
+++ apps/image-labeler/src/features/project/Project.tsx
@@ -1,6 +1,7 @@
 import { getRouteApi } from '@tanstack/react-router'
 import { useEffect, useState } from 'react'
 import { Spinner } from '@globalfishingwatch/ui-components/spinner'
+import { Button } from '@globalfishingwatch/ui-components/button'
 import { useGetLabellingProjectTasksQuery } from '../../api/project'
 import styles from './Project.module.css'
 import Task, { LabellingTask } from './Task'
@@ -29,11 +30,35 @@ export function Project() {
       (task) => task.id === activeTaskId
     )
     setActiveTaskId((data?.entries as LabellingTask[])[currentIndex + 1]?.id)
+    console.log(window)
+
+    window.scrollTo(0, window.scrollY + 71)
+  }
+
+  const handleLoadMoreTasks = () => {
+    window.scrollTo(0, 0)
+    window.location.reload()
   }
 
   return (
-    <div>
-      <h1 className={styles.pageTitle}>Project: {projectId}</h1>
+    <div className={styles.project}>
+      <h1 className={styles.pageTitle}>{data.metadata.name}</h1>
+      <div className={styles.projectInfo}>
+        <div className={styles.projectInfoItem}>
+          <label>Big Query Table</label>
+          <div>{data.metadata.bqTable}</div>
+        </div>
+        <div className={styles.projectInfoItem}>
+          <label>Query</label>
+          <code>{data.metadata.bqQuery}</code>
+        </div>
+        <div className={styles.projectInfoItem}>
+          <label>Labels</label>
+          <div>{data.metadata.labels.join(', ')}</div>
+        </div>
+      </div>
+
+      <h2 className={styles.tasksTitle}>Tasks</h2>
       {(data?.entries as LabellingTask[]).map((task, index) => (
         <Task
           projectId={projectId}
@@ -43,6 +68,9 @@ export function Project() {
           onFinishTask={setNextTask}
         />
       ))}
+      <Button onClick={handleLoadMoreTasks} className={styles.loadMoreButton} type="secondary">
+        Load more tasks
+      </Button>
     </div>
   )
 }
