6bc74bd9cd6157531b409984fed0f07fba75cb11
satellitestudiodesign <juancarlos@satellitestud.io>
2024-06-07 15:30:37 +0200

    remove import

--- apps/image-labeler/src/api/index.ts
+++ apps/image-labeler/src/api/index.ts
@@ -1,6 +1,5 @@
 export * from './project'
 export * from './project-edit'
 export * from './project-create'
-export * from './project-delete'
 export * from './projects-list'
 export * from './task'
