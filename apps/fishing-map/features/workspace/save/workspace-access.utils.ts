3bc0eb003ca140d9410929adb62d0995aa4ca183
satellitestudiodesign <juancarlos@satellitestud.io>
2024-06-12 15:26:45 +0200

    add daysFromLatest option to save/edit workspace UI

--- apps/fishing-map/features/workspace/save/workspace-access.utils.ts
+++ apps/fishing-map/features/workspace/save/workspace-access.utils.ts
@@ -7,6 +7,18 @@ import {
 } from '@globalfishingwatch/api-types'
 import { SelectOption } from '@globalfishingwatch/ui-components'
 import { t } from 'features/i18n/i18n'
+import { formatI18nDate } from 'features/i18n/i18nDate'
+import { pickDateFormatByRange } from 'features/map/controls/MapInfo'
+
+export const formatTimerangeBoundary = (
+  boundary: string | undefined,
+  dateFormat: Intl.DateTimeFormatOptions
+) => {
+  if (!boundary) return ''
+  return formatI18nDate(boundary, {
+    format: dateFormat,
+  }).replace(/[.,]/g, '')
+}
 
 export function getViewAccessOptions(): SelectOption<WorkspaceViewAccessType>[] {
   return [
@@ -16,6 +28,23 @@ export function getViewAccessOptions(): SelectOption<WorkspaceViewAccessType>[] 
       label: t('common.anyoneWithThePassword', 'Anyone with the password'),
     },
     { id: WORKSPACE_PRIVATE_ACCESS, label: t('common.onlyMe', 'Only me') },
+  ]
+}
+export function getTimeRangeOptions(start: string, end: string) {
+  const dateFormat = pickDateFormatByRange(start, end)
+  return [
+    {
+      id: 'static',
+      label: t('common.timerangeStatic', {
+        defaultValue: 'Static: from {{start}} to {{end}}',
+        start: formatTimerangeBoundary(start, dateFormat),
+        end: formatTimerangeBoundary(end, dateFormat),
+      }),
+    },
+    {
+      id: 'dynamic',
+      label: t('common.timerangeDynamic', 'Dynamic'),
+    },
   ]
 }
 
