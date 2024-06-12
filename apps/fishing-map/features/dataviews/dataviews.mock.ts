8c1af08295d01236c9b0d024feb0d8095e2e260e
weberjavi <weberjavi@gmail.com>
2024-06-12 11:22:47 +0200

    Create mock dataview

--- apps/fishing-map/features/dataviews/dataviews.mock.ts
+++ apps/fishing-map/features/dataviews/dataviews.mock.ts
@@ -1,7 +1,30 @@
-import { Dataview } from '@globalfishingwatch/api-types'
-// import { GeneratorType, Group } from '@globalfishingwatch/layer-composer'
-// import { TEMPLATE_HEATMAP_STATIC_DATAVIEW_SLUG } from 'data/workspaces'
+import { Dataview, DataviewCategory } from '@globalfishingwatch/api-types'
 
-export const dataviews: Dataview[] = []
+export const dataviews: Dataview[] = [
+  {
+    app: 'fishing-map',
+    category: DataviewCategory.Events, // Assign the correct enum value here
+    config: { type: 'TILE_CLUSTER', color: '#FAE9A0' },
+    createdAt: '2024-05-16T08:21:11.723Z',
+    datasetsConfig: [
+      {
+        datasetId: 'proto-global-encounters-events-30min:v3.0',
+        endpoint: 'events-cluster-tiles',
+        params: [],
+        query: [
+          {
+            id: 'encounter-types',
+            value: ['CARRIER-FISHING', 'FISHING-CARRIER', 'FISHING-SUPPORT', 'SUPPORT-FISHING'],
+          },
+        ],
+      },
+    ],
+    description: 'Encounter cluster events 30min',
+    id: 400,
+    name: 'Encounter cluster events 30min pipe 3',
+    slug: 'encounters-events-30min',
+    updatedAt: '2024-05-16T08:21:11.723Z',
+  },
+]
 
 export default dataviews
