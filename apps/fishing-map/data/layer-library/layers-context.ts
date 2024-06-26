cc16018381ff27b6820af65ed2573605096c3b65
weberjavi <weberjavi@gmail.com>
2024-06-26 13:04:24 +0200

    Add to layer library

--- apps/fishing-map/data/layer-library/layers-context.ts
+++ apps/fishing-map/data/layer-library/layers-context.ts
@@ -3,6 +3,7 @@ import { LibraryLayerConfig } from 'data/layer-library/layers.types'
 import {
   EEZ_DATAVIEW_SLUG,
   FAO_AREAS_DATAVIEW_SLUG,
+  FIXED_SAR_INFRASTRUCTURE,
   GRATICULES_DATAVIEW_SLUG,
   HIGH_SEAS_DATAVIEW_SLUG,
   MPA_DATAVIEW_SLUG,
@@ -65,6 +66,14 @@ export const LAYERS_LIBRARY_CONTEXT: LibraryLayerConfig[] = [
     previewImageUrl: `${PATH_BASENAME}/images/layer-library/high-seas.jpg`,
     config: {
       color: '#4184F4',
+    },
+  },
+  {
+    id: 'offshore-fixed-infrastructure',
+    dataviewId: FIXED_SAR_INFRASTRUCTURE,
+    previewImageUrl: `${PATH_BASENAME}/images/layer-library/offshore-fixed-infrastructure.png`,
+    config: {
+      color: '#8E24A9',
     },
   },
 ]
