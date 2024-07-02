47dc07231d7cb54fda56fc6b2243dc80f42a36de
j8seangel <jseangel@gmail.com>
2024-07-02 16:02:50 +0200

    better out of range disclaimer

--- apps/fishing-map/features/workspace/common/OutOfBoundsDisclaimer.tsx
+++ apps/fishing-map/features/workspace/common/OutOfBoundsDisclaimer.tsx
@@ -1,6 +1,6 @@
 import cx from 'classnames'
 import { useTranslation } from 'react-i18next'
-import { UrlDataviewInstance } from '@globalfishingwatch/dataviews-client'
+import { UrlDataviewInstance, getDatasetsExtent } from '@globalfishingwatch/dataviews-client'
 import { DataviewCategory } from '@globalfishingwatch/api-types'
 import { useTimerangeConnect } from 'features/timebar/timebar.hooks'
 import styles from 'features/workspace/shared/LayerPanel.module.css'
@@ -10,42 +10,79 @@ import {
   getDatasetsInDataviews,
 } from 'features/datasets/datasets.utils'
 
+type OutOfTimerangeDisclaimerValidate = 'start' | 'end' | 'both'
 type OutOfTimerangeDisclaimerProps = {
+  validate?: OutOfTimerangeDisclaimerValidate
   dataview: UrlDataviewInstance
   className?: string
 }
 
-const OutOfTimerangeDisclaimer = ({ dataview, className }: OutOfTimerangeDisclaimerProps) => {
+const OutOfTimerangeDisclaimer = ({
+  dataview,
+  className,
+  validate = 'both',
+}: OutOfTimerangeDisclaimerProps) => {
   const { t } = useTranslation()
   const { start, end } = useTimerangeConnect()
+
   const activeDatasetIds =
     dataview.category === DataviewCategory.Environment ||
     dataview.category === DataviewCategory.Context
       ? getDatasetsInDataviews([dataview])
       : getActiveDatasetsInActivityDataviews([dataview])
 
-  const dataviewDatasets = dataview.datasets?.filter((d) => activeDatasetIds.includes(d.id))
+  const activeDatasets = dataview.datasets?.filter((d) => activeDatasetIds.includes(d.id))
+  const { extentStart, extentEnd } = getDatasetsExtent(activeDatasets, {
+    format: 'isoString',
+  })
 
-  if (dataviewDatasets?.length !== 1) {
-    return null
+  if (validate === 'start') {
+    if (!extentStart) {
+      return null
+    }
+    const datasetInTimerange = end >= extentStart
+    return datasetInTimerange ? null : (
+      <span className={cx(styles.dataWarning, styles.error, className)}>
+        {t('dataset.noDataForTimerange', 'No data for current timerange')}.
+        <br />
+        {t('dataset.extentStart', {
+          start: formatI18nDate(extentStart),
+          defaultValue: 'Dataset start date: {{start}}',
+        })}
+      </span>
+    )
+  } else if (validate === 'end') {
+    if (!extentEnd) {
+      return null
+    }
+    const datasetInTimerange = start <= extentEnd
+    return datasetInTimerange ? null : (
+      <span className={cx(styles.dataWarning, styles.error, className)}>
+        {t('dataset.noDataForTimerange', 'No data for current timerange')}.
+        <br />
+        {t('dataset.extentEnd', {
+          end: formatI18nDate(extentEnd),
+          defaultValue: 'Dataset end date: {{end}}',
+        })}
+      </span>
+    )
   }
 
-  const { startDate, endDate } = dataviewDatasets[0]
-  if (!startDate || !endDate) {
+  if (!extentStart && !extentEnd) {
     return null
   }
 
   const datasetInTimerange =
-    (start >= startDate && start <= endDate) ||
-    (end >= startDate && end <= endDate) ||
-    (start <= startDate && end >= endDate)
+    (start >= extentStart && start <= extentEnd) ||
+    (end >= extentStart && end <= extentEnd) ||
+    (start <= extentStart && end >= extentEnd)
 
   return datasetInTimerange ? null : (
     <span className={cx(styles.dataWarning, styles.error, className)}>
       {t('dataset.noDataForTimerange', 'No data for current timerange')}.
       <br />
       {t('dataset.extent', {
-        extent: [formatI18nDate(startDate), formatI18nDate(endDate)].join(' - '),
+        extent: [formatI18nDate(extentStart), formatI18nDate(extentEnd)].join(' - '),
         defaultValue: 'Dataset extent: {{ extent }}',
       })}
     </span>
