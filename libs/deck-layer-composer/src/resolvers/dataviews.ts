21e7e02c3c126d4314442357929645c4b57b96ed
satellitestudiodesign <juancarlos@satellitestud.io>
2024-07-22 11:47:24 +0200

    reverse dataview instances to match sidebar order

--- libs/deck-layer-composer/src/resolvers/dataviews.ts
+++ libs/deck-layer-composer/src/resolvers/dataviews.ts
@@ -240,7 +240,7 @@ export function getDataviewsSorted(
   dataviews: (UrlDataviewInstance | DataviewInstance)[],
   order = DATAVIEWS_LAYER_ORDER
 ) {
-  return [...(dataviews || [])].sort((a, b) => {
+  return [...(dataviews || [])].reverse().sort((a, b) => {
     const aType = a.config?.type as DataviewType
     const bType = b.config?.type as DataviewType
     const aPos = order.indexOf(aType)
