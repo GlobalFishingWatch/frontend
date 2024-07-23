0f19deeefd372d4bcc14d9e3738440ae8a5cbcf4
j8seangel <jseangel@gmail.com>
2024-07-23 16:02:31 +0200

    show map tooltip content with debounce

--- apps/fishing-map/features/map/popups/MapPopups.tsx
+++ apps/fishing-map/features/map/popups/MapPopups.tsx
@@ -1,14 +1,19 @@
 import { Fragment, useCallback } from 'react'
 import { useSelector } from 'react-redux'
 import { useMapHoverInteraction } from '@globalfishingwatch/deck-layer-composer'
+import { useDebounce } from '@globalfishingwatch/react-hooks'
 import PopupWrapper from 'features/map/popups/PopupWrapper'
 import { selectClickedEvent } from '../map.slice'
 import { useClickedEventConnect } from '../map-interactions.hooks'
 import styles from './Popup.module.css'
 import PopupByCategory from './PopupByCategory'
 
+const DEBOUNCED_TOOLTIP_DELAY = 300
+
 function MapPopups() {
   const hoverInteraction = useMapHoverInteraction()
+  const debouncedHoverLatitude = useDebounce(hoverInteraction.latitude, DEBOUNCED_TOOLTIP_DELAY)
+  const debouncedHoverLongitude = useDebounce(hoverInteraction.longitude, DEBOUNCED_TOOLTIP_DELAY)
   const clickInteraction = useSelector(selectClickedEvent)
   const { dispatchClickedEvent, cancelPendingInteractionRequests } = useClickedEventConnect()
 
@@ -19,17 +24,20 @@ function MapPopups() {
 
   return (
     <Fragment>
-      {hoverInteraction && !clickInteraction?.features?.length && (
-        <PopupWrapper
-          latitude={hoverInteraction.latitude}
-          longitude={hoverInteraction.longitude}
-          className={styles.hover}
-          showArrow={false}
-          showClose={false}
-        >
-          <PopupByCategory interaction={hoverInteraction} type="hover" />
-        </PopupWrapper>
-      )}
+      {hoverInteraction &&
+        !clickInteraction?.features?.length &&
+        debouncedHoverLatitude === hoverInteraction.latitude &&
+        debouncedHoverLongitude === hoverInteraction.longitude && (
+          <PopupWrapper
+            latitude={hoverInteraction.latitude}
+            longitude={hoverInteraction.longitude}
+            className={styles.hover}
+            showArrow={false}
+            showClose={false}
+          >
+            <PopupByCategory interaction={hoverInteraction} type="hover" />
+          </PopupWrapper>
+        )}
       {clickInteraction && clickInteraction?.features?.length && (
         <PopupWrapper
           latitude={clickInteraction.latitude}
