987005fcb4d2aebfa81964490b062a12e3b67e92
satellitestudiodesign <juancarlos@satellitestud.io>
2024-07-31 10:26:39 +0200

    fix flashing environmental layers

--- libs/deck-layers/src/layers/fourwings/heatmap/FourwingsHeatmapTileLayer.ts
+++ libs/deck-layers/src/layers/fourwings/heatmap/FourwingsHeatmapTileLayer.ts
@@ -244,7 +244,7 @@ export class FourwingsHeatmapTileLayer extends CompositeLayer<FourwingsHeatmapTi
 
   updateColorDomain = debounce(() => {
     requestAnimationFrame(() => {
-      const { comparisonMode, aggregationOperation } = this.props
+      const { comparisonMode } = this.props
       const { colorDomain: oldColorDomain } = this.state
       const newColorDomain = this._calculateColorDomain()
       let avgChange = Infinity
@@ -267,10 +267,7 @@ export class FourwingsHeatmapTileLayer extends CompositeLayer<FourwingsHeatmapTi
               )
             }
           )
-        } else if (
-          comparisonMode === FourwingsComparisonMode.Compare &&
-          aggregationOperation === FourwingsAggregationOperation.Sum
-        ) {
+        } else if (comparisonMode === FourwingsComparisonMode.Compare) {
           change = (oldColorDomain as number[]).map((oldValue, i) => {
             const newValue = newColorDomain[i] as number
             return (Math.abs(newValue - oldValue) / oldValue) * 100 || 0
