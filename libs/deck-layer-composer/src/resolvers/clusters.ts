dd8d202a9a4bae60be7e2a9147d7a6b36a3eefaf
j8seangel <jseangel@gmail.com>
2024-08-28 17:19:31 +0200

    add support for fourwings cluster filters

--- libs/deck-layer-composer/src/resolvers/clusters.ts
+++ libs/deck-layer-composer/src/resolvers/clusters.ts
@@ -1,8 +1,37 @@
-import { UrlDataviewInstance } from '@globalfishingwatch/dataviews-client'
+import { uniqBy } from 'es-toolkit'
+import { DateTime } from 'luxon'
+import {
+  getDataviewSqlFiltersResolved,
+  UrlDataviewInstance,
+} from '@globalfishingwatch/dataviews-client'
 import { FourwingsClustersLayerProps, getUTCDateTime } from '@globalfishingwatch/deck-layers'
 import { getDatasetsExtent, resolveEndpoint } from '@globalfishingwatch/datasets-client'
-import { EndpointId } from '@globalfishingwatch/api-types'
+import { DataviewDatasetConfig, EndpointId } from '@globalfishingwatch/api-types'
 import { DeckResolverFunction, ResolverGlobalConfig } from './types'
+
+const getDateRangeQuery = ({
+  startTime,
+  endTime,
+  extentStart,
+  extentEnd,
+}: {
+  startTime: number
+  endTime: number
+  extentStart?: number
+  extentEnd?: number
+}) => {
+  const start = extentStart && extentStart > startTime ? extentStart : startTime
+  const end =
+    extentEnd && extentEnd < endTime
+      ? DateTime.fromMillis(extentEnd).plus({ day: 1 }).toMillis()
+      : endTime
+  const startIso = getUTCDateTime(start < end ? start : end)
+    .startOf('hour')
+    .toISO()
+  const endIso = getUTCDateTime(end).startOf('hour').toISO()
+
+  return `${startIso},${endIso}`
+}
 
 export const resolveDeckFourwingsClustersLayerProps: DeckResolverFunction<
   FourwingsClustersLayerProps
@@ -12,21 +41,46 @@ export const resolveDeckFourwingsClustersLayerProps: DeckResolverFunction<
 ): FourwingsClustersLayerProps => {
   const startTime = start ? getUTCDateTime(start).toMillis() : 0
   const endTime = end ? getUTCDateTime(end).toMillis() : Infinity
-  const { extentStart, extentEnd } = getDatasetsExtent(dataview.datasets, { format: 'timestamp' })
+  const { extentStart, extentEnd } = getDatasetsExtent<number>(dataview.datasets, {
+    format: 'timestamp',
+  })
 
   const dataset = dataview.datasets?.[0]
+  const dataviewDatasetConfig = dataview.datasetsConfig?.[0] || ({} as DataviewDatasetConfig)
+  const datasetId = dataviewDatasetConfig.datasetId || dataset?.id
 
-  const tilesUrl = dataset
-    ? resolveEndpoint(
-        dataset,
+  if (!dataset || !datasetId) {
+    console.warn('No datasetId found for dataview', dataview)
+    return {} as FourwingsClustersLayerProps
+  }
+
+  const datasetConfig = {
+    datasetId: datasetId,
+    endpoint: dataviewDatasetConfig.endpoint || EndpointId.ClusterTiles,
+    params: uniqBy(
+      [...(dataviewDatasetConfig.params || []), { id: 'type', value: 'heatmap' }],
+      (p) => p.id
+    ),
+    query: uniqBy(
+      [
+        ...(dataviewDatasetConfig.query || []),
+        { id: 'format', value: '4WINGS' },
+        { id: 'temporal-aggregation', value: true },
+        { id: 'datasets', value: datasetId },
+        { id: 'filters', value: getDataviewSqlFiltersResolved(dataview) },
         {
-          datasetId: dataset.id,
-          endpoint: EndpointId.ClusterTiles,
-          params: [{ id: 'type', value: 'heatmap' }],
+          id: 'date-range',
+          value: getDateRangeQuery({
+            startTime,
+            endTime,
+            extentStart,
+            extentEnd,
+          }),
         },
-        { absolute: true }
-      )
-    : undefined
+      ],
+      (p) => p.id
+    ),
+  } as DataviewDatasetConfig
 
   return {
     id: dataview.id,
@@ -34,12 +88,9 @@ export const resolveDeckFourwingsClustersLayerProps: DeckResolverFunction<
     subcategory: dataview.config?.type!,
     datasetId: dataset?.id || '',
     color: dataview.config?.color || '',
-    filters: dataview.config?.filter || '',
     startTime,
     endTime,
     visible: dataview.config?.visible ?? true,
-    tilesUrl: tilesUrl || '',
-    extentStart: extentStart as number,
-    extentEnd: extentEnd as number,
+    tilesUrl: resolveEndpoint(dataset, datasetConfig, { absolute: true }) || '',
   }
 }
