c84ac2c0369c0cf5f17aeacb9f42dcb03945160a
j8seangel <jseangel@gmail.com>
2024-10-23 09:34:46 +0200

    use vessel group 403 error

--- apps/fishing-map/features/reports/vessel-groups/VesselGroupReportError.tsx
+++ apps/fishing-map/features/reports/vessel-groups/VesselGroupReportError.tsx
@@ -23,6 +23,15 @@ function VesselGroupReportError({ vesselGroupId }: { vesselGroupId: string }) {
     )
   }
 
+  if (reportError?.status === 403) {
+    return (
+      <WorkspaceLoginError
+        title={t('errors.privateVesselGroupReport', 'This is a private vessel group report')}
+        emailSubject={`Requesting access for ${vesselGroupId} vessel group report`}
+      />
+    )
+  }
+
   if (reportError?.status === 401) {
     return (
       <div className={styles.emptyState}>
@@ -31,18 +40,6 @@ function VesselGroupReportError({ vesselGroupId }: { vesselGroupId: string }) {
           <LocalStorageLoginLink className={styles.loginLink}>log in</LocalStorageLoginLink> again.
         </Trans>
       </div>
-    )
-  }
-  const isPrivateError =
-    // reportError?.status === 403 &&
-    reportError?.status === 422 &&
-    reportError?.message.includes('you do not have permissions to read it')
-  if (isPrivateError) {
-    return (
-      <WorkspaceLoginError
-        title={t('errors.privateVesselGroupReport', 'This is a private vessel group report')}
-        emailSubject={`Requesting access for ${vesselGroupId} vessel group report`}
-      />
     )
   }
 
