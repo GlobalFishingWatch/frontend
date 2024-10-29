db60bbdc72fd065d8b889a8521107f7456c8807b
j8seangel <jseangel@gmail.com>
2024-10-29 11:50:29 +0100

    fix fishing-map build

--- apps/image-labeler/src/routeTree.gen.ts
+++ apps/image-labeler/src/routeTree.gen.ts
@@ -22,11 +22,13 @@ const IndexLazyImport = createFileRoute('/')()
 // Create/Update Routes
 
 const IndexLazyRoute = IndexLazyImport.update({
+  id: '/',
   path: '/',
   getParentRoute: () => rootRoute,
 } as any).lazy(() => import('./routes/index.lazy').then((d) => d.Route))
 
 const ProjectProjectIdRoute = ProjectProjectIdImport.update({
+  id: '/project/$projectId',
   path: '/project/$projectId',
   getParentRoute: () => rootRoute,
 } as any).lazy(() =>
@@ -56,10 +58,44 @@ declare module '@tanstack/react-router' {
 
 // Create and export the route tree
 
-export const routeTree = rootRoute.addChildren({
-  IndexLazyRoute,
-  ProjectProjectIdRoute,
-})
+export interface FileRoutesByFullPath {
+  '/': typeof IndexLazyRoute
+  '/project/$projectId': typeof ProjectProjectIdRoute
+}
+
+export interface FileRoutesByTo {
+  '/': typeof IndexLazyRoute
+  '/project/$projectId': typeof ProjectProjectIdRoute
+}
+
+export interface FileRoutesById {
+  __root__: typeof rootRoute
+  '/': typeof IndexLazyRoute
+  '/project/$projectId': typeof ProjectProjectIdRoute
+}
+
+export interface FileRouteTypes {
+  fileRoutesByFullPath: FileRoutesByFullPath
+  fullPaths: '/' | '/project/$projectId'
+  fileRoutesByTo: FileRoutesByTo
+  to: '/' | '/project/$projectId'
+  id: '__root__' | '/' | '/project/$projectId'
+  fileRoutesById: FileRoutesById
+}
+
+export interface RootRouteChildren {
+  IndexLazyRoute: typeof IndexLazyRoute
+  ProjectProjectIdRoute: typeof ProjectProjectIdRoute
+}
+
+const rootRouteChildren: RootRouteChildren = {
+  IndexLazyRoute: IndexLazyRoute,
+  ProjectProjectIdRoute: ProjectProjectIdRoute,
+}
+
+export const routeTree = rootRoute
+  ._addFileChildren(rootRouteChildren)
+  ._addFileTypes<FileRouteTypes>()
 
 /* prettier-ignore-end */
 
