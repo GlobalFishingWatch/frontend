fa2083f642a8d7062c758121036e9020b36f66cb
j8seangel <jseangel@gmail.com>
2024-10-31 15:39:06 +0100

    remove maplibre dependencies in dataviews and fourwings-aggregate libs

--- libs/features-aggregate/src/timeseries.ts
+++ libs/features-aggregate/src/timeseries.ts
@@ -1,6 +1,5 @@
 import { DateTime } from 'luxon'
 import { uniqBy } from 'es-toolkit'
-import { GeoJSONFeature } from '@globalfishingwatch/maplibre-gl'
 import {
   AggregationOperation,
   getTimeSeries,
@@ -13,6 +12,26 @@ import {
   Interval,
   quantizeOffsetToDate,
 } from '@globalfishingwatch/layer-composer'
+
+// Copied from below to avoid importing the dependency
+// import type { GeoJSONFeature } from '@globalfishingwatch/maplibre-gl'
+export declare class GeoJSONFeature<P = Record<string, any>> {
+  type: 'Feature'
+  _geometry: GeoJSON.Geometry
+  properties: P
+  id: number | string | undefined
+  _vectorTileFeature: any
+  constructor(
+    vectorTileFeature: any,
+    z: number,
+    x: number,
+    y: number,
+    id: string | number | undefined
+  )
+  get geometry(): GeoJSON.Geometry
+  set geometry(g: GeoJSON.Geometry)
+  toJSON(): any
+}
 
 export type TilesSourceState = {
   loaded: boolean
