7fcd61ff8745b3116280e1133ca8de16a48800d3
j8seangel <jseangel@gmail.com>
2024-11-04 15:55:45 +0100

    fix deck-layers build

--- libs/deck-layers/src/layers/draw/draw.types.ts
+++ libs/deck-layers/src/layers/draw/draw.types.ts
@@ -1,5 +1,6 @@
 import { Feature, Polygon, MultiPolygon, Point } from 'geojson'
 import { PickingInfo } from '@deck.gl/core'
+import { FeatureWithProps } from '@deck.gl-community/editable-layers'
 import { DeckPickingObject } from '../../types'
 
 export type DrawFeatureProperties = {
@@ -10,3 +11,24 @@ export type DrawFeature = Feature<Polygon | MultiPolygon | Point, DrawFeaturePro
 
 export type DrawPickingObject = DeckPickingObject<DrawFeature & DrawFeatureProperties>
 export type DrawPickingInfo = PickingInfo<DrawPickingObject>
+
+// Copied from 👇 to fix build
+// import { EditHandleType } from '@deck.gl-community/editable-layers/dist/edit-modes/types'
+export type EditHandleType =
+  | 'existing'
+  | 'intermediate'
+  | 'snap-source'
+  | 'snap-target'
+  | 'scale'
+  | 'rotate'
+
+export type EditHandleFeature = FeatureWithProps<
+  Point,
+  {
+    guideType: 'editHandle'
+    editHandleType: EditHandleType
+    featureIndex: number
+    positionIndexes?: number[]
+    shape?: string
+  }
+>
