4ef0826d4518fac04e082b5c9b9ced2b8df39c63
j8seangel <jseangel@gmail.com>
2024-11-08 17:35:55 +0100

    fix missing param clean on close

--- apps/fishing-map/features/reports/ports/ports-report.config.ts
+++ apps/fishing-map/features/reports/ports/ports-report.config.ts
@@ -3,7 +3,9 @@ import { PortsReportState } from './ports-report.types'
 
 export const DEFAULT_PORT_REPORT_DATASET_ID = 'public-global-port-visits-events:v3.1'
 
-export const DEFAULT_PORT_REPORT_STATE: PortsReportState = {
+export const DEFAULT_PORT_REPORT_STATE: Required<PortsReportState> = {
+  portsReportName: '',
+  portsReportCountry: '',
   portsReportDatasetId: DEFAULT_PORT_REPORT_DATASET_ID,
   portsReportVesselsProperty: 'flag',
   portsReportVesselsPage: 0,
