f5307193f96b7c621a93d40838b974dfa8258559
weberjavi <weberjavi@gmail.com>
2024-11-27 12:05:50 +0100

    Update tracks when hovering over the sidebar

--- apps/track-labeler/src/features/sidebar/Sidebar.tsx
+++ apps/track-labeler/src/features/sidebar/Sidebar.tsx
@@ -17,7 +17,11 @@ import {
 } from '../../features/tracks/tracks.selectors'
 import { useViewport } from '../../features/map/map.hooks'
 import { useMapboxInstance } from '../../features/map/map.context'
-import { disableHighlightedEvent, setHighlightedEvent } from '../../features/timebar/timebar.slice'
+import {
+  disableHighlightedEvent,
+  setHighlightedEvent,
+  setHighlightedTime,
+} from '../../features/timebar/timebar.slice'
 import { getActionShortcuts } from '../../features/projects/projects.selectors'
 import { ActionType } from '../../types'
 import { findPreviousTimestamp, isFiniteBbox } from '../../utils/shared'
@@ -139,6 +143,7 @@ const Sidebar: React.FC = (props): React.ReactElement => {
         const start = new Date(findPreviousTimestamp(timestamps, selection.start))
         const end = new Date(selection.end)
         dispatch(setHighlightedEvent({ start: start.toISOString(), end: end.toISOString() }))
+        dispatch(setHighlightedTime({ start: start.toISOString(), end: end.toISOString() }))
       }
     },
     [dispatch, timestamps]
