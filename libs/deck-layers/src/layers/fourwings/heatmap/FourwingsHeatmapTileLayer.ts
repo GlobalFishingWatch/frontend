7e2e447aaa6c4167f4d4dcae332a4154829c9807
satellitestudiodesign <juancarlos@satellitestud.io>
2024-11-28 18:39:46 +0100

    activate worker

--- libs/deck-layers/src/layers/fourwings/heatmap/FourwingsHeatmapTileLayer.ts
+++ libs/deck-layers/src/layers/fourwings/heatmap/FourwingsHeatmapTileLayer.ts
@@ -558,7 +558,7 @@ export class FourwingsHeatmapTileLayer extends CompositeLayer<FourwingsHeatmapTi
     })
 
     const data = await parse(arrayBuffers.filter(Boolean) as ArrayBuffer[], FourwingsLoader, {
-      worker: false,
+      worker: true,
       fourwings: {
         sublayers: 1,
         cols,
