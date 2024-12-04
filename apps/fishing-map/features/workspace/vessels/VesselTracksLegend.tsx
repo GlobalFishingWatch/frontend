216022ded04112b6aabbfdef009ce92a45fc58ef
j8seangel <jseangel@gmail.com>
2024-12-04 20:55:08 +0100

    add missing translation

--- apps/fishing-map/features/workspace/vessels/VesselTracksLegend.tsx
+++ apps/fishing-map/features/workspace/vessels/VesselTracksLegend.tsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux'
+import { useTranslation } from 'react-i18next'
 import { LegendType, MapLegend } from '@globalfishingwatch/ui-components'
 import {
   VESSEL_GRAPH_COLORS,
@@ -12,6 +13,7 @@ const SPEEDS = [0, ...VESSEL_SPEED_VALUES.slice(0, -1)]
 const DEPTHS = [0, ...VESSEL_DEPTH_VALUES.slice(0, -1)]
 
 function VesselTracksLegend(): React.ReactElement | null {
+  const { t } = useTranslation()
   const vesselsTimebarGraph = useSelector(selectTimebarGraph)
 
   if (!vesselsTimebarGraph || vesselsTimebarGraph === 'none') {
@@ -21,7 +23,8 @@ function VesselTracksLegend(): React.ReactElement | null {
   const legend = {
     id: vesselsTimebarGraph,
     type: LegendType.ColorRampDiscrete,
-    unit: vesselsTimebarGraph === 'speed' ? 'knots' : 'meters',
+    unit:
+      vesselsTimebarGraph === 'speed' ? t('common.knots', 'knots') : t('common.meters', 'meters'),
     values: vesselsTimebarGraph === 'speed' ? SPEEDS : DEPTHS,
     colors:
       vesselsTimebarGraph === 'speed' ? VESSEL_GRAPH_COLORS : VESSEL_GRAPH_COLORS.slice().reverse(),
