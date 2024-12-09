116735e6c75b7e788392a61e2110fa3d82322344
j8seangel <jseangel@gmail.com>
2024-12-09 18:28:42 +0100

    support tag filters remove

--- apps/fishing-map/features/workspace/shared/DatasetFlagsField.tsx
+++ apps/fishing-map/features/workspace/shared/DatasetFlagsField.tsx
@@ -1,10 +1,12 @@
 import { useTranslation } from 'react-i18next'
+import type { TagItem } from '@globalfishingwatch/ui-components'
 import { TagList } from '@globalfishingwatch/ui-components'
 import type { UrlDataviewInstance } from '@globalfishingwatch/dataviews-client'
 import { EXCLUDE_FILTER_ID } from '@globalfishingwatch/api-types'
 import { getFlagsByIds } from 'utils/flags'
 import styles from 'features/workspace/shared/LayerPanel.module.css'
 import { getSchemaFilterOperationInDataview } from 'features/datasets/datasets.utils'
+import { useDataviewInstancesConnect } from '../workspace.hook'
 
 type DatasetFlagFieldProps = {
   dataview: UrlDataviewInstance
@@ -13,8 +15,22 @@ type DatasetFlagFieldProps = {
 
 function DatasetFlagField({ dataview, showWhenEmpty = false }: DatasetFlagFieldProps) {
   const { t } = useTranslation()
+  const { upsertDataviewInstance } = useDataviewInstancesConnect()
   const filterOperation = getSchemaFilterOperationInDataview(dataview, 'flag')
   let fishingFiltersOptions = getFlagsByIds(dataview.config?.filters?.flag || [])
+
+  const onRemoveClick = (tag: TagItem, tags: TagItem[]) => {
+    console.log(tags)
+    upsertDataviewInstance({
+      id: dataview.id,
+      config: {
+        filters: {
+          ...(dataview.config?.filters || {}),
+          ['flag']: tags.length ? tags.map((t) => t.id) : undefined,
+        },
+      },
+    })
+  }
 
   if (!fishingFiltersOptions?.length) {
     if (showWhenEmpty) {
@@ -37,6 +53,7 @@ function DatasetFlagField({ dataview, showWhenEmpty = false }: DatasetFlagFieldP
         tags={fishingFiltersOptions}
         color={dataview.config?.color}
         className={styles.tagList}
+        onRemove={onRemoveClick}
       />
     </div>
   )
