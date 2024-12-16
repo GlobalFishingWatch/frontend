016bd23ece13278c1c2e4c6fce035ba41dd7f985
j8seangel <jseangel@gmail.com>
2024-12-16 16:35:27 +0100

    move MapScreenshot hook to shared lib

--- apps/fishing-map/features/map/MapScreenshot.tsx
+++ apps/fishing-map/features/map/MapScreenshot.tsx
@@ -1,26 +1,14 @@
-import { Fragment, memo, useEffect, useLayoutEffect, useRef, useState } from 'react'
+/* eslint-disable @next/next/no-img-element */
+import { Fragment, memo, useEffect, useState } from 'react'
 import { createPortal } from 'react-dom'
 // import { getParser } from 'bowser'
 import type { Deck } from '@deck.gl/core'
-import { getCSSVarValue } from 'utils/dom'
+import { usePrintSize } from '@globalfishingwatch/react-hooks'
 import { useDeckMap } from 'features/map/map-context.hooks'
 import styles from './Map.module.css'
 import { MAP_WRAPPER_ID } from './map.config'
 
-type PrintSize = {
-  px: number
-  in: string
-}
-
-// TODO: review why it isn't always render propertly, disabled for now
-export const isPrintSupported = false
 export const MAP_IMAGE_DEBOUNCE = 800
-// const browser = getParser(window.navigator.userAgent)
-// export const isPrintSupported = browser.satisfies({
-//   chrome: '>22',
-//   opera: '>30',
-//   edge: '>79',
-// })
 
 const getMapImage = (map: Deck): Promise<string> => {
   return new Promise((resolve, reject) => {
@@ -42,24 +30,7 @@ const getMapImage = (map: Deck): Promise<string> => {
 function MapScreenshot() {
   const map = useDeckMap()
   const [screenshotImage, setScreenshotImage] = useState<string | null>(null)
-  const printSize = useRef<{ width: PrintSize; height: PrintSize } | undefined>()
-
-  useLayoutEffect(() => {
-    const pixelPerInch = window.devicePixelRatio * 96
-    const baseSize = parseInt(getCSSVarValue('--base-font-size')) || 10
-    const timebarSize = parseFloat(getCSSVarValue('--timebar-size') || '7.2') * baseSize
-    const height = window.innerHeight - timebarSize
-    printSize.current = {
-      width: {
-        px: window.innerWidth,
-        in: `${window.innerWidth / pixelPerInch}in`,
-      },
-      height: {
-        in: `${height / pixelPerInch}in`,
-        px: height,
-      },
-    }
-  }, [])
+  const printSize = usePrintSize()
 
   useEffect(() => {
     if (map) {
@@ -76,20 +47,18 @@ function MapScreenshot() {
   // insert the image just below the canvas
   const canvasDomElement = document.getElementById(MAP_WRAPPER_ID)
   if (!canvasDomElement) return null
-  const size = isPrintSupported
-    ? `${printSize.current?.width.in} ${printSize.current?.height.in}`
-    : 'landscape'
+
   return createPortal(
     <Fragment>
       <img
-        id="screenshot-img"
+        id="map-screenshot-img"
         className={styles.screenshot}
         src={screenshotImage}
         alt="map screenshot"
       />
       <style>
         {`@page {
-          size: ${size};
+          size: ${printSize};
           margin: 0;
         }`}
       </style>
---
+++ libs/react-hooks/src/use-print-size/use-print-size.ts
@@ -0,0 +1,52 @@
+import { useState, useLayoutEffect } from 'react'
+
+export const getCSSVarValue = (property: string) => {
+  if (typeof window !== 'undefined') {
+    return window.getComputedStyle(document.body).getPropertyValue(property)
+  }
+  return ''
+}
+
+// TODO: review why it isn't always render propertly, disabled for now
+export const isPrintSupported = false
+// const browser = getParser(window.navigator.userAgent)
+// export const isPrintSupported = browser.satisfies({
+//   chrome: '>22',
+//   opera: '>30',
+//   edge: '>79',
+// })
+
+export type PrintSizeUnit = {
+  px: number
+  in: string
+}
+export type PrintSize = { width: PrintSizeUnit; height: PrintSizeUnit }
+
+export function getPrintSize(printSize: PrintSize | undefined) {
+  const size =
+    printSize && isPrintSupported ? `${printSize?.width.in} ${printSize.height?.in}` : 'landscape'
+  return size
+}
+
+export function usePrintSize() {
+  const [printSize, setPrintSize] = useState<PrintSize | undefined>()
+
+  useLayoutEffect(() => {
+    const pixelPerInch = window.devicePixelRatio * 96
+    const baseSize = parseInt(getCSSVarValue('--base-font-size')) || 10
+    const timebarSize = parseFloat(getCSSVarValue('--timebar-size') || '7.2') * baseSize
+    const height = window.innerHeight - timebarSize
+    setPrintSize({
+      width: {
+        px: window.innerWidth,
+        in: `${window.innerWidth / pixelPerInch}in`,
+      },
+      height: {
+        in: `${height / pixelPerInch}in`,
+        px: height,
+      },
+    })
+  }, [])
+
+  return getPrintSize(printSize)
+}
