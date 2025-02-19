04afcf8b6873909b820ed25c7385894a02e75c5e
j8seangel <jseangel@gmail.com>
2025-02-19 16:35:12 +0100

    fix missing query params in global report link

--- apps/fishing-map/features/workspace/shared/GlobalReportLink.tsx
+++ apps/fishing-map/features/workspace/shared/GlobalReportLink.tsx
@@ -5,13 +5,17 @@ import Link from 'redux-first-router-link'
 import { IconButton } from '@globalfishingwatch/ui-components'
 
 import { DEFAULT_WORKSPACE_ID } from 'data/workspaces'
+import { useFitAreaInViewport } from 'features/reports/report-area/area-reports.hooks'
 import type { ReportCategory } from 'features/reports/reports.types'
 import { selectWorkspace } from 'features/workspace/workspace.selectors'
 import { WORKSPACE_REPORT } from 'routes/routes'
+import { selectLocationQuery } from 'routes/routes.selectors'
 
 const GlobalReportLink = ({ reportCategory }: { reportCategory: ReportCategory }) => {
   const { t } = useTranslation()
   const workspace = useSelector(selectWorkspace)
+  const query = useSelector(selectLocationQuery)
+  const fitAreaInViewport = useFitAreaInViewport()
 
   const reportLinkTo = {
     type: WORKSPACE_REPORT,
@@ -20,6 +24,7 @@ const GlobalReportLink = ({ reportCategory }: { reportCategory: ReportCategory }
       workspaceId: workspace?.id || DEFAULT_WORKSPACE_ID,
     },
     query: {
+      ...query,
       reportCategory,
       latitude: 0,
       longitude: 0,
@@ -27,7 +32,7 @@ const GlobalReportLink = ({ reportCategory }: { reportCategory: ReportCategory }
     },
   }
   return (
-    <Link to={reportLinkTo}>
+    <Link to={reportLinkTo} onClick={fitAreaInViewport}>
       <IconButton
         icon="analysis"
         type="border"
