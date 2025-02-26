6934b001c90408fed41bca21bc7d646b4c771936
j8seangel <jseangel@gmail.com>
2025-02-26 15:41:54 +0100

    add environmental stats placeholder

---
+++ apps/fishing-map/features/reports/shared/placeholders/ReportStatsPlaceholder.tsx
@@ -0,0 +1,22 @@
+import cx from 'classnames'
+
+import styles from './placeholders.module.css'
+
+export default function ReportStatsPlaceholder() {
+  return (
+    <div className={styles.paragraph}>
+      <div className={styles.sentence}>
+        <div style={{ width: '20rem' }} className={cx(styles.block, styles.animate, styles.M)} />
+        <div style={{ width: '5rem' }} className={cx(styles.block, styles.animate, styles.M)} />
+        <div style={{ width: '20rem' }} className={cx(styles.block, styles.animate, styles.M)} />
+        <div style={{ width: '5rem' }} className={cx(styles.block, styles.animate, styles.M)} />
+        <div style={{ width: '10rem' }} className={cx(styles.block, styles.animate, styles.M)} />
+        <div style={{ width: '15rem' }} className={cx(styles.block, styles.animate, styles.M)} />
+      </div>
+      <div className={styles.sentence}>
+        <div style={{ width: '10rem' }} className={cx(styles.block, styles.animate, styles.M)} />
+        <div style={{ width: '5rem' }} className={cx(styles.block, styles.animate, styles.M)} />
+      </div>
+    </div>
+  )
+}
--- apps/fishing-map/features/reports/tabs/environment/ReportEnvironment.tsx
+++ apps/fishing-map/features/reports/tabs/environment/ReportEnvironment.tsx
@@ -13,6 +13,7 @@ import { formatI18nDate } from 'features/i18n/i18nDate'
 import { formatI18nNumber } from 'features/i18n/i18nNumber'
 import { getDatasetNameTranslated } from 'features/i18n/utils.datasets'
 import ReportActivityPlaceholder from 'features/reports/shared/placeholders/ReportActivityPlaceholder'
+import ReportStatsPlaceholder from 'features/reports/shared/placeholders/ReportStatsPlaceholder'
 import ReportActivityEvolution from 'features/reports/tabs/activity/ReportActivityEvolution'
 import {
   useComputeReportTimeSeries,
@@ -81,7 +82,9 @@ function ReportEnvironment() {
                 />
               )
             ) : null}
-            {!isLoading && min && mean && max && (
+            {isLoading ? (
+              <ReportStatsPlaceholder />
+            ) : min && mean && max ? (
               <p className={cx(styles.disclaimer, { [styles.marginTop]: isDynamic })}>
                 {isDynamic
                   ? t('analysis.statsDisclaimerDynamic', {
@@ -108,7 +111,7 @@ function ReportEnvironment() {
                   </span>
                 )}
               </p>
-            )}
+            ) : null}
           </div>
         )
       })}
