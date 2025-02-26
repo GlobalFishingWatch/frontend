6e60495705aef28dba43e3cdd19135b5141d09f4
j8seangel <jseangel@gmail.com>
2025-02-26 15:07:42 +0100

    migrate layers to deck 9.1 UBO

--- libs/deck-layers/src/layers/vessel/VesselTrackLayer.ts
+++ libs/deck-layers/src/layers/vessel/VesselTrackLayer.ts
@@ -24,7 +24,7 @@ export const COLOR_BY: Record<VesselsColorByProperty, VesselsColorByValue> = {
   elevation: 3,
 }
 
-/** Properties added by VesselTrackLayer. */
+/** Properties added by track. */
 export type _VesselTrackLayerProps<DataT = any> = {
   id: string
   /**
@@ -114,9 +114,9 @@ function generateShaderColorSteps({
   return [...Array(stepsNum)]
     .map((_, index) => {
       if (index === stepsNum - 1) {
-        return `{ color = color${index}; }`
+        return `{ color = track.color${index}; }`
       }
-      return `if (${property} ${operation} value${index}) { color = color${index}; }`
+      return `if (${property} ${operation} track.value${index}) { color = track.color${index}; }`
     })
     .join(' else ')
 }
@@ -145,11 +145,81 @@ const defaultProps: DefaultProps<VesselTrackLayerProps> = {
   trackUrl: { type: 'accessor', value: '' },
 }
 
-/** All properties supported by VesselTrackLayer. */
+/** All properties supported by track. */
 export type VesselTrackLayerProps<DataT = any> = _VesselTrackLayerProps<DataT> &
   PathLayerProps<DataT>
 
-/** Render paths that represent vessel trips. */
+const uniformBlock = `
+  uniform trackUniforms {
+    uniform float startTime;
+    uniform float endTime;
+    uniform float highlightStartTime;
+    uniform float highlightEndTime;
+    uniform float minSpeedFilter;
+    uniform float maxSpeedFilter;
+    uniform float minElevationFilter;
+    uniform float maxElevationFilter;
+    uniform float value0;
+    uniform float value1;
+    uniform float value2;
+    uniform float value3;
+    uniform float value4;
+    uniform float value5;
+    uniform float value6;
+    uniform float value7;
+    uniform float value8;
+    uniform float value9;
+    uniform vec4 color0;
+    uniform vec4 color1;
+    uniform vec4 color2;
+    uniform vec4 color3;
+    uniform vec4 color4;
+    uniform vec4 color5;
+    uniform vec4 color6;
+    uniform vec4 color7;
+    uniform vec4 color8;
+    uniform vec4 color9;
+    uniform float colorBy;
+  } track;
+`
+
+const trackLayerUniforms = {
+  name: 'track',
+  vs: uniformBlock,
+  fs: uniformBlock,
+  uniformTypes: {
+    startTime: 'f32',
+    endTime: 'f32',
+    highlightStartTime: 'f32',
+    highlightEndTime: 'f32',
+    minSpeedFilter: 'f32',
+    maxSpeedFilter: 'f32',
+    minElevationFilter: 'f32',
+    maxElevationFilter: 'f32',
+    value0: 'f32',
+    value1: 'f32',
+    value2: 'f32',
+    value3: 'f32',
+    value4: 'f32',
+    value5: 'f32',
+    value6: 'f32',
+    value7: 'f32',
+    value8: 'f32',
+    value9: 'f32',
+    color0: 'vec4<f32>',
+    color1: 'vec4<f32>',
+    color2: 'vec4<f32>',
+    color3: 'vec4<f32>',
+    color4: 'vec4<f32>',
+    color5: 'vec4<f32>',
+    color6: 'vec4<f32>',
+    color7: 'vec4<f32>',
+    color8: 'vec4<f32>',
+    color9: 'vec4<f32>',
+    colorBy: 'f32',
+  },
+}
+
 export class VesselTrackLayer<DataT = any, ExtraProps = Record<string, unknown>> extends PathLayer<
   DataT,
   VesselTrackLayerProps & ExtraProps
@@ -159,11 +229,9 @@ export class VesselTrackLayer<DataT = any, ExtraProps = Record<string, unknown>>
 
   getShaders() {
     const shaders = super.getShaders()
+    shaders.modules = [...(shaders.modules || []), trackLayerUniforms]
     shaders.inject = {
       'vs:#decl': `
-        uniform float highlightStartTime;
-        uniform float highlightEndTime;
-
         in float instanceTimestamps;
         in float instanceSpeeds;
         in float instanceElevations;
@@ -175,72 +243,43 @@ export class VesselTrackLayer<DataT = any, ExtraProps = Record<string, unknown>>
         vTime = instanceTimestamps;
         vSpeed = instanceSpeeds;
         vElevation = instanceElevations;
-        if(vTime > highlightStartTime && vTime < highlightEndTime) {
+        if(vTime > track.highlightStartTime && vTime < track.highlightEndTime) {
           gl_Position.z = 1.0;
         }
       `,
       'fs:#decl': `
-        uniform float startTime;
-        uniform float endTime;
-        uniform float highlightStartTime;
-        uniform float highlightEndTime;
-        uniform float minSpeedFilter;
-        uniform float maxSpeedFilter;
-        uniform float minElevationFilter;
-        uniform float maxElevationFilter;
-        uniform float value0;
-        uniform float value1;
-        uniform float value2;
-        uniform float value3;
-        uniform float value4;
-        uniform float value5;
-        uniform float value6;
-        uniform float value7;
-        uniform float value8;
-        uniform float value9;
-        uniform vec4 color0;
-        uniform vec4 color1;
-        uniform vec4 color2;
-        uniform vec4 color3;
-        uniform vec4 color4;
-        uniform vec4 color5;
-        uniform vec4 color6;
-        uniform vec4 color7;
-        uniform vec4 color8;
-        uniform vec4 color9;
-        uniform int colorBy;
         in float vTime;
         in float vSpeed;
         in float vElevation;
       `,
       // Drop the segments outside of the time window
       'fs:#main-start': `
-        if (vTime < startTime || vTime > endTime) {
+        if (vTime < track.startTime || vTime > track.endTime) {
           discard;
         }
       `,
       'fs:DECKGL_FILTER_COLOR': `
-        if(colorBy == ${COLOR_BY.speed}) {
+        if(track.colorBy == ${COLOR_BY.speed}.0) {
           ${generateShaderColorSteps({
             property: 'vSpeed',
             operation: '<=',
           })}
-        } else if(colorBy == ${COLOR_BY.elevation}){
+        } else if(track.colorBy == ${COLOR_BY.elevation}.0){
           ${generateShaderColorSteps({
             property: 'vElevation',
             operation: '>=',
           })}
         }
 
-        if (vSpeed < minSpeedFilter ||
-            vSpeed > maxSpeedFilter ||
-            vElevation < minElevationFilter ||
-            vElevation > maxElevationFilter)
+        if (vSpeed < track.minSpeedFilter ||
+            vSpeed > track.maxSpeedFilter ||
+            vElevation < track.minElevationFilter ||
+            vElevation > track.maxElevationFilter)
         {
           color.a = 0.25;
         }
 
-        if (vTime > highlightStartTime && vTime < highlightEndTime) {
+        if (vTime > track.highlightStartTime && vTime < track.highlightEndTime) {
           color = vec4(${DEFAULT_HIGHLIGHT_COLOR_VEC.join(',')});
         }
       `,
@@ -315,20 +354,24 @@ export class VesselTrackLayer<DataT = any, ExtraProps = Record<string, unknown>>
       {} as Record<string, number[]>
     )
 
-    params.uniforms = {
-      ...params.uniforms,
-      startTime,
-      endTime,
-      highlightStartTime,
-      highlightEndTime,
-      minSpeedFilter,
-      maxSpeedFilter,
-      minElevationFilter,
-      maxElevationFilter,
-      colorBy: colorBy ? COLOR_BY[colorBy] : COLOR_BY.track,
-      ...values,
-      ...colors,
+    if (this.state.model) {
+      this.state.model.shaderInputs.setProps({
+        track: {
+          startTime,
+          endTime,
+          highlightStartTime,
+          highlightEndTime,
+          minSpeedFilter,
+          maxSpeedFilter,
+          minElevationFilter,
+          maxElevationFilter,
+          colorBy: colorBy ? COLOR_BY[colorBy] : COLOR_BY.track,
+          ...values,
+          ...colors,
+        },
+      })
     }
+
     super.draw(params)
   }
 
