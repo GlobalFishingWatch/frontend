a31a0187faee10c8232badada62c1c3950893d28
j8seangel <jseangel@gmail.com>
2025-03-10 11:34:51 +0100

    remove track dataview mock

--- apps/fishing-map/features/dataviews/dataviews.mock.ts
+++ apps/fishing-map/features/dataviews/dataviews.mock.ts
@@ -1,49 +1,6 @@
 import type { Dataview } from '@globalfishingwatch/api-types'
 import { DataviewCategory } from '@globalfishingwatch/api-types'
 
-const dataviews: Dataview[] = [
-  {
-    id: 1111111111,
-    name: 'Vessel track only',
-    slug: 'vessel-track-only',
-    app: 'fishing-map',
-    config: {
-      type: 'TRACK',
-      color: '#FAE9A0',
-    },
-    datasetsConfig: [
-      {
-        query: [
-          {
-            id: 'binary',
-            value: true,
-          },
-          {
-            id: 'wrap-longitudes',
-            value: false,
-          },
-          {
-            id: 'fields',
-            value: ['LONLAT', 'TIMESTAMP', 'SPEED', 'ELEVATION'],
-          },
-          {
-            id: 'format',
-            value: 'DECKGL',
-          },
-        ],
-        params: [
-          {
-            id: 'vesselId',
-            value: '',
-          },
-        ],
-        endpoint: 'tracks',
-        datasetId: '',
-      },
-    ],
-    description: 'Vessel track only',
-    category: 'vessels',
-  },
-]
+const dataviews: Dataview[] = []
 
 export default dataviews
