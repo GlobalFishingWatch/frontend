d4b4d02675444f8f7056c9fa81dde3b571016bed
j8seangel <jseangel@gmail.com>
2025-04-09 19:44:07 +0200

    enable context layer for CVP report

--- apps/fishing-map/data/highlighted-workspaces/report.dataviews.ts
+++ apps/fishing-map/data/highlighted-workspaces/report.dataviews.ts
@@ -1,12 +1,13 @@
-import { DataviewInstance } from '@globalfishingwatch/api-types'
+import { uniq } from 'es-toolkit'
+
 import type { UrlDataviewInstance } from '@globalfishingwatch/dataviews-client'
 
+import { EEZ_DATAVIEW_INSTANCE_ID, RFMO_DATAVIEW_INSTANCE_ID } from 'data/workspaces'
 import {
   ENCOUNTER_EVENTS_SOURCE_ID,
   LOITERING_EVENTS_SOURCE_ID,
   PORT_VISITS_EVENTS_SOURCE_ID,
 } from 'features/dataviews/dataviews.utils'
-import { getDefaultWorkspace } from 'features/workspace/workspace.slice'
 
 export const AIS_DATAVIEW_INSTANCE_ID = 'ais'
 export const VMS_DATAVIEW_INSTANCE_ID = 'vms'
@@ -25,13 +26,25 @@ const REPORT_EVENTS_DATAVIEW_INSTANCES_IDS: string[] = [
   PORT_VISITS_EVENTS_SOURCE_ID,
 ]
 
-export const CARRIER_PORTAL_DATAVIEW_INSTANCES: UrlDataviewInstance[] =
-  REPORT_EVENTS_DATAVIEW_INSTANCES_IDS.map((id) => ({
-    id,
-    config: {
-      visible: id === ENCOUNTER_EVENTS_SOURCE_ID,
-    },
-  }))
+const CONTEXT_LAYERS_DATAVIEW_INSTANCES_IDS: string[] = [
+  EEZ_DATAVIEW_INSTANCE_ID,
+  RFMO_DATAVIEW_INSTANCE_ID,
+]
+
+const CARRIER_PORTAL_DATAVIEW_INSTANCES_IDS: string[] = [
+  ENCOUNTER_EVENTS_SOURCE_ID,
+  ...CONTEXT_LAYERS_DATAVIEW_INSTANCES_IDS,
+]
+
+export const CARRIER_PORTAL_DATAVIEW_INSTANCES: UrlDataviewInstance[] = uniq([
+  ...REPORT_EVENTS_DATAVIEW_INSTANCES_IDS,
+  ...CONTEXT_LAYERS_DATAVIEW_INSTANCES_IDS,
+]).map((id) => ({
+  id,
+  config: {
+    visible: CARRIER_PORTAL_DATAVIEW_INSTANCES_IDS.includes(id),
+  },
+}))
 
 export const REPORT_EVENTS_DATAVIEW_INSTANCES: UrlDataviewInstance[] =
   REPORT_EVENTS_DATAVIEW_INSTANCES_IDS.map((id) => ({
