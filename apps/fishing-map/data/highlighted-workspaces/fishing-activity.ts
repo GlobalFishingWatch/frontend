f75f185f2dcb3f4e7c9a33d32afffe8f8856db0b
j8seangel <jseangel@gmail.com>
2025-05-08 18:33:49 +0200

    rollback missing legacy CVP link

--- apps/fishing-map/data/highlighted-workspaces/fishing-activity.ts
+++ apps/fishing-map/data/highlighted-workspaces/fishing-activity.ts
@@ -1,5 +1,5 @@
 import { IS_PRODUCTION_WORKSPACE_ENV, PATH_BASENAME } from 'data/config'
-import { DEEP_SEA_MINING_WORKSPACE_ID } from 'data/workspaces'
+import { DEEP_SEA_MINING_WORKSPACE_ID, DEFAULT_WORKSPACE_ID } from 'data/workspaces'
 
 import type workspaceTranslations from '../../public/locales/source/workspaces.json'
 
@@ -11,10 +11,16 @@ export type FishingMapWorkspace = {
   reportId?: string
 }
 
+export const LEGACY_CVP_WORKSPACE_ID = 'carrier-portal-public'
+
 export const FISHING_MAP_WORKSPACES: FishingMapWorkspace[] = [
   {
-    id: 'default-public',
+    id: DEFAULT_WORKSPACE_ID,
     img: `${PATH_BASENAME}/images/highlighted-workspaces/fishing-activity.jpg`,
+  },
+  {
+    id: LEGACY_CVP_WORKSPACE_ID,
+    img: `${PATH_BASENAME}/images/highlighted-workspaces/carrier-portal.jpg`,
   },
   {
     id: DEEP_SEA_MINING_WORKSPACE_ID,
