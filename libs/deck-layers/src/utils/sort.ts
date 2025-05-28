1e8ff6bf1ac572616718f73c63c1db12eea74e22
satellitestudiodesign <juancarlos@satellitestud.io>
2025-05-28 17:53:51 +0200

    remove unused LayerGroups

--- libs/deck-layers/src/utils/sort.ts
+++ libs/deck-layers/src/utils/sort.ts
@@ -4,26 +4,21 @@
 export enum LayerGroup {
   Background = 'background', // Solid bg color
   Basemap = 'basemap', // Satellite tiles
+  OutlinePolygonsBackground = 'outlinePolygonsBackground', // Polygons  that need to be rendered below landmass
   Bathymetry = 'bathymetry', // 4Wings Bathymetry layer
   HeatmapStatic = 'heatmapStatic', // Fill/gradient-based heatmaps
   Heatmap = 'heatmap', // Fill/gradient-based temporal heatmaps
   HeatmapFootprint = 'heatmapFootprint', // Fill/gradient-based temporal heatmaps
   OutlinePolygons = 'outlinePolygons', // Context layers with an outlined/hollow style such as RFMOs, MPAs, etc
-  OutlinePolygonsFill = 'outlinePolygonsFill', // User context layers with a filled styles, below OutlinePolygons
   BasemapFill = 'basemapFill', // Landmass
-  OutlinePolygonsBackground = 'outlinePolygonsBackground', // Polygons  that need to be rendered below landmass
-  OutlinePolygonsHighlighted = 'outlinePolygonsHighlighted', // Context layers with selected features
-  CustomLayer = 'customLayer', // Context custom user layers
   Default = 'default', // Default stack position when group is not specified
+  CustomLayer = 'customLayer', // Context custom user layers
   Track = 'track', // Tracks
   Point = 'point', // Events, etc
-  TrackHighlightedEvent = 'trackHighlightedEvent', // Fixed highlight section normally used for a event duration
-  TrackHighlighted = 'trackHighlighted', // Highlighted sections of tracks
-  BasemapForeground = 'basemapForeground', // Graticule labels, bathymetry labels, etc
+  OutlinePolygonsHighlighted = 'outlinePolygonsHighlighted', // Context layers with selected features
+  Label = 'label', // All non-basemap layers labels
   Cluster = 'cluster', // Cluster circles
   Tool = 'tool', // Tools such as rulers, etc
-  Label = 'label', // All non-basemap layers labels
-  ClusterLabel = 'clusterLabel', // Cluster labels
   Overlay = 'overlay', // Popups, ruler tool, etc
 }
 
@@ -35,20 +30,15 @@ export const LAYER_GROUP_ORDER = [
   LayerGroup.HeatmapStatic,
   LayerGroup.Heatmap,
   LayerGroup.HeatmapFootprint,
-  LayerGroup.OutlinePolygonsFill,
   LayerGroup.OutlinePolygons,
   LayerGroup.BasemapFill,
   LayerGroup.Default,
-  LayerGroup.BasemapForeground,
   LayerGroup.CustomLayer,
   LayerGroup.Track,
-  LayerGroup.TrackHighlightedEvent,
-  LayerGroup.TrackHighlighted,
   LayerGroup.Point,
   LayerGroup.OutlinePolygonsHighlighted,
   LayerGroup.Label,
   LayerGroup.Cluster,
-  LayerGroup.ClusterLabel,
   LayerGroup.Tool,
   LayerGroup.Overlay,
 ]
