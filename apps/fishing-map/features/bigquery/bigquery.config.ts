1026afbd4c773a75f11c52e7bb659b39a601821c
j8seangel <jseangel@gmail.com>
2025-06-12 08:02:49 +0200

    fix outdated bq hint

--- apps/fishing-map/features/bigquery/bigquery.config.ts
+++ apps/fishing-map/features/bigquery/bigquery.config.ts
@@ -21,8 +21,8 @@ export const VisualisationOptions: {
     id: 'events',
     label: t('bigQuery.visualisationEvents', 'Events (clusters)'),
     fieldsHint: t(
-      'bigQuery.visualisationEventsHint',
-      '(Ensure event_id, event_start, event_end and geom are all present)'
+      'bigQuery.visualisationActivityHint',
+      '(Ensure id, lat, lon, timestamp and value are all present)'
     ),
   },
 ]
