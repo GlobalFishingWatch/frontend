bf8cf3f51bd83ba9d8721d4014d9a570141348da
j8seangel <jseangel@gmail.com>
2025-06-13 13:36:24 +0200

    include comments in getAll issues endpoint

--- apps/fishing-map/pages/api/track-corrections/[workspaceId]/_issues/get-all.ts
+++ apps/fishing-map/pages/api/track-corrections/[workspaceId]/_issues/get-all.ts
@@ -1,11 +1,19 @@
-import type { TrackCorrection } from 'features/track-correction/track-correction.slice'
+import type {
+  TrackCorrection,
+  TrackCorrectionComment,
+} from 'features/track-correction/track-correction.slice'
 import { loadSpreadsheetDocByWorkspace } from 'pages/api/_utils/spreadsheets'
 import {
+  COMMENTS_SPREADSHEET_TITLE,
   ISSUES_SPREADSHEET_TITLE,
+  parseIssueComment,
   parseIssueRow,
 } from 'pages/api/track-corrections/[workspaceId]/_issues/utils'
 
-export async function getWorkspaceIssues(workspaceId: string) {
+export async function getWorkspaceIssues(
+  workspaceId: string,
+  { includeComments = true }: { includeComments?: boolean } = {}
+) {
   const spreadsheetDoc = await loadSpreadsheetDocByWorkspace(workspaceId)
   const sheet = spreadsheetDoc.sheetsByTitle[ISSUES_SPREADSHEET_TITLE]
 
@@ -17,6 +25,23 @@ export async function getWorkspaceIssues(workspaceId: string) {
 
   const rows = await sheet.getRows()
   const issues: TrackCorrection[] = rows.map(parseIssueRow)
+
+  if (includeComments) {
+    const commentsSheet = spreadsheetDoc.sheetsByTitle[COMMENTS_SPREADSHEET_TITLE]
+    if (!commentsSheet) {
+      throw new Error(
+        `Spreadsheet tab (${COMMENTS_SPREADSHEET_TITLE}) not found in spreadsheet ${workspaceId}`
+      )
+    }
+
+    const commentsRows = await commentsSheet.getRows<TrackCorrectionComment>()
+    const allComments = commentsRows.map(parseIssueComment)
+
+    return issues.map((issue) => {
+      const comments = allComments.filter((comment) => comment.issueId === issue.issueId)
+      return { ...issue, comments }
+    })
+  }
 
   return issues
 }
