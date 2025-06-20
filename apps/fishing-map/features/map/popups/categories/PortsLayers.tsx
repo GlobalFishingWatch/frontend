644af0bb6fcc86df9fbfba18bd77097a352e31d7
satellitestudiodesign <juancarlos@satellitestud.io>
2025-06-20 15:04:10 +0200

    add to layer library

--- apps/fishing-map/features/map/popups/categories/PortsLayers.tsx
+++ apps/fishing-map/features/map/popups/categories/PortsLayers.tsx
@@ -1,14 +1,10 @@
 import { Fragment } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useSelector } from 'react-redux'
 import { groupBy } from 'es-toolkit'
 
-import type { UrlDataviewInstance } from '@globalfishingwatch/dataviews-client'
 import type { ContextPickingObject, UserLayerPickingObject } from '@globalfishingwatch/deck-layers'
 import { Icon, IconButton } from '@globalfishingwatch/ui-components'
 
-import { getDatasetLabel } from 'features/datasets/datasets.utils'
-import { selectCustomUserDataviews } from 'features/dataviews/selectors/dataviews.categories.selectors'
 import type { ExtendedFeatureByVesselEventPort } from 'features/map/map.slice'
 import PortsReportLink from 'features/reports/report-port/PortsReportLink'
 import { formatInfoField } from 'utils/info'
@@ -22,20 +18,18 @@ type PortsLayersProps = {
 
 function PortsTooltipSection({ features, showFeaturesDetails = false }: PortsLayersProps) {
   const { t } = useTranslation()
-  const dataviews = useSelector(selectCustomUserDataviews) as UrlDataviewInstance[]
   const featuresByType = groupBy(features, (f) => f.layerId)
   return (
     <Fragment>
       {Object.values(featuresByType).map((featureByType, index) => {
-        const { color, datasetId, title } = featureByType[0]
-        const dataview = dataviews.find((d) => d.id === title)
-        const dataset = dataview?.datasets?.find((d) => d.id === datasetId)
-        const rowTitle = dataset ? getDatasetLabel(dataset) : title
+        const { color } = featureByType[0]
         return (
           <div key={`${featureByType[0].title}-${index}`} className={styles.popupSection}>
             <Icon icon="dots" className={styles.layerIcon} style={{ color }} />
             <div className={styles.popupSectionContent}>
-              {showFeaturesDetails && <h3 className={styles.popupSectionTitle}>{rowTitle}</h3>}
+              {showFeaturesDetails && (
+                <h3 className={styles.popupSectionTitle}>{t('event.port_other', 'Ports')}</h3>
+              )}
               {featureByType.map((feature) => {
                 const { port_id, name, flag } = feature.properties
                 const port: ExtendedFeatureByVesselEventPort = {
