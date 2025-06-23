657681dee820a6a86d22a65fc6e7b5fa489440cd
satellitestudiodesign <juancarlos@satellitestud.io>
2025-06-23 10:44:39 +0200

    fix importing auto-reload

--- apps/fishing-map/features/datasets/datasets.hook.ts
+++ apps/fishing-map/features/datasets/datasets.hook.ts
@@ -190,12 +190,19 @@ export const useAutoRefreshImportingDataset = (
 ) => {
   const { dispatchFetchDataset } = useDatasetsAPI()
   useEffect(() => {
-    let timeOut: any
-    if (dataset && dataset.status === DatasetStatus.Importing) {
-      timeOut = setTimeout(() => {
-        dispatchFetchDataset(dataset.id)
-      }, refreshTimeout)
+    let timeOut: NodeJS.Timeout | undefined
+
+    const refreshDataset = async () => {
+      const result = await dispatchFetchDataset(dataset!.id)
+      if (result.payload?.status === DatasetStatus.Importing) {
+        timeOut = setTimeout(refreshDataset, refreshTimeout)
+      }
     }
+
+    if (dataset?.status === DatasetStatus.Importing) {
+      timeOut = setTimeout(refreshDataset, refreshTimeout)
+    }
+
     return () => {
       if (timeOut) {
         clearTimeout(timeOut)
