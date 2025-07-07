f13cd9bf36fa963012b508c4d3924925e522580a
GeorgiaDesigns <gdmespindola@gmail.com>
2025-07-07 10:21:39 +0200

    fix comment list spacing

--- apps/fishing-map/features/track-correction/TrackCommentsList.tsx
+++ apps/fishing-map/features/track-correction/TrackCommentsList.tsx
@@ -18,22 +18,20 @@ const TrackCommentsList = ({ track }: TrackCommentsListProps) => {
   if (!track.comments || !track.comments.length) return
 
   return (
-    <div className={styles.container}>
-      <ul>
-        {track.comments.map((comment) => (
-          <li key={comment.issueId + '-' + comment.date} className={styles.item}>
-            <div className={styles.header}>
-              <span className={styles.user}>{comment.user}</span>
-              <span className={styles.version}>{getTimeAgo(getUTCDateTime(comment.date), t)}</span>
-            </div>
-            <div className="comment-content">{comment.comment}</div>
-            <span className={styles.version}>
-              {t('trackCorrection.version', 'Version') + ' ' + comment.datasetVersion}
-            </span>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className={styles.container}>
+      {track.comments.map((comment) => (
+        <li key={comment.issueId + '-' + comment.date} className={styles.item}>
+          <div className={styles.header}>
+            <span className={styles.user}>{comment.user}</span>
+            <span className={styles.version}>{getTimeAgo(getUTCDateTime(comment.date), t)}</span>
+          </div>
+          <div className="comment-content">{comment.comment}</div>
+          <span className={styles.version}>
+            {t('trackCorrection.version', 'Version') + ' ' + comment.datasetVersion}
+          </span>
+        </li>
+      ))}
+    </ul>
   )
 }
 
