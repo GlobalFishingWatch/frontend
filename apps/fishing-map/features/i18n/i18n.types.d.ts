58c03630e1960c7daa400d7c739155de978b2a2c
j8seangel <jseangel@gmail.com>
2025-07-24 10:27:39 +0100

    report others app integration

--- apps/fishing-map/features/i18n/i18n.types.d.ts
+++ apps/fishing-map/features/i18n/i18n.types.d.ts
@@ -483,6 +483,7 @@ interface Resources {
       "onScreen": "on screen",
       "optional": "Optional",
       "or": "or",
+      "others": "common.others",
       "partnerSites": "Partner sites",
       "password": "password",
       "permissions": "permissions required",
