46a5359e85764b4a489af50446fa50d0ff10708d
GeorgiaDesigns <gdmespindola@gmail.com>
2025-07-25 14:09:48 -0300

    logos and title

--- apps/vessel-info-tool/src/routes/__root.tsx
+++ apps/vessel-info-tool/src/routes/__root.tsx
@@ -20,8 +20,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       ...seo({
-        title: 'TanStack Start | Type-Safe, Client-First, Full-Stack React Framework',
-        description: `TanStack Start is a type-safe, client-first, full-stack React framework. `,
+        title: 'Interoperability tool | Vessel info tool',
       }),
     ],
     links: [
