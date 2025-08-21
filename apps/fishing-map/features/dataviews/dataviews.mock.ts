e4fd7a05c3ba18870e901267c10c3c9992bfeda0
j8seangel <jseangel@gmail.com>
2025-08-21 15:22:48 +0100

    random forest dataviews

--- apps/fishing-map/features/dataviews/dataviews.mock.ts
+++ apps/fishing-map/features/dataviews/dataviews.mock.ts
@@ -1,6 +1,9 @@
 import type { Dataview } from '@globalfishingwatch/api-types'
 import { DataviewCategory, DataviewType } from '@globalfishingwatch/api-types'
 
+export const RANDOM_FOREST_FISHING_EFFORT_DATAVIEW_ID = 431
+export const RANDOM_FOREST_PRESENCE_DATAVIEW_ID = 432
+
 const dataviews: Dataview[] = []
 
 export default dataviews
