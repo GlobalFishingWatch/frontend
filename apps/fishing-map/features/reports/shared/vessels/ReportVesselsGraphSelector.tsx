f33246d5631240ed468d137e416cf1efa51e63f5
j8seangel <jseangel@gmail.com>
2025-09-17 17:59:24 +0200

    fix experimental coverage tooltip styles

--- apps/fishing-map/features/reports/shared/vessels/ReportVesselsGraphSelector.tsx
+++ apps/fishing-map/features/reports/shared/vessels/ReportVesselsGraphSelector.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
+import cx from 'classnames'
 
 import type { ChoiceOption } from '@globalfishingwatch/ui-components'
 import { Choice } from '@globalfishingwatch/ui-components'
@@ -93,7 +94,7 @@ function VesselGroupReportVesselsGraphSelector() {
                     }
                     terminologyKey="insightsCoverage"
                     tooltip={t('common.experimental')}
-                    className={styles.dataTerminology}
+                    className={cx(styles.dataTerminology, styles.experimental)}
                   />
                 )}
               </span>
