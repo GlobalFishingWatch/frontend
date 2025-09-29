f5e5dad31698020c3b13a6631e4f37ec5cb810e4
j8seangel <jseangel@gmail.com>
2025-09-29 15:38:35 +0200

    fix build

--- apps/fishing-map/utils/ports.ts
+++ apps/fishing-map/utils/ports.ts
@@ -1,22 +1,11 @@
 import memoizeOne from 'memoize-one'
 
+import ports from 'data/ports'
 import i18n, { t } from 'features/i18n/i18n'
 import { formatInfoField } from 'utils/info'
 
 type Port = { id: string; label: string }
 type PortData = { id: string; name: string; flag: string }
-
-let _portsData: PortData[] = []
-
-const getPortsData = (): PortData[] => {
-  if (_portsData.length === 0) {
-    const portsModule = (() => {
-      return require('data/ports')
-    })()
-    _portsData = portsModule.default || portsModule
-  }
-  return _portsData
-}
 
 const parsePort = (port: PortData, lng = i18n.language): Port => {
   const flag = t(`flags:${port.flag}`, { lng, defaultValue: port.flag }) as string
@@ -27,13 +16,12 @@ const parsePort = (port: PortData, lng = i18n.language): Port => {
 }
 
 export const getPortById = (id: string, lng = i18n.language): Port | undefined => {
-  const port = getPortsData().find((f) => f.id === id)
+  const port = ports.find((f) => f.id === id)
   if (!port || !lng) return undefined
   return parsePort(port, lng)
 }
 
 export const getPortsByIds = (ids: string[], lng = i18n.language): Port[] => {
-  const ports = getPortsData()
   return ids.flatMap((id) => {
     const port = ports.find((f) => f.id === id)
     if (!port || !lng) return []
@@ -42,5 +30,5 @@ export const getPortsByIds = (ids: string[], lng = i18n.language): Port[] => {
 }
 
 export const getPorts = memoizeOne((lng = i18n.language): Port[] => {
-  return getPortsData().map((port) => parsePort(port, lng))
+  return ports.map((port) => parsePort(port, lng))
 })
