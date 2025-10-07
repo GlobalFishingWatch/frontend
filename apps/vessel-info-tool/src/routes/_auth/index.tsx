dc8f9d6aeb725a28a305d1592bf53752079f8000
j8seangel <jseangel@gmail.com>
2025-10-07 14:48:25 -0300

    wip

--- apps/vessel-info-tool/src/routes/_auth/index.tsx
+++ apps/vessel-info-tool/src/routes/_auth/index.tsx
@@ -1,8 +1,22 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute, Link, redirect } from '@tanstack/react-router'
 
+import { getTokens } from '@/server/auth'
 import { UserPermissionValues } from '@/utils/source'
 
 export const Route = createFileRoute('/_auth/')({
+  beforeLoad: async ({ search }) => {
+    const user = await getTokens()
+
+    if (!user) {
+      throw redirect({
+        to: '/login',
+        search: { redirect: location.href },
+      })
+    }
+
+    // Pass user to child routes
+    return { user }
+  },
   component: VesselsIndexComponent,
 })
 
