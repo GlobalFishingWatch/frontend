24fc7972bb52afa87258ff72eb6a45883332773b
j8seangel <jseangel@gmail.com>
2025-11-10 17:19:15 +0100

    remove year from timebar and tooltips

--- apps/fishing-map/features/map/controls/TimeRangeDates.tsx
+++ apps/fishing-map/features/map/controls/TimeRangeDates.tsx
@@ -16,14 +16,17 @@ export default function TimeRangeDates({
   start,
   end,
   format = pickDateFormatByRange(start, end),
+  showUTCLabel,
 }: {
   start: string
   end: string
   format?: DateTimeFormatOptions
+  showUTCLabel?: boolean
 }) {
   return (
     <Fragment>
-      <I18nDate date={start} format={format} /> - <I18nDate date={end} format={format} />
+      <I18nDate date={start} format={format} showUTCLabel={showUTCLabel} /> -{' '}
+      <I18nDate date={end} format={format} showUTCLabel={showUTCLabel} />
     </Fragment>
   )
 }
