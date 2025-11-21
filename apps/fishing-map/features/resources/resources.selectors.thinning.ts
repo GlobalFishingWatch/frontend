9e2792989a9714a717400f319e1f6f40406c0c4b
j8seangel <jseangel@gmail.com>
2025-11-21 12:30:51 +0100

    remove thinning when calculating gaps

--- apps/fishing-map/features/resources/resources.selectors.thinning.ts
+++ apps/fishing-map/features/resources/resources.selectors.thinning.ts
@@ -15,8 +15,10 @@ const TRACK_THINNING_BY_ZOOM_USER = { ...TRACK_THINNING_BY_ZOOM_GUEST, 7: Thinni
 
 export const selectTrackThinningConfig = createSelector(
   [selectIsGuestUser, selectDebugOptions, selectIsTurningTidesWorkspace],
-  (guestUser, { thinning }, isTurningTidesWorkspace) => {
-    if (!thinning || isTurningTidesWorkspace) return { 0: ThinningLevels.None }
+  (guestUser, { thinning, vesselsMaxTimeGapHours }, isTurningTidesWorkspace) => {
+    if (!thinning || isTurningTidesWorkspace || vesselsMaxTimeGapHours) {
+      return { 0: ThinningLevels.None }
+    }
     return guestUser ? TRACK_THINNING_BY_ZOOM_GUEST : TRACK_THINNING_BY_ZOOM_USER
   }
 )
