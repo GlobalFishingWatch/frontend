dc6b3cb1d380b7cf9577eded01eeff6a158fb475
j8seangel <jseangel@gmail.com>
2025-12-03 17:23:51 +0100

    new datasets structure

---
+++ libs/api-types/src/datasets.filters.ts
@@ -0,0 +1,33 @@
+export type FilterType = 'fourwings' | 'events' | 'tracks' | 'vessels'
+
+export type DatasetFilterType =
+  | 'boolean'
+  | 'coordinate'
+  | 'number'
+  | 'range'
+  | 'sql'
+  | 'string'
+  | 'timestamp'
+
+export type DatasetFilterFormat = 'date-time' | 'latitude' | 'longitude'
+
+export type DatasetFilterEnum = (string | number | boolean)[]
+export type DatasetFilterOperation = 'gt' | 'lt' | 'gte' | 'lte'
+export type DatasetFilter = {
+  label: string
+  id: string
+  type: DatasetFilterType
+  required?: boolean
+  enabled?: boolean
+  array?: boolean
+  enum?: DatasetFilterEnum
+  format?: DatasetFilterFormat
+  maxLength?: number
+  minLength?: number
+  min?: number
+  max?: number
+  singleSelection?: boolean
+  operation?: DatasetFilterOperation
+}
+
+export type DatasetFilters = Partial<Record<FilterType, DatasetFilter[]>>
--- libs/api-types/src/datasets.ts
+++ libs/api-types/src/datasets.ts
@@ -1,63 +1,13 @@
+import type { DatasetConfiguration, DatasetTypeToConfigurationType } from './datasets.configuration'
+import type { DatasetFilters } from './datasets.filters'
+import type { Endpoint } from './endpoints'
 import type { EventTypes } from './events'
 
 export const DRAW_DATASET_SOURCE = 'drawn_on_gfw_map'
 
-export interface UploadResponse {
+export type UploadResponse = {
   path: string
   url: string
-}
-
-export type EndpointParamType =
-  | 'enum'
-  | 'boolean'
-  | 'number'
-  | 'string'
-  | 'sql'
-  | 'date-iso' // legacy from v2 replaced by type: string, format: 'date-iso'
-  | '4wings-datasets' // legacy from v2 replaced by type: string, array: true
-
-export interface EndpointParam {
-  id: string
-  label: string
-  type: EndpointParamType
-  enum?: string[]
-  array?: boolean
-  required?: boolean
-  description?: string
-  default?: string | boolean | number
-}
-
-export enum EndpointId {
-  ContextTiles = 'context-tiles',
-  ContextFeature = 'context-feature',
-  ClusterTiles = 'events-cluster-tiles',
-  ClusterTilesInteraction = 'events-cluster-interaction',
-  ContextGeojson = 'temporal-context-geojson',
-  Events = 'events',
-  EventsDetail = 'events-detail',
-  FourwingsBreaks = '4wings-bins',
-  FourwingsInteraction = '4wings-interaction',
-  FourwingsLegend = '4wings-legend',
-  FourwingsTiles = '4wings-tiles',
-  Tracks = 'tracks',
-  Thumbnails = 'thumbnails',
-  PMTiles = 'pm-tiles',
-  UserTracks = 'user-tracks-data',
-  Vessel = 'vessel',
-  VesselAdvancedSearch = 'advanced-search-vessels',
-  VesselList = 'list-vessels',
-  VesselSearch = 'search-vessels',
-}
-
-export interface Endpoint {
-  id: EndpointId
-  description?: string
-  method?: 'GET' | 'POST'
-  pathTemplate: string
-  downloadable: boolean
-  body?: any
-  params: EndpointParam[]
-  query: EndpointParam[]
 }
 
 export enum DatasetTypes {
@@ -66,30 +16,22 @@ export enum DatasetTypes {
   Events = 'events:v1',
   Fourwings = '4wings:v1',
   PMTiles = 'pm-tiles:v1',
-  Ports = 'ports:v1',
   TemporalContext = 'temporal-context-layer:v1',
   Thumbnails = 'thumbnails:v1',
   Tracks = 'tracks:v1',
   UserContext = 'user-context-layer:v1',
   UserTracks = 'user-tracks:v1',
-  VesselGroups = 'vessel-group:v1',
   Vessels = 'vessels:v1',
 }
 
 export enum DatasetStatus {
-  Error = 'error',
-  Done = 'done',
   Deleted = 'deleted',
+  Done = 'done',
+  Error = 'error',
   Importing = 'importing',
 }
 
-export type DatasetGeometryType = 'polygons' | 'tracks' | 'points' | 'draw'
-export type DatasetGeometryToGeoJSONGeometry = {
-  [Property in DatasetGeometryType]: string[]
-}
-export type TimeFilterType = 'date' | 'dateRange'
-
-export interface DatasetDocumentation {
+export type DatasetDocumentation = {
   type?: string
   enable?: boolean
   status?: 'Active' | 'Deprecated'
@@ -97,184 +39,56 @@ export interface DatasetDocumentation {
   provider?: string
 }
 
-export type DatasetConfigurationSourceFormat = 'GeoJSON' | 'Shapefile' | 'CSV' | 'KML'
-export interface DatasetConfigurationUI {
-  latitude?: string
-  longitude?: string
-  timestamp?: string
-  sourceFormat?: DatasetConfigurationSourceFormat
-  includeWithoutEndDate?: boolean // used in infrastructure layer to render points that we consider are still there because don't have end date
-  pointName?: string
-  pointSize?: string
-  maxPointSize?: number
-  minPointSize?: number
-  startTime?: string | number
-  endTime?: string | number
-  timeFilterType?: TimeFilterType
-  polygonColor?: string
-  /**
-   * Feature properties array to inform the API
-   * which data is to be be added to tiles features
-   */
-  valueProperties?: string[]
-  geometryType?: DatasetGeometryType
-  lineId?: string | number
-  segmentId?: string | number
-}
-
 export type DatasetConfigurationInterval = 'YEAR' | 'MONTH' | 'DAY' | 'HOUR'
-
-interface DatasetBackendConfig {
-  table?: string
-  dataset?: string
-  project?: string
-  version?: number
-  tileScale?: number
-  tileOffset?: number
-  timestamp?: string
-  translate?: boolean | null
-  numBytes?: number | null
-  indexBoost?: number | null
-  insightSources?: string[]
-  interactionColumns?: string[]
-  interactionGroupColumns?: string[]
-  ttl?: number | null
-  band?: string | null
-  images?: string | null
-  source?: string | null
-  tableName?: string
-  filePath?: string
-  srid?: number
-  file?: string
-}
-
-export interface DatasetConfiguration extends DatasetBackendConfig {
-  id?: string
-  index?: string
-  type?: EventTypes
-  source?: string
-  function?: 'AVG' | 'SUM'
-  aggregationOperation?: 'avg' | 'sum'
-  geometryType?: DatasetGeometryType
-  format?: 'geojson'
-  documentation?: DatasetDocumentation
-  fields?: string[]
-  idProperty?: string
-  /**
-   * Feature properties array to inform the API
-   * which data is to be be added to tiles features
-   */
-  valueProperties?: string[]
-  propertyToInclude?: string
-  maxZoom?: number
-  min?: number
-  max?: number
-  intervals?: DatasetConfigurationInterval[]
-  disableInteraction?: boolean
-  apiSupportedVersions?: ('v1' | 'v2' | 'v3')[]
-  configurationUI?: DatasetConfigurationUI
-  scale?: number
-}
-
-export interface EnviromentalDatasetConfiguration extends DatasetConfiguration {
-  min: number
-  max: number
-  offset: number
-  scale: number
-  propertyToInclude: string
-  propertyToIncludeRange: { min: number; max: number }
-  maxZoom?: number
-}
-
-export type AnyDatasetConfiguration = DatasetConfiguration | EnviromentalDatasetConfiguration
 
 export type RelatedDataset = {
   id: string
   type: DatasetTypes
 }
 
-export type DatasetSchemaType =
-  | 'range'
-  | 'number'
-  | 'string'
-  | 'boolean'
-  | 'array'
-  | 'coordinate'
-  | 'timestamp'
-
-export type DatasetSchemaFormat = 'date-time' | 'latitude' | 'longitude'
-
-export type DatasetSchemaItemEnum = (string | number | boolean)[]
-export type DatasetSchemaItemOperation = 'gt' | 'lt' | 'gte' | 'lte'
-export type DatasetSchemaItem = {
-  type: DatasetSchemaType
-  enum?: DatasetSchemaItemEnum
-  format?: DatasetSchemaFormat
-  maxLength?: number
-  minLength?: number
-  min?: number
-  max?: number
-  stats?: boolean
-  unit?: string
-  singleSelection?: boolean
-  operation?: DatasetSchemaItemOperation
-  items?: { type: DatasetSchemaType; enum: string[] }
-  properties?: Record<string, DatasetSchemaItem>
-}
-
-export type DatasetSchema = {
-  type: DatasetSchemaType
-  maxLength: number
-  minLength: number
-  enum: string[]
-  min: number
-  max: number
-  stats?: boolean
-  unit?: string
-  singleSelection?: boolean
-  items?: Record<string, DatasetSchemaItem>
-}
-
 export enum DatasetCategory {
-  Event = 'event',
-  Detections = 'detections',
-  Context = 'context',
-  Environment = 'environment',
   Activity = 'activity',
+  Context = 'context',
+  Detections = 'detections',
+  Environment = 'environment',
+  Event = 'event',
   Vessel = 'vessel',
   VesselGroups = 'vessel groups',
 }
 
 export enum DatasetSubCategory {
-  Info = 'info',
-  Track = 'track',
   Fishing = 'fishing',
+  Info = 'info',
   Presence = 'presence',
-  Viirs = 'viirs',
   Sar = 'sar',
   Sentinel2 = 'sentinel-2',
+  Track = 'track',
+  Viirs = 'viirs',
 }
 
-export interface DatasetFile {
+export type DatasetFile = {
   name: string
   path: string
   size: number | string
   lastUpdate: string
 }
 
-export interface Dataset {
+export type ApiDataset<T extends DatasetTypes = DatasetTypes> = {
   id: string
-  type: DatasetTypes
+  type: T
   alias: string[] | null
   name: string
   description: string
-  readme?: string
-  schema?: Record<string, DatasetSchema | DatasetSchemaItem>
-  category?: DatasetCategory
-  subcategory?: DatasetSubCategory | EventTypes | string
+  category: DatasetCategory
+  configuration: [DatasetTypes] extends [T]
+    ? DatasetConfiguration
+    : T extends keyof DatasetTypeToConfigurationType
+      ? DatasetConfiguration<T>
+      : null
+  documentation: DatasetDocumentation
+  subcategory?: DatasetSubCategory | EventTypes
   source?: string
   status: DatasetStatus
-  importLogs?: string
   unit?: string
   ownerType: string
   ownerId: number
@@ -282,53 +96,15 @@ export interface Dataset {
   endDate?: string
   lastUpdated?: string
   createdAt: string
-  files?: DatasetFile[]
-  endpoints?: Endpoint[]
-  configuration: AnyDatasetConfiguration | null
   relatedDatasets: RelatedDataset[] | null
-  fieldsAllowed: string[]
-  filters: Record<string, DatasetSchemaItem>
+  filters: DatasetFilters
 }
 
-export interface ThinningConfig {
-  /**
-   * Maximum distance between fishing points (in km)
-   */
-  'distance-fishing'?: number
-  /**
-   * Maximum distance between points in transit (in km)
-   */
-  'distance-transit'?: number
-  /**
-   * Accumulated bearing change between consecutive fishing points (in degrees)
-   */
-  'bearing-val-fishing'?: number
-  /**
-   * Accumulated bearing change between consecutive points in transit (in degrees)
-   */
-  'bearing-val-transit'?: number
-  /**
-   * Accumulated speed change between consecutive fishing points (in percentage)
-   */
-  'change-speed-fishing'?: number
-  /**
-   * Accumulated speed change between consecutive pointsin transit (in percentage)
-   */
-  'change-speed-transit'?: number
-  /**
-   * Maximum number of fishing points that could be skipped when no other
-   * thinning criterias are met (in quantity)
-   *
-   * This is to ensure we always include a point if X previous fishing points
-   * were not included
-   */
-  'min-accuracy-fishing'?: number
-  /**
-   * Maximum number of points in transit that could be skipped when no other
-   * thinning criterias are met (in quantity)
-   *
-   * This is to ensure we always include a point if X previous points in transit
-   * were not included
-   */
-  'min-accuracy-transit'?: number
+export type Dataset<T extends DatasetTypes = DatasetTypes> = ApiDataset<T> & {
+  endpoints?: Endpoint[]
+}
+
+export type DownloadDataset<T extends DatasetTypes = DatasetTypes> = ApiDataset<T> & {
+  readme?: string
+  files?: DatasetFile[]
 }
