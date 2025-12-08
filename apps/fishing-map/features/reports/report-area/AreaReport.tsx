5e0942d1826e370778a74fcd2a27a55aba385f31
j8seangel <jseangel@gmail.com>
2025-12-08 12:09:20 +0100

    stop selecting comparison datasets when not needed

--- apps/fishing-map/features/reports/report-area/AreaReport.tsx
+++ apps/fishing-map/features/reports/report-area/AreaReport.tsx
@@ -11,10 +11,7 @@ import { Spinner, Tabs } from '@globalfishingwatch/ui-components'
 import { TrackCategory, trackEvent } from 'features/app/analytics.hooks'
 import { useAppDispatch } from 'features/app/app.hooks'
 import { selectTimebarSelectedEnvId } from 'features/app/selectors/app.timebar.selectors'
-import {
-  selectActiveReportDataviews,
-  selectReportLayersVisible,
-} from 'features/dataviews/selectors/dataviews.selectors'
+import { selectReportLayersVisible } from 'features/dataviews/selectors/dataviews.selectors'
 import { OUT_OF_TIME_REPORT_AREA_ID } from 'features/reports/report-area/area-reports.config'
 import {
   useFetchReportArea,
@@ -46,8 +43,6 @@ import WorkspaceError from 'features/workspace/WorkspaceError'
 import { useLocationConnect } from 'routes/routes.hook'
 import { TimebarVisualisations } from 'types'
 import { AsyncReducerStatus } from 'utils/async-slice'
-
-import { selectReportComparisonDataviewIds } from '../reports.config.selectors'
 
 import styles from 'features/reports/report-area/AreaReport.module.css'
 
@@ -86,8 +81,6 @@ export default function Report() {
   const reportDataviews = useSelector(selectReportLayersVisible)
   const timebarSelectedEnvId = useSelector(selectTimebarSelectedEnvId)
   const hasChangedSettingsOnce = useSelector(selectHasChangedSettingsOnce)
-  const activeDataviews = useSelector(selectActiveReportDataviews)
-  const reportComparisonDataviewIds = useSelector(selectReportComparisonDataviewIds)
 
   const dataviewCategories = useMemo(
     () => uniq(reportDataviews?.map((d) => getReportCategoryFromDataview(d)) || []),
@@ -145,18 +138,6 @@ export default function Report() {
       highlightArea(undefined)
     }
   }, [highlightArea, reportArea, hasReportBuffer])
-
-  useEffect(() => {
-    if (
-      reportCategory === ReportCategory.Environment &&
-      activeDataviews.length > 0 &&
-      !reportComparisonDataviewIds?.main
-    ) {
-      dispatchQueryParams({
-        reportComparisonDataviewIds: { main: activeDataviews[0].id, compare: '' },
-      })
-    }
-  }, [activeDataviews, dispatchQueryParams, reportCategory, reportComparisonDataviewIds?.main])
 
   const setTimebarVisualizationByCategory = useCallback(
     (category: ReportCategory) => {
