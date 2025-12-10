316e723c1141e8d8e63bb03d07d3926f0b581f13
GeorgiaDesigns <gdmespindola@gmail.com>
2025-12-10 15:56:20 +0100

    reorder timebar settings

--- apps/fishing-map/features/timebar/timebar.hooks.ts
+++ apps/fishing-map/features/timebar/timebar.hooks.ts
@@ -19,8 +19,8 @@ import {
   selectActiveActivityDataviews,
   selectActiveDetectionsDataviews,
   selectActiveEventsDataviews,
+  selectActiveUserPointsWithTimeRangeDataviews,
   selectActiveVesselGroupDataviews,
-  selectPointsActiveReportDataviews,
 } from 'features/dataviews/selectors/dataviews.categories.selectors'
 import { selectActiveTrackDataviews } from 'features/dataviews/selectors/dataviews.instances.selectors'
 import { selectActiveHeatmapEnvironmentalDataviewsWithoutStatic } from 'features/dataviews/selectors/dataviews.selectors'
@@ -292,7 +292,7 @@ export const useTimebarVisualisation = () => {
   const activeDetectionsDataviews = useSelector(selectActiveDetectionsDataviews)
   const activeEventsDataviews = useSelector(selectActiveEventsDataviews)
   const activeVesselGroupDataviews = useSelector(selectActiveVesselGroupDataviews)
-  const activeUserPointsDataviews = useSelector(selectPointsActiveReportDataviews)
+  const activeUserPointsDataviews = useSelector(selectActiveUserPointsWithTimeRangeDataviews)
   const activeTrackDataviews = useSelector(selectActiveTrackDataviews)
   const activeEnvDataviews = useSelector(selectActiveHeatmapEnvironmentalDataviewsWithoutStatic)
   const hasChangedSettingsOnce = useSelector(selectHasChangedSettingsOnce)
@@ -301,7 +301,7 @@ export const useTimebarVisualisation = () => {
   const prevActiveHeatmapDataviewsNum = usePrevious(activeActivityDataviews.length)
   const prevActiveDetectionsDataviewsNum = usePrevious(activeDetectionsDataviews.length)
   const prevActiveVesselGroupDataviewsNum = usePrevious(activeVesselGroupDataviews.length)
-  const prevActiveUserPointsDataviews = usePrevious(activeUserPointsDataviews.length)
+  const prevActiveUserPointsDataviewsNum = usePrevious(activeUserPointsDataviews.length)
   const prevActiveTrackDataviewsNum = usePrevious(activeTrackDataviews.length)
   const prevactiveEnvDataviewsNum = usePrevious(activeEnvDataviews.length)
   const prevActiveEventsDataviewsNum = usePrevious(activeEventsDataviews.length)
@@ -314,11 +314,10 @@ export const useTimebarVisualisation = () => {
         !activeDetectionsDataviews?.length) ||
       (timebarVisualisation === TimebarVisualisations.VesselGroup &&
         !activeVesselGroupDataviews?.length) ||
-      (timebarVisualisation === TimebarVisualisations.Points &&
-        !activeUserPointsDataviews?.length) ||
       (timebarVisualisation === TimebarVisualisations.Vessel && !activeTrackDataviews?.length) ||
       (timebarVisualisation === TimebarVisualisations.Environment && !activeEnvDataviews?.length) ||
-      (timebarVisualisation === TimebarVisualisations.Events && !activeEventsDataviews?.length)
+      (timebarVisualisation === TimebarVisualisations.Events && !activeEventsDataviews?.length) ||
+      (timebarVisualisation === TimebarVisualisations.Points && !activeUserPointsDataviews?.length)
     ) {
       if (activeActivityDataviews?.length) {
         dispatchTimebarVisualisation(TimebarVisualisations.HeatmapActivity, true)
@@ -326,14 +325,14 @@ export const useTimebarVisualisation = () => {
         dispatchTimebarVisualisation(TimebarVisualisations.HeatmapDetections, true)
       } else if (activeVesselGroupDataviews?.length) {
         dispatchTimebarVisualisation(TimebarVisualisations.VesselGroup, true)
-      } else if (activeUserPointsDataviews?.length) {
-        dispatchTimebarVisualisation(TimebarVisualisations.Points, true)
       } else if (activeTrackDataviews?.length) {
         dispatchTimebarVisualisation(TimebarVisualisations.Vessel, true)
       } else if (activeEnvDataviews?.length) {
         dispatchTimebarVisualisation(TimebarVisualisations.Environment, true)
       } else if (activeEventsDataviews?.length) {
         dispatchTimebarVisualisation(TimebarVisualisations.Events, true)
+      } else if (activeUserPointsDataviews?.length) {
+        dispatchTimebarVisualisation(TimebarVisualisations.Points, true)
       }
     } else if (!hasChangedSettingsOnce) {
       if (activeActivityDataviews.length === 1 && prevActiveHeatmapDataviewsNum === 0) {
@@ -347,12 +346,12 @@ export const useTimebarVisualisation = () => {
         dispatchTimebarVisualisation(TimebarVisualisations.VesselGroup, true)
       } else if (activeTrackDataviews.length >= 1 && prevActiveTrackDataviewsNum === 0) {
         dispatchTimebarVisualisation(TimebarVisualisations.Vessel, true)
-      } else if (activeUserPointsDataviews.length >= 1 && prevActiveUserPointsDataviews === 0) {
-        dispatchTimebarVisualisation(TimebarVisualisations.Points, true)
       } else if (activeEnvDataviews.length === 1 && prevactiveEnvDataviewsNum === 0) {
         dispatchTimebarVisualisation(TimebarVisualisations.Environment, true)
       } else if (activeEventsDataviews.length === 1 && prevActiveEventsDataviewsNum === 0) {
         dispatchTimebarVisualisation(TimebarVisualisations.Events, true)
+      } else if (activeUserPointsDataviews.length >= 1 && prevActiveUserPointsDataviewsNum === 0) {
+        dispatchTimebarVisualisation(TimebarVisualisations.Points, true)
       }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
