e11bb5f7f78821f53ec6d45282f9de400d72254d
j8seangel <jseangel@gmail.com>
2026-01-07 14:03:40 +0100

    review initial html render

--- apps/fishing-map/features/sidebar/sidebar.utils.ts
+++ apps/fishing-map/features/sidebar/sidebar.utils.ts
@@ -1,6 +1,9 @@
 export const SCROLL_CONTAINER_DOM_ID = 'scroll-container'
 
 export function getScrollElement(id = SCROLL_CONTAINER_DOM_ID) {
+  if (typeof window === 'undefined') {
+    return null
+  }
   return document.getElementById(id) as HTMLElement
 }
 
