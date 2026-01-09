aec3041449f56942aba42d58a99bad94f780f4fc
j8seangel <jseangel@gmail.com>
2026-01-09 13:22:11 +0100

    update eslint to latest version and reuse shared lib

--- apps/fishing-map/eslint.config.js
+++ apps/fishing-map/eslint.config.js
@@ -1,8 +1,1 @@
-const gfwConfig = require('../../eslint.config.js')
-
-module.exports = [
-  ...gfwConfig,
-  {
-    ignores: ['.next', 'features/i18n/i18n.types.d.ts'],
-  },
-]
+export { default } from '../../eslint.config.js'
