a21f93650e55a6e5725f09884c8aca79222e46f1
j8seangel <jseangel@gmail.com>
2026-02-09 18:07:02 +0100

    replace Links and query params redux navigation

--- apps/fishing-map/store.ts
+++ apps/fishing-map/store.ts
@@ -1,4 +1,4 @@
-import type { Action, AnyAction, Middleware, ThunkAction, ThunkDispatch } from '@reduxjs/toolkit'
+import type { Action, AnyAction, ThunkAction, ThunkDispatch } from '@reduxjs/toolkit'
 import { configureStore } from '@reduxjs/toolkit'
 import { logoutUserMiddleware } from 'middlewares'
 import { queriesApiMiddlewares, queriesApiReducers } from 'queries'
