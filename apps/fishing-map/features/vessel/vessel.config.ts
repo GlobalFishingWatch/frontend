f6a8c9700b840a25554e7c774f19c92440ab5c0d
j8seangel <jseangel@gmail.com>
2026-02-11 09:18:12 +0100

    fix default random forest vessel identity dataset id

--- apps/fishing-map/features/vessel/vessel.config.ts
+++ apps/fishing-map/features/vessel/vessel.config.ts
@@ -11,8 +11,8 @@ import type { IdentityVesselData } from 'features/vessel/vessel.slice'
 import type { VesselProfileState } from './vessel.types'
 
 export const DEFAULT_VESSEL_IDENTITY_DATASET = 'public-global-vessel-identity-vi-653'
-export const DEFAULT_VESSEL_IDENTITY_ID =
-  `${DEFAULT_VESSEL_IDENTITY_DATASET}:${PIPE_DATASET_ID}` as const
+export const DEFAULT_VESSEL_IDENTITY_ID = `${DEFAULT_VESSEL_IDENTITY_DATASET}:v1.0` as const
+// `${DEFAULT_VESSEL_IDENTITY_DATASET}:${PIPE_DATASET_ID}` as const
 export const INCLUDES_RELATED_SELF_REPORTED_INFO_ID =
   'POTENTIAL_RELATED_SELF_REPORTED_INFO' as const
 export const CACHE_FALSE_PARAM = { id: 'cache', value: 'false' }
