5a01fbbb7d4b30da0ff07af62d562087f375abbf
Carlos Díaz <linkmetal18@gmail.com>
2026-03-17 11:59:45 +0000

    fix: change timeout by a polling expect with 10 seconds of timeout

--- apps/fishing-map/test/integration/MarineManager.spec.tsx
+++ apps/fishing-map/test/integration/MarineManager.spec.tsx
@@ -45,76 +45,74 @@ describe('Marine Manager', () => {
     expect(fijiWorkspace).toBeDefined()
 
     // Wait for layers to initialize
-    await new Promise((resolve) => setTimeout(resolve, 5000))
-
-    const deckLayersState = jotaiStore.get(deckLayersStateAtom)
-
-    expect(deckLayersState).toMatchObject({
-      basemap: {
-        loaded: false,
-        cacheHash: '',
-      },
-      'context-layer-high-seas': {
-        loaded: true,
-        cacheHash: '',
-      },
-      'context-layer-rfmo': {
-        loaded: true,
-        cacheHash: '',
-      },
-      'context-layer-mpa': {
-        loaded: true,
-        cacheHash: '',
-      },
-      'context-layer-eez': {
-        loaded: true,
-        cacheHash: '',
-      },
-      'sar-match-auxiliar': {
-        loaded: true,
-        cacheHash: '',
-      },
-      'sentinel2-auxiliar': {
-        loaded: true,
-        cacheHash: '',
-      },
-      'global-sea-surface-temperature': {
-        loaded: true,
-        cacheHash:
-          '1761955200000,1771372800000,1759276800000,DAY,,,-global-sea-surface-temperature-public-global-sst:v20231213--|false|true',
-      },
-      'global-water-salinity': {
-        loaded: true,
-        cacheHash:
-          '1761955200000,1771372800000,1759276800000,DAY,,,-global-water-salinity-public-global-salinity:v20231213--|false|true',
-      },
-      'global-chlorophyl': {
-        loaded: true,
-        cacheHash:
-          '1735689600000,1771372800000,1704067200000,MONTH,,,-global-chlorophyl-public-global-chlorophyl:v20231213--|false|true',
-      },
-      'sentinel2,viirs-match,sar-match': {
-        loaded: true,
-        cacheHash:
-          '1761955200000,1771372800000,1759276800000,DAY,,,-sentinel2,viirs-match,sar-match-public-global-sentinel2-presence:v4.0,public-global-viirs-presence:v4.0,public-global-sar-presence:v4.0--,,|false|true',
-      },
-      'fishing,presence': {
-        loaded: true,
-        cacheHash:
-          '1761955200000,1771372800000,1759276800000,DAY,,,-fishing,presence-public-global-fishing-effort:v4.0,public-global-presence:v4.0--,|false|true',
-      },
-      'encounter-events': {
-        loaded: true,
-        cacheHash: 'true',
-      },
-      'basemap-labels': {
-        loaded: true,
-        cacheHash: '',
-      },
-      'context-layer-graticules': {
-        loaded: true,
-        cacheHash: '',
-      },
-    })
+    await expect
+      .poll(() => jotaiStore.get(deckLayersStateAtom), { timeout: 10000 })
+      .toMatchObject({
+        basemap: {
+          loaded: true,
+          cacheHash: '',
+        },
+        'context-layer-high-seas': {
+          loaded: true,
+          cacheHash: '',
+        },
+        'context-layer-rfmo': {
+          loaded: true,
+          cacheHash: '',
+        },
+        'context-layer-mpa': {
+          loaded: true,
+          cacheHash: '',
+        },
+        'context-layer-eez': {
+          loaded: true,
+          cacheHash: '',
+        },
+        'sar-match-auxiliar': {
+          loaded: true,
+          cacheHash: '',
+        },
+        'sentinel2-auxiliar': {
+          loaded: true,
+          cacheHash: '',
+        },
+        'global-sea-surface-temperature': {
+          loaded: true,
+          cacheHash:
+            '1761955200000,1771372800000,1759276800000,DAY,,,-global-sea-surface-temperature-public-global-sst:v20231213--|false|true',
+        },
+        'global-water-salinity': {
+          loaded: true,
+          cacheHash:
+            '1761955200000,1771372800000,1759276800000,DAY,,,-global-water-salinity-public-global-salinity:v20231213--|false|true',
+        },
+        'global-chlorophyl': {
+          loaded: true,
+          cacheHash:
+            '1735689600000,1771372800000,1704067200000,MONTH,,,-global-chlorophyl-public-global-chlorophyl:v20231213--|false|true',
+        },
+        'sentinel2,viirs-match,sar-match': {
+          loaded: true,
+          cacheHash:
+            '1761955200000,1771372800000,1759276800000,DAY,,,-sentinel2,viirs-match,sar-match-public-global-sentinel2-presence:v4.0,public-global-viirs-presence:v4.0,public-global-sar-presence:v4.0--,,|false|true',
+        },
+        'fishing,presence': {
+          loaded: true,
+          cacheHash:
+            '1761955200000,1771372800000,1759276800000,DAY,,,-fishing,presence-public-global-fishing-effort:v4.0,public-global-presence:v4.0--,|false|true',
+        },
+        'encounter-events': {
+          loaded: true,
+          cacheHash: 'true',
+        },
+        'basemap-labels': {
+          loaded: true,
+          cacheHash: '',
+        },
+        'context-layer-graticules': {
+          loaded: true,
+          cacheHash: '',
+        },
+      })
   })
 })
