7e8a071c5cfafcbe4cd8cf1385f40cc587b14b00
j8seangel <jseangel@gmail.com>
2026-03-25 19:03:30 +0100

    add screenshots to github

--- apps/fishing-map-e2e/src/e2e/screenshots/screenshots.config.ts
+++ apps/fishing-map-e2e/src/e2e/screenshots/screenshots.config.ts
@@ -93,11 +93,6 @@ const ANALYSTS_URLS: MapUrl[] = [
   },
 ]
 
-const PUBLIC_URLS: MapUrl[] = [
-  {
-    id: 'default',
-    url: 'https://globalfishingwatch.org/map',
-  },
-]
+const PUBLIC_URLS: MapUrl[] = [{ id: 'default', url: 'https://globalfishingwatch.org/map' }]
 
 export const MAP_URLS = [...PUBLIC_URLS, ...MARINE_MANAGER_URLS, ...ANALYSTS_URLS]
