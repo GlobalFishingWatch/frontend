c1645f5be4aaf2587840da1e1a691e30fa3cf4f4
satellitestudiodesign <juancarlos@satellitestud.io>
2026-04-01 16:38:56 +0200

    load login page in popup

--- apps/fishing-map/router/LoginLink.tsx
+++ apps/fishing-map/router/LoginLink.tsx
@@ -1,9 +1,12 @@
-import { forwardRef } from 'react'
+import { forwardRef, useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 import { getLoginUrl, useLoginRedirect } from '@globalfishingwatch/react-hooks'
+import { Modal } from '@globalfishingwatch/ui-components'
 
+import { ROOT_DOM_ELEMENT } from 'data/config'
 import { useAppDispatch } from 'features/app/app.hooks'
+import { fetchUserThunk } from 'features/user/user.slice'
 import { selectWorkspaceHistoryNavigation } from 'features/workspace/workspace.selectors'
 import { setWorkspaceSuggestSave } from 'features/workspace/workspace.slice'
 
@@ -16,24 +19,54 @@ function LocalStorageLoginLink({ children, className = '' }: LocalStorageLoginLi
   const { saveRedirectUrl, saveHistoryNavigation } = useLoginRedirect()
   const dispatch = useAppDispatch()
   const workspaceHistoryNavigation = useSelector(selectWorkspaceHistoryNavigation)
+  const [iframeOpen, setIframeOpen] = useState(false)
 
-  const onClick = () => {
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      if (event.origin !== window.location.origin) return
+      if (event.data?.type === 'LOGIN_SUCCESS') {
+        dispatch(fetchUserThunk({ accessToken: event.data.accessToken }))
+        setIframeOpen(false)
+      }
+    }
+    window.addEventListener('message', handleMessage)
+    return () => window.removeEventListener('message', handleMessage)
+  }, [dispatch])
+
+  const onClick = (e: React.MouseEvent) => {
+    e.preventDefault()
     dispatch(setWorkspaceSuggestSave(false))
     saveRedirectUrl()
     saveHistoryNavigation(workspaceHistoryNavigation)
+    setIframeOpen(true)
   }
 
   return (
-    <a
-      ref={ref}
-      href={getLoginUrl()}
-      onClick={onClick}
-      className={className}
-      title="Login"
-      data-testid="login-link"
-    >
-      {children}
-    </a>
+    <>
+      <a
+        ref={ref}
+        href={getLoginUrl()}
+        onClick={onClick}
+        className={className}
+        title="Login"
+        data-testid="login-link"
+      >
+        {children}
+      </a>
+      {iframeOpen && (
+        <Modal
+          appSelector={ROOT_DOM_ELEMENT}
+          isOpen={iframeOpen}
+          onClose={() => setIframeOpen(false)}
+        >
+          <iframe
+            src={getLoginUrl()}
+            style={{ width: '100%', height: '80vh', border: 'none', minHeight: '600px' }}
+            title="SSO Login"
+          />
+        </Modal>
+      )}
+    </>
   )
 }
 
--- apps/fishing-map/router/routes.hook.ts
+++ apps/fishing-map/router/routes.hook.ts
@@ -100,6 +100,12 @@ export const useReplaceLoginUrl = () => {
     const currentQuery = parse(window.location.search, { ignoreQueryPrefix: true })
     const hasCallbackUrlStorageQuery = currentQuery[DEFAULT_CALLBACK_URL_PARAM]
     const accessToken = currentQuery[ACCESS_TOKEN_STRING]
+
+    if (hasCallbackUrlStorageQuery && window.parent !== window) {
+      window.parent.postMessage({ type: 'LOGIN_SUCCESS', accessToken }, window.location.origin)
+      return
+    }
+
     if (redirectUrl && hasCallbackUrlStorageQuery) {
       const query = {
         ...parseWorkspace(new URL(redirectUrl).search),
