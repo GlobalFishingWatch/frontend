4502a27eeb6407ce9a289cf93621899d3426efc7
GeorgiaDesigns <gdmespindola@gmail.com>
2026-04-21 11:41:27 -0300

    fix: Cannot access refs during render

--- libs/ui-components/src/input-date/InputDate.tsx
+++ libs/ui-components/src/input-date/InputDate.tsx
@@ -1,5 +1,5 @@
 import type { JSX, Ref } from 'react'
-import React, { forwardRef, useImperativeHandle, useRef } from 'react'
+import React, { forwardRef, useImperativeHandle, useRef, useState } from 'react'
 import cx from 'classnames'
 
 import { IconButton } from '../icon-button'
@@ -46,12 +46,13 @@ function InputDateComponent(props: InputDateProps, forwardedRef: Ref<HTMLInputEl
   } = props
   const inputRef = useRef<HTMLInputElement>(null)
   useImperativeHandle(forwardedRef, () => inputRef.current as HTMLInputElement)
+  const [isNativelyInvalid, setIsNativelyInvalid] = useState(false)
 
   const yymmddDate = value?.toString().slice(0, 10)
 
   const labelContent = htmlLabel || label
 
-  const isInvalid = props.invalid === true || inputRef.current?.validity.valid === false
+  const isInvalid = props.invalid === true || isNativelyInvalid
 
   const inputProps = {
     id: id ?? label,
@@ -76,6 +77,14 @@ function InputDateComponent(props: InputDateProps, forwardedRef: Ref<HTMLInputEl
         className={cx(styles.input, { [styles.invalid]: isInvalid })}
         ref={inputRef}
         {...inputProps}
+        onInput={(e) => {
+          setIsNativelyInvalid(e.currentTarget.validity.valid === false)
+          inputProps.onInput?.(e)
+        }}
+        onInvalid={(e) => {
+          setIsNativelyInvalid(true)
+          inputProps.onInvalid?.(e)
+        }}
         key={label || defaultKey}
       />
       <div className={styles.actionsContainer}>
