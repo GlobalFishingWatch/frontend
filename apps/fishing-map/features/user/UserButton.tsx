d19dd41649e527c907c963218f2c5c54f2d84699
GeorgiaDesigns <gdmespindola@gmail.com>
2026-05-18 22:40:08 -0300

    add animation for indicating saved workspaces location

--- apps/fishing-map/features/user/UserButton.tsx
+++ apps/fishing-map/features/user/UserButton.tsx
@@ -1,8 +1,10 @@
+import { useEffect, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
+import cx from 'classnames'
 import Link from 'redux-first-router-link'
 
-import { Icon, Tooltip } from '@globalfishingwatch/ui-components'
+import { Icon, IconButton, Tooltip } from '@globalfishingwatch/ui-components'
 
 import { DEFAULT_WORKSPACE_LIST_VIEWPORT } from 'data/config'
 import {
@@ -10,19 +12,37 @@ import {
   selectIsUserExpired,
   selectUserData,
 } from 'features/user/selectors/user.selectors'
+import { selectWorkspaceCustomStatus } from 'features/workspace/workspace.selectors'
 import LocalStorageLoginLink from 'routes/LoginLink'
 import { USER } from 'routes/routes'
+import { AsyncReducerStatus } from 'utils/async-slice'
+
+import styles from './UserButton.module.css'
 
 const UserButton = ({ className = '', testId }: { className?: string; testId?: string }) => {
   const { t } = useTranslation()
   const guestUser = useSelector(selectIsGuestUser)
   const isUserExpired = useSelector(selectIsUserExpired)
   const userData = useSelector(selectUserData)
+  const customStatus = useSelector(selectWorkspaceCustomStatus)
+  const prevStatusRef = useRef(customStatus)
+  const [isAnimating, setIsAnimating] = useState(false)
+
+  useEffect(() => {
+    if (
+      prevStatusRef.current === AsyncReducerStatus.Loading &&
+      customStatus === AsyncReducerStatus.Finished
+    ) {
+      setIsAnimating(true)
+    }
+    prevStatusRef.current = customStatus
+  }, [customStatus])
+
   const initials = userData?.firstName
     ? `${userData?.firstName?.slice(0, 1)}${userData?.lastName?.slice(0, 1)}`
     : ''
   return (
-    <div className={className}>
+    <div className={cx(className, styles.wrapper)}>
       {guestUser || isUserExpired ? (
         <Tooltip content={t((t) => t.common.login)}>
           <LocalStorageLoginLink>
@@ -42,6 +62,15 @@ const UserButton = ({ className = '', testId }: { className?: string; testId?: s
           {userData ? initials : <Icon icon="user" className="print-hidden" />}
         </Link>
       )}
+      <span
+        aria-hidden
+        className={cx(styles.savePopIcon, { [styles.animating]: isAnimating })}
+        onAnimationEnd={() => setIsAnimating(false)}
+      >
+        <span className={styles.savePopBadge}>
+          <IconButton icon="save" type="solid" />
+        </span>
+      </span>
     </div>
   )
 }
