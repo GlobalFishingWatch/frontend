8bb466dace0bcd6213ddf5862a194eec80636947
j8seangel <jseangel@gmail.com>
2026-05-20 08:28:35 +0200

    rollback columns guess

--- libs/data-transforms/src/schema/guess-columns.ts
+++ libs/data-transforms/src/schema/guess-columns.ts
@@ -85,7 +85,7 @@ export const guessColumnsFromFilters = (
     return emptyGuessedColumns
   }
   const guessedColumns = GUESS_COLUMN_NAMES.map(([columnToGuess, candidates]) => {
-    const exactGuess = candidates?.find((candidate) => columns.includes(candidate))
+    const exactGuess = columns?.find((column) => candidates.includes(column))
     const longEnoughCandidates = candidates.filter((c) => c.length >= 3)
     const approximateGuess =
       !exactGuess &&
