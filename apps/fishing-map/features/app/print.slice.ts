87b9b888c25084ecf7ac8cd530f773a6c584b61e
satellitestudiodesign <juancarlos@satellitestud.io>
2026-05-21 12:13:05 +0200

    show all report vessels in print mode

---
+++ apps/fishing-map/features/app/print.slice.ts
@@ -0,0 +1,18 @@
+import type { PayloadAction } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
+import type { RootState } from 'reducers'
+
+const printSlice = createSlice({
+  name: 'print',
+  initialState: { printMode: false },
+  reducers: {
+    setPrintMode: (state, action: PayloadAction<boolean>) => {
+      state.printMode = action.payload
+    },
+  },
+})
+
+export const { setPrintMode } = printSlice.actions
+export const selectPrintMode = (state: RootState): boolean => state.print.printMode
+
+export default printSlice.reducer
