05eb42272d0208fbdc1d4658617ff097e4d15120
GeorgiaDesigns <gdmespindola@gmail.com>
2026-06-02 12:32:41 -0300

    remove tracePropagationTargets

--- apps/fishing-map/client.tsx
+++ apps/fishing-map/client.tsx
@@ -7,7 +7,6 @@ Sentry.init({
   dsn: 'https://f093e15df0145c6c0b1b9afe8f15fdba@o4510353401577472.ingest.us.sentry.io/4510462762942464',
   sendDefaultPii: true,
   tracesSampleRate: 1.0,
-  tracePropagationTargets: ['localhost', /^https:\/\/gateway\.api\..*\.globalfishingwatch\.org/],
 })
 
 startTransition(() => {
