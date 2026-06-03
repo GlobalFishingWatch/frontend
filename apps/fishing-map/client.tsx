26c2b151370eb806028ca329a69d3a155f572980
GeorgiaDesigns <gdmespindola@gmail.com>
2026-06-03 13:35:51 -0300

    enable sentry loggs only on production

--- apps/fishing-map/client.tsx
+++ apps/fishing-map/client.tsx
@@ -5,6 +5,7 @@ import { StartClient } from '@tanstack/react-start/client'
 
 Sentry.init({
   dsn: 'https://f093e15df0145c6c0b1b9afe8f15fdba@o4510353401577472.ingest.us.sentry.io/4510462762942464',
+  enabled: import.meta.env.PROD,
   sendDefaultPii: true,
   tracesSampleRate: 1.0,
 })
