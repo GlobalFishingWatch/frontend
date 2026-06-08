96ce4110baf88334b7b8e403937f6fc5986d7b42
GeorgiaDesigns <gdmespindola@gmail.com>
2026-06-08 12:05:00 -0300

    redirect to correct section from other section in user guide

--- apps/fishing-map/features/content-panel/MarkdownLink.tsx
+++ apps/fishing-map/features/content-panel/MarkdownLink.tsx
@@ -1,14 +1,42 @@
 import { useMemo } from 'react'
 import { parse } from 'qs'
 
+import type {
+  UserGuideSectionSlug,
+  UserGuideSubSectionSlug,
+} from 'features/cms/loaders/user-guide.types'
+import { CATEGORIES_CONFIG } from 'features/cms/loaders/user-guide.types'
+import { useSidePanel } from 'features/content-panel/contentPanel.hooks'
 import { useReplaceQueryParams } from 'router/routes.hook'
 import { Route } from 'routes/_app'
 import type { QueryParams } from 'types'
 
 type MarkdownLinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement>
 
+function findExistingSectionForSlug(slug: string | undefined): {
+  section: UserGuideSectionSlug
+  subSection?: UserGuideSubSectionSlug
+} | null {
+  if (!slug) {
+    return null
+  }
+  if (slug in CATEGORIES_CONFIG) {
+    return { section: slug as UserGuideSectionSlug }
+  }
+  for (const [section, subsections] of Object.entries(CATEGORIES_CONFIG) as [
+    UserGuideSectionSlug,
+    readonly string[],
+  ][]) {
+    if (subsections.includes(slug)) {
+      return { section, subSection: slug as UserGuideSubSectionSlug }
+    }
+  }
+  return null
+}
+
 const MarkdownLink = ({ href, children, ...props }: MarkdownLinkProps) => {
   const { replaceQueryParams } = useReplaceQueryParams()
+  const { openSidePanel } = useSidePanel()
   const { sidePanelContent, sidePanelId, sidePanelSubcontentId } = Route.useSearch()
 
   const sameRouteUrl = useMemo(() => {
@@ -22,6 +50,29 @@ const MarkdownLink = ({ href, children, ...props }: MarkdownLinkProps) => {
       return null
     }
   }, [href])
+
+  if (href?.startsWith('#')) {
+    const sectionMatch = findExistingSectionForSlug(href.replace(/^#/, ''))
+    if (!sectionMatch) {
+      return <span>{children}</span>
+    }
+    return (
+      <a
+        href={href}
+        onClick={(e) => {
+          e.preventDefault()
+          openSidePanel({
+            type: 'userGuide',
+            id: sectionMatch.section,
+            subcontentId: sectionMatch.subSection,
+          })
+        }}
+        {...props}
+      >
+        {children}
+      </a>
+    )
+  }
 
   if (!sameRouteUrl) {
     return (
