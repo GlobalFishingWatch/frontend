94c6681dcc557e068e56ef5fc59a3fffc1059ef2
satellitestudiodesign <juancarlos@satellitestud.io>
2026-06-12 09:26:28 +0200

    self host fonts

--- apps/fishing-map/client.tsx
+++ apps/fishing-map/client.tsx
@@ -3,6 +3,9 @@ import { hydrateRoot } from 'react-dom/client'
 import * as Sentry from '@sentry/tanstackstart-react'
 import { StartClient } from '@tanstack/react-start/client'
 
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+
 Sentry.init({
   dsn: 'https://f093e15df0145c6c0b1b9afe8f15fdba@o4510353401577472.ingest.us.sentry.io/4510462762942464',
   enabled: import.meta.env.PROD,
