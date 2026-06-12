bc81932003addaf70697c917999d5dcff3a35c5a
j8seangel <jseangel@gmail.com>
2026-06-12 13:33:25 +0200

    fix typo

--- apps/fishing-map/routes/_app/$category/index.tsx
+++ apps/fishing-map/routes/_app/$category/index.tsx
@@ -2,13 +2,13 @@ import { createFileRoute, lazyRouteComponent } from '@tanstack/react-router'
 import { lowerCase } from 'es-toolkit'
 
 import type { WorkspaceCategoryDescriptionKey } from 'router/router.meta'
-import { getTFuntion, getWorkspaceHead } from 'router/router.meta'
+import { getTFunction, getWorkspaceHead } from 'router/router.meta'
 
 export const Route = createFileRoute('/_app/$category/')({
   component: lazyRouteComponent(() => import('features/workspaces-list/WorkspacesList')),
   head: ({ matches, params }) =>
     getWorkspaceHead(
       lowerCase(params.category || '') as WorkspaceCategoryDescriptionKey,
-      getTFuntion(matches)
+      getTFunction(matches)
     ),
 })
