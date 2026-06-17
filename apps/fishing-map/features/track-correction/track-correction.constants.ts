81def96368fd35a2e4a322e4443f36907dfe0088
j8seangel <jseangel@gmail.com>
2026-06-17 15:35:36 +0200

    support custom layer filters in TT layers

--- apps/fishing-map/features/track-correction/track-correction.constants.ts
+++ apps/fishing-map/features/track-correction/track-correction.constants.ts
@@ -8,6 +8,8 @@ export const TURNING_TIDES_WORKSPACES_IDS = [
   'tt-chile-public',
   'tt-peru-public',
   'tt_ais-public',
+  'tt_ais_v_2_1-public',
+  'tt_vms_v_2_1-public',
 ] as const
 
 export type TurningTidesWorkspaceId = (typeof TURNING_TIDES_WORKSPACES_IDS)[number]
@@ -18,4 +20,6 @@ export const TRACK_CORRECTION_SPREADSHEET_ID_BY_WORKSPACE: Record<TurningTidesWo
     'tt-chile-public': TURNING_TIDES_CHILE_ID,
     'tt-peru-public': TURNING_TIDES_PERU_ID,
     'tt_ais-public': TURNING_TIDES_AIS_ID,
+    'tt_ais_v_2_1-public': TURNING_TIDES_AIS_ID,
+    'tt_vms_v_2_1-public': TURNING_TIDES_AIS_ID,
   }
