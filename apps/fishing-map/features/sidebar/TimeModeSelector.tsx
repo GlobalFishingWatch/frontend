7b0ad7722635220d8ee7b3624faa1c7b5e73593f
j8seangel <jseangel@gmail.com>
2026-07-08 09:04:39 +0200

    update dates in time mode selector changes

--- apps/fishing-map/features/sidebar/TimeModeSelector.tsx
+++ apps/fishing-map/features/sidebar/TimeModeSelector.tsx
@@ -1,19 +1,29 @@
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
+import { DateTime } from 'luxon'
 
 import type { ChoiceOption } from '@globalfishingwatch/ui-components'
 import { Choice } from '@globalfishingwatch/ui-components'
 
+import { useAppDispatch } from 'features/app/app.hooks'
+import { useTimerangeConnect } from 'features/timebar/timebar.hooks'
+import type { TimeRange } from 'features/timebar/timebar.slice'
+import { setRealTimeLatestUpdate } from 'features/timebar/timebar.slice'
 import { selectTimeMode } from 'features/workspace/workspace.selectors'
 import { useReplaceQueryParams } from 'router/routes.hook'
 import type { TimeMode } from 'types'
+import { getRealTimeTimerange } from 'utils/dates'
 
 import styles from './SidebarHeader.module.css'
 
 function TimeModeSelector() {
+  const dispatch = useAppDispatch()
   const { t } = useTranslation()
   const { replaceQueryParams } = useReplaceQueryParams()
+  const { timerange, setTimerange } = useTimerangeConnect()
   const timeMode = useSelector(selectTimeMode)
+  const [previousTimeRange, setPreviousTimeRange] = useState<TimeRange | null>(null)
 
   const options = [
     {
@@ -26,14 +36,25 @@ function TimeModeSelector() {
     },
   ] as ChoiceOption<TimeMode>[]
 
+  const onSelect = (option: ChoiceOption<TimeMode>) => {
+    if (option.id === timeMode) {
+      return
+    }
+    replaceQueryParams({ timeMode: option.id })
+    if (option.id === 'realTime') {
+      setPreviousTimeRange(timerange)
+      setTimerange(getRealTimeTimerange())
+      dispatch(setRealTimeLatestUpdate(DateTime.now().toISO() as string))
+    } else {
+      if (previousTimeRange) {
+        setTimerange(previousTimeRange)
+      }
+    }
+  }
+
   return (
     <div className={styles.header}>
-      <Choice
-        size="medium"
-        options={options}
-        activeOption={timeMode}
-        onSelect={(option) => replaceQueryParams({ timeMode: option.id })}
-      />
+      <Choice size="medium" options={options} activeOption={timeMode} onSelect={onSelect} />
     </div>
   )
 }
