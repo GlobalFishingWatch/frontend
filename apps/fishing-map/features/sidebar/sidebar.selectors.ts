e206101354d9807f486328767af05e4ffdd26e53
j8seangel <jseangel@gmail.com>
2026-07-09 09:58:06 +0200

    remove realTime selector from report routes

--- apps/fishing-map/features/sidebar/sidebar.selectors.ts
+++ apps/fishing-map/features/sidebar/sidebar.selectors.ts
@@ -1,15 +1,11 @@
 import { createSelector } from '@reduxjs/toolkit'
 
-import {
-  selectIsAnyReportLocation,
-  selectIsAnyVesselLocation,
-  selectIsWorkspaceLocation,
-} from 'router/routes.selectors'
+import { selectIsAnyVesselLocation, selectIsWorkspaceLocation } from 'router/routes.selectors'
 
 export const selectHasTimeModeEnabled = createSelector(
-  [selectIsWorkspaceLocation, selectIsAnyVesselLocation, selectIsAnyReportLocation],
-  (isWorkspaceLocation, isAnyVesselLocation, isAnyReportLocation) => {
-    if (isWorkspaceLocation || isAnyVesselLocation || isAnyReportLocation) {
+  [selectIsWorkspaceLocation, selectIsAnyVesselLocation],
+  (isWorkspaceLocation, isAnyVesselLocation) => {
+    if (isWorkspaceLocation || isAnyVesselLocation) {
       return true
     }
     return false
