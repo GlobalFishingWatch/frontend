768700cfc77f637ba62e3e47321d13e75d1faccb
j8seangel <jseangel@gmail.com>
2026-07-22 15:48:21 +0200

    remove develop triggers

--- cloudbuild/main.tf
+++ cloudbuild/main.tf
@@ -2,12 +2,6 @@ provider "google" {
   project = "gfw-int-infrastructure"
 }
 
-module "develop" {
-  source            = "./template"
-  short_environment = "dev"
-  branch_name       = "develop"
-}
-
 module "staging" {
   source            = "./template"
   short_environment = "sta"
