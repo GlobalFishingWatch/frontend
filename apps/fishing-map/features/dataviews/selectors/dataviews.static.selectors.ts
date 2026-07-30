e4e00241c95ed2236941f4bb284c68fe5b2c539f
GeorgiaDesigns <gdmespindola@gmail.com>
2026-07-30 12:38:45 -0300

    select activity private per country dataviews instead of static slug

--- apps/fishing-map/features/dataviews/selectors/dataviews.static.selectors.ts
+++ apps/fishing-map/features/dataviews/selectors/dataviews.static.selectors.ts
@@ -1,11 +1,14 @@
 import { createSelector } from '@reduxjs/toolkit'
 
+import { DatasetSubCategory } from '@globalfishingwatch/api-types'
+
 import {
   FISHING_DATAVIEW_SLUG_ALL,
   PRESENCE_DATAVIEW_SLUG,
   VESSEL_TEMPLATE_DATAVIEW_SLUGS,
 } from 'data/workspaces'
 import { VMS_VESSEL_DATAVIEW_SLUGS } from 'data/workspaces-vms'
+import { selectAllDatasets } from 'features/datasets/datasets.slice'
 import { selectAllDataviews } from 'features/dataviews/dataviews.slice'
 
 const VESSEL_TRACK_DATAVIEW_TEMPLATES = [
@@ -26,6 +29,25 @@ export const selectVesselTemplateDataviews = createSelector(
 export const selectPresenceDataview = createSelector([selectAllDataviews], (dataviews = []) => {
   return dataviews.find((dataview) => dataview.slug === PRESENCE_DATAVIEW_SLUG)
 })
+
+const selectActivityDataviewsBySubcategory = (subcategory: DatasetSubCategory) =>
+  createSelector([selectAllDataviews, selectAllDatasets], (dataviews = [], allDatasets = []) => {
+    return dataviews.filter((dataview) =>
+      dataview.datasetsConfig?.some(
+        (datasetConfig) =>
+          allDatasets.find((dataset) => dataset.id === datasetConfig.datasetId)?.subcategory ===
+          subcategory
+      )
+    )
+  })
+
+export const selectPresenceDataviews = selectActivityDataviewsBySubcategory(
+  DatasetSubCategory.Presence
+)
+
+export const selectFishingDataviews = selectActivityDataviewsBySubcategory(
+  DatasetSubCategory.Fishing
+)
 
 export const selectFishingDataview = createSelector([selectAllDataviews], (dataviews = []) => {
   return dataviews.find((dataview) => dataview.slug === FISHING_DATAVIEW_SLUG_ALL)
