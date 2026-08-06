b43822d6ebf4d76733d290fc48c37f7a52897286
j8seangel <jseangel@gmail.com>
2026-08-06 15:44:50 +0200

    avoid colorRamp re calculation

--- apps/platform/features/_map/timebar/timebar.utils.ts
+++ apps/platform/features/_map/timebar/timebar.utils.ts
@@ -1,12 +1,9 @@
-import type { Color } from '@deck.gl/core'
-import { scaleLinear } from 'd3-scale'
 import type { Feature, Point } from 'geojson'
 import type { DateTimeUnit, DurationUnit } from 'luxon'
 import { DateTime } from 'luxon'
 
 import type { TimeFilterType } from '@globalfishingwatch/api-types'
 import { getDateInIntervalResolution, getFeatureTimeRange } from '@globalfishingwatch/deck-layers'
-import { rgbaStringToComponents } from '@globalfishingwatch/deck-layers/utils'
 import type {
   FourwingsFeature,
   FourwingsInterval,
@@ -396,13 +393,4 @@ export function getGraphDataFromFourwingsHeatmap(
   })
 
   return timeseries.sort((a, b) => a.date - b.date)
-}
-
-export const getLegendColorScale = (domain: number[], rgbaColors: string[]) => {
-  if (!domain?.length || domain.length !== rgbaColors?.length) {
-    return undefined
-  }
-  const colors = rgbaColors.map((color) => rgbaStringToComponents(color) as Color)
-  const scale = scaleLinear<Color>(domain, colors).clamp(true)
-  return (value: number) => scale(value)?.map(Math.round) as Color
 }
