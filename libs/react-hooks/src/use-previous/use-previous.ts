9162c84c1cfb715e2fe1366eacc01d9d602c130a
j8seangel <jseangel@gmail.com>
2026-08-10 08:27:18 +0200

    fix breaking change

--- libs/react-hooks/src/use-previous/use-previous.ts
+++ libs/react-hooks/src/use-previous/use-previous.ts
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import { useEffect, useRef } from 'react'
 
 export function usePrevious<N = unknown>(value: N) {
-  const [current, setCurrent] = useState(value)
-  const [previous, setPrevious] = useState<N | undefined>(undefined)
-  if (value !== current) {
-    setPrevious(current)
-    setCurrent(value)
-  }
-  return previous
+  const ref = useRef<N | undefined>(undefined)
+  useEffect(() => {
+    ref.current = value
+  }, [value])
+  // Returning the prior render's value is the purpose of this hook.
+  // eslint-disable-next-line react-hooks/refs
+  return ref.current
 }
