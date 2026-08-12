729e40ca9eb7f3c1e61f8913f3f054d6c0b4a89b
GeorgiaDesigns <gdmespindola@gmail.com>
2026-08-12 18:47:28 -0300

    fix styles

--- apps/platform/features/help/HelpHubTopicContent.tsx
+++ apps/platform/features/help/HelpHubTopicContent.tsx
@@ -3,7 +3,7 @@ import { memo } from 'react'
 import ContentMarkdown from 'features/_map/content-panel/ContentMarkdown'
 import type { HelpHubTopic } from 'features/help/helpHub.types'
 
-import styles from './HelpHubTopicContent.module.css'
+import styles from 'features/_map/content-panel/ContentPanel.module.css'
 
 type HelpHubTopicContentProps = {
   topic: HelpHubTopic
@@ -12,6 +12,7 @@ type HelpHubTopicContentProps = {
 function HelpHubTopicContent({ topic }: HelpHubTopicContentProps) {
   return (
     <article id={topic.slug} data-topic-slug={topic.slug} className={styles.topic}>
+      <h1 className={styles.title}>{topic.title}</h1>
       {topic.thumbnail && (
         <img
           className={styles.thumbnail}
