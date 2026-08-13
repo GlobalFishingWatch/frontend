78db7ec2dcee93e7600dd1ed2a7abb9f3cfb3f5a
GeorgiaDesigns <gdmespindola@gmail.com>
2026-08-13 09:59:28 -0300

    fix translations

--- apps/platform/features/help/helpHub.i18n.ts
+++ apps/platform/features/help/helpHub.i18n.ts
@@ -6,21 +6,8 @@ export function getHelpHubSectionCopy(id: HelpHubSectionId): {
   title: string
   description: string
 } {
-  switch (id) {
-    case 'toolsAndFeatures':
-      return {
-        title: t((s) => s.helpHub.sections.toolsAndFeatures.title),
-        description: t((s) => s.helpHub.sections.toolsAndFeatures.description),
-      }
-    case 'useCases':
-      return {
-        title: t((s) => s.helpHub.sections.useCases.title),
-        description: t((s) => s.helpHub.sections.useCases.description),
-      }
-    case 'platformAndUpdates':
-      return {
-        title: t((s) => s.helpHub.sections.platformAndUpdates.title),
-        description: t((s) => s.helpHub.sections.platformAndUpdates.description),
-      }
+  return {
+    title: t((t) => t.helpHub[id].title),
+    description: t((t) => t.helpHub[id].description),
   }
 }
