f5997f4af9349846229558fc6dd908ed78b3c160
GeorgiaDesigns <gdmespindola@gmail.com>
2026-08-14 10:34:56 -0300

    update sentinel disclaimer

--- apps/platform/features/_map/workspace/shared/OutOfBoundsDisclaimer.tsx
+++ apps/platform/features/_map/workspace/shared/OutOfBoundsDisclaimer.tsx
@@ -1,5 +1,5 @@
 import { useMemo } from 'react'
-import { useTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 import cx from 'classnames'
 
 import { DataviewCategory } from '@globalfishingwatch/api-types'
@@ -7,7 +7,11 @@ import { getDatasetsExtent } from '@globalfishingwatch/datasets-client'
 import type { UrlDataviewInstance } from '@globalfishingwatch/dataviews-client'
 
 import { LAST_DATA_UPDATE } from 'data/map/config'
-import { VIIRS_DATAVIEW_INSTANCE_ID, VIIRS_SKYLIGHT_DATAVIEW_INSTANCE_ID } from 'data/map/dataviews'
+import {
+  SAR_DATAVIEW_INSTANCE_ID,
+  VIIRS_DATAVIEW_INSTANCE_ID,
+  VIIRS_SKYLIGHT_DATAVIEW_INSTANCE_ID,
+} from 'data/map/dataviews'
 import {
   getActiveDatasetsInActivityDataviews,
   getDatasetsInDataviews,
@@ -48,11 +52,33 @@ const OutOfTimerangeDisclaimer = ({
   const isVIIRSLayer =
     dataview.id.includes(VIIRS_DATAVIEW_INSTANCE_ID) &&
     !dataview.id.includes(VIIRS_SKYLIGHT_DATAVIEW_INSTANCE_ID)
+  const isSARLayer = dataview.id.includes(SAR_DATAVIEW_INSTANCE_ID)
+
   if (isVIIRSLayer) {
     return (
       <span className={cx(styles.dataWarning, styles.error, className)}>
         <span className={cx(styles.dataWarning, styles.error, className)}>
           {t((t) => t.dataset.VIIRSDelayDisclaimer)}
+        </span>
+      </span>
+    )
+  }
+
+  if (isSARLayer) {
+    return (
+      <span className={cx(styles.dataWarning, styles.error, className)}>
+        <span className={cx(styles.dataWarning, styles.error, className)}>
+          <Trans i18nKey={(t) => t.dataset.SARDelayDisclaimer}>
+            Paused while we move from Sentinel-1A to Sentinel-1C and 1D.{' '}
+            <a
+              className={styles.link}
+              href="https://globalfishingwatch.org/platform-update/sar-vessel-detection-and-fixed-infrastructure-datasets-issue"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Learn more.
+            </a>
+          </Trans>
         </span>
       </span>
     )
