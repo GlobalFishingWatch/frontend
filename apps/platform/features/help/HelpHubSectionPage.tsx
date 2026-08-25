eb5e4243618ff8d7b1ef97acf33912c02167cfdc
GeorgiaDesigns <gdmespindola@gmail.com>
2026-08-25 19:27:19 -0300

    remove unneeded div

--- apps/platform/features/help/HelpHubSectionPage.tsx
+++ apps/platform/features/help/HelpHubSectionPage.tsx
@@ -54,69 +54,67 @@ function HelpHubSectionPage() {
   }
 
   return (
-    <div className={styles.page}>
-      <div className={styles.container}>
-        <div className={styles.aside}>
-          {copy && (
-            <header className={styles.header}>
-              <nav>
-                <ul className={styles.breadcrumb}>
-                  <li>
-                    <Link to={ROUTE_PATHS.HELP_HUB} className={styles.breadcrumbLink}>
-                      {t((s) => s.helpHub.title)}
-                    </Link>
-                  </li>
-                  <li className={styles.breadcrumbSeparator} aria-hidden="true">
-                    ›
-                  </li>
-                  <li>{copy.title}</li>
-                </ul>
-              </nav>
-            </header>
+    <div className={styles.container}>
+      <div className={styles.aside}>
+        {copy && (
+          <header className={styles.header}>
+            <nav>
+              <ul className={styles.breadcrumb}>
+                <li>
+                  <Link to={ROUTE_PATHS.HELP_HUB} className={styles.breadcrumbLink}>
+                    {t((s) => s.helpHub.title)}
+                  </Link>
+                </li>
+                <li className={styles.breadcrumbSeparator} aria-hidden="true">
+                  ›
+                </li>
+                <li>{copy.title}</li>
+              </ul>
+            </nav>
+          </header>
+        )}
+        <TableOfContents
+          data={index}
+          activeId={activeSlug}
+          className={styles.tableOfContents}
+          onClick={goToItem}
+          onSubItemClick={(sectionId, subId) => {
+            if (sectionId === activeSlug) {
+              scrollContainerRef.current
+                ?.querySelector(`#${CSS.escape(subId)}`)
+                ?.scrollIntoView({ block: 'start' })
+              return
+            }
+            goToItem(sectionId)
+          }}
+        />
+      </div>
+      <div ref={scrollContainerRef} className={styles.content}>
+        <HelpHubItemContent item={item} />
+        <nav className={styles.pagination}>
+          {previous && (
+            <Link
+              to={ROUTE_PATHS.HELP_HUB_SECTION}
+              params={{ sectionSlug, itemSlug: previous.slug }}
+              className={styles.paginationLink}
+            >
+              <span className={styles.paginationLabel}>
+                ‹ {t((s) => s.helpHub.previousArticle)}
+              </span>
+              <span className={styles.paginationTitle}>{previous.title}</span>
+            </Link>
           )}
-          <TableOfContents
-            data={index}
-            activeId={activeSlug}
-            className={styles.tableOfContents}
-            onClick={goToItem}
-            onSubItemClick={(sectionId, subId) => {
-              if (sectionId === activeSlug) {
-                scrollContainerRef.current
-                  ?.querySelector(`#${CSS.escape(subId)}`)
-                  ?.scrollIntoView({ block: 'start' })
-                return
-              }
-              goToItem(sectionId)
-            }}
-          />
-        </div>
-        <div ref={scrollContainerRef} className={styles.content}>
-          <HelpHubItemContent item={item} />
-          <nav className={styles.pagination}>
-            {previous && (
-              <Link
-                to={ROUTE_PATHS.HELP_HUB_SECTION}
-                params={{ sectionSlug, itemSlug: previous.slug }}
-                className={styles.paginationLink}
-              >
-                <span className={styles.paginationLabel}>
-                  ‹ {t((s) => s.helpHub.previousArticle)}
-                </span>
-                <span className={styles.paginationTitle}>{previous.title}</span>
-              </Link>
-            )}
-            {next && (
-              <Link
-                to={ROUTE_PATHS.HELP_HUB_SECTION}
-                params={{ sectionSlug, itemSlug: next.slug }}
-                className={cx(styles.paginationLink, styles.paginationNext)}
-              >
-                <span className={styles.paginationLabel}>{t((s) => s.helpHub.nextArticle)} ›</span>
-                <span className={styles.paginationTitle}>{next.title}</span>
-              </Link>
-            )}
-          </nav>
-        </div>
+          {next && (
+            <Link
+              to={ROUTE_PATHS.HELP_HUB_SECTION}
+              params={{ sectionSlug, itemSlug: next.slug }}
+              className={cx(styles.paginationLink, styles.paginationNext)}
+            >
+              <span className={styles.paginationLabel}>{t((s) => s.helpHub.nextArticle)} ›</span>
+              <span className={styles.paginationTitle}>{next.title}</span>
+            </Link>
+          )}
+        </nav>
       </div>
     </div>
   )
