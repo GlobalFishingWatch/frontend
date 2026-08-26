a50a990b3bc75cd707cd873bfbd7cae1b4f2e81a
GeorgiaDesigns <gdmespindola@gmail.com>
2026-08-26 10:58:57 -0300

    fix url

--- apps/data-download-portal/src/config.ts
+++ apps/data-download-portal/src/config.ts
@@ -3,6 +3,6 @@ export const API_GATEWAY =
   import.meta.env.VITE_API_GATEWAY || 'https://gateway.api.dev.globalfishingwatch.org'
 
 export const DOWNLOAD_SURVEY_URL =
-  import.meta.env.VITE_DOWNLOAD_SURVEY_URL || '/map/api/downloadSurvey'
+  import.meta.env.VITE_DOWNLOAD_SURVEY_URL || '/platform/api/downloadSurvey'
 
 export const DISABLE_DOWNLOAD_SURVEY = 'disableDownloadSurveyDDP'
