0358e39bdf8fb69bc02c28a5b4498134abb4e70c
GeorgiaDesigns <gdmespindola@gmail.com>
2026-09-15 13:52:04 -0300

    Update report modal for dynamic days and editing state

--- apps/platform/features/_reports/shared/new-report-modal/NewAreaReportModal.tsx
+++ apps/platform/features/_reports/shared/new-report-modal/NewAreaReportModal.tsx
@@ -19,12 +19,13 @@ import {
   DAYS_FROM_LATEST_MAX,
   DAYS_FROM_LATEST_MIN,
   getViewAccessOptions,
+  isValidDaysFromLatest,
 } from 'features/_map/workspace/save/workspace-save.utils'
 import { selectWorkspaceWithCurrentState } from 'features/_map/workspace/selectors/app.workspace.selectors'
 import { getWorkspaceReport } from 'features/_map/workspace/workspace.utils'
 import { selectReportAreaIds } from 'features/_reports/report-area/area-reports.selectors'
 import { createReportThunk, updateReportThunk } from 'features/_reports/reports.slice'
-import { selectUserData } from 'features/_user/selectors/user.selectors'
+import { selectIsGFWUser, selectUserData } from 'features/_user/selectors/user.selectors'
 import { TrackCategory, trackEvent } from 'features/app/analytics.hooks'
 import { useAppDispatch } from 'features/app/app.hooks'
 import { selectDatasetAreaDetail } from 'features/data/areas/areas.slice'
@@ -48,6 +49,7 @@ function NewReportModal({ isOpen, onClose, onFinish, report }: NewReportModalPro
   const privateDatasets = useSelector(selectPrivateDatasetsInWorkspace)
   const containsPrivateDatasets = privateDatasets.length > 0
   const userData = useSelector(selectUserData)
+  const isGFWUser = useSelector(selectIsGFWUser)
 
   const [name, setName] = useState(report?.name || reportArea?.name || '')
   const [description, setDescription] = useState(report?.description || '')
@@ -65,8 +67,10 @@ function NewReportModal({ isOpen, onClose, onFinish, report }: NewReportModalPro
     handleTimeRangeChange,
   } = useSaveWorkspaceTimerange(workspace)
   const [loading, setLoading] = useState(false)
+  const validDaysFromLatestValue =
+    timeRangeOption === 'dynamic' ? isValidDaysFromLatest(daysFromLatest) : true
 
-  const isEditing = report?.id !== undefined
+  const isEditing = report?.id !== undefined && (report.ownerId === userData?.id || isGFWUser)
 
   const updateReport = async (event: any) => {
     event.preventDefault()
@@ -223,7 +227,7 @@ function NewReportModal({ isOpen, onClose, onFinish, report }: NewReportModalPro
         )}
         <div className={styles.footer}>
           {error && <p className={styles.error}>{error}</p>}
-          <Button loading={loading} disabled={!name} htmlType="submit">
+          <Button loading={loading} disabled={!name || !validDaysFromLatestValue} htmlType="submit">
             {isEditing ? t((t) => t.common.update) : t((t) => t.common.save)}
           </Button>
         </div>
